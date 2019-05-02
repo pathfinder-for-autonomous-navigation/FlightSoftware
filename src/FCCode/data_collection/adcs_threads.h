150401d6bc826e3623c8c5766dab265a6d902e5f
athena255 <athena.cheung97@gmail.com>
2019-05-02 16:25:21 -0400

    Add j2 template and documentation

--- src/FCCode/data_collection/adcs_threads.h
+++ src/FCCode/data_collection/adcs_threads.h
@@ -1,19 +1,28 @@
+// adcs_threads.h
+// 
+// Need to set [PRODUCER_COUNT]!!!
+// 
+
 namespace adcs_threads
 {
 #include "circular_buffer.hpp"
-#define PRODUCER_COUNT 3 // must set this
+
+// SET THIS --------------------------
+#define PRODUCER_COUNT 3 
+// ------------------------------------
+
 void consumer(void *vargp);
 void init();
-template <size_t data_size, size_t history_size, typename T>
+template <typename T, size_t sample_size>
 void producer(void *vargp);
-template <size_t data_size, size_t history_size, typename T>
-thread_t *deployProducers(std::array<T, data_size> data,
-                          circular_buffer<std::array<T, data_size>, history_size> history,
-                          size_t rate,
-                          size_t i);
+// template <typename T, size_t sample_size>
+// thread_t *deployProducers(T data,
+//                           circular_buffer<T, sample_size> history,
+//                           size_t rate,
+//                           size_t i);
 extern thread_t *consumer_thd;
 extern thread_t *prod_threads[PRODUCER_COUNT];
 extern THD_WORKING_AREA(waConsumerThd, 16384);
-extern THD_WORKING_AREA(waProd[PRODUCER_COUNT], 128);
+extern THD_WORKING_AREA(waProd[PRODUCER_COUNT], 64);
 
-} // namespace adcs_threads
+} 
