d29a76b24e6c96477515deb139f6a2f404e13426
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2019-09-06 22:21:26 -0400

    Updating console script

--- console/console.py
+++ console/console.py
@@ -2,19 +2,88 @@
 import sys
 import datetime
 import serial
+import threading
 import struct
 import json
+from cmd import Cmd
 from argparse import ArgumentParser
 
-def close_logfile(args):
-    if args.store_log:
-        print("Saving log.")
-        logfile.close()
+class FCStateCmdPrompt(Cmd):
+    def __init__(self, console_port, logfile_name, store_log):
+        self.logfile = open(logfile_name)
 
-def graceful_exit(args):
-    close_logfile(args)
-    console.close()
-    sys.exit()
+        try:
+            self.console = serial.Serial(console_port, 9600)
+        except serial.SerialException:
+            print("Error: unable to open serial port. Exiting.")
+            self.do_quit(None)
+        except KeyboardInterrupt:
+            print("Exiting due to keyboard interrupt.")
+            self.do_quit(None)
+
+        self.store_log = store_log
+        self.start_time = datetime.datetime.now()
+
+    def preloop(self):
+        try:
+            # Read line coming from device and parse it
+            line = self.console.readline().rstrip()
+            data = json.loads(line)
+
+            # Print it to the logfile
+            if "msg" in data:
+                logline = "[{}] ({}) {}".format(data["time"], data["svrty"],
+                                                data["msg"])
+            else:
+                # Write state variable to 
+                logline = "[{}] (STATEMSG) {} = {}".format(
+                    data["time"], data["field"], data["val"])
+
+            data["time"] = self.start_time + datetime.timedelta(
+                milliseconds=data["t"])
+        except json.JSONDecodeError:
+            logline = "[RAW] {}".format(line)
+        except serial.SerialException:
+            print("Error: unable to read serial port. Exiting.")
+            self.do_quit(None)
+        except:
+            print("Unspecified error. Exiting.")
+            self.do_quit(None)
+
+        if args.store_log:
+            self.logfile.write(logline + "\n")
+
+    def do_rs(self, args):
+        """Read the value of the state field associated with the given field name on the flight controller."""
+        field_name = args[0]
+        json_cmd = {
+            "r/w" : 'r',
+            "field" : field_name
+        }
+        self.console.write(json.dumps(json_cmd))
+        # Getting the actual value is handled by the logger thread.
+
+    def do_ws(self, args):
+        """Overwrite the value of the state field with the given state field name on the flight controller."""
+        field_name = args[0]
+        val = args[1]
+        json_cmd = {
+            "r/w" : 'w',
+            "field" : field_name,
+            "val" : val
+        }
+        self.console.write(json.dumps(json_cmd))
+        # Getting a confirmation of the actual value being set is
+        # handled by the logger thread.
+
+    def do_quit(self, args):
+        """Quits the program."""
+        if self.store_log:
+            print("Saving log.")
+            self.logfile.close()
+        self.console.close()
+        print("Quitting.")
+        raise SystemExit
 
 if __name__ == '__main__':
     parser = ArgumentParser(description="Parses console output from Flight Controller into human-readable, storable logging information.")
@@ -25,47 +94,11 @@ if __name__ == '__main__':
               Default is logs/.""", default="logs")
     args = parser.parse_args()
 
-    if args.store_log:
-        logfile = open("{}/{}.log".format(args.log_dir, str(datetime.datetime.now())), "w")
+    logfile_name = "{}/{}.log".format(args.log_dir, str(datetime.datetime.now()))
 
     with open("pan_logo.txt", "r") as pan_logo:
-        print(pan_logo.read())
+        pan_logo_str = pan_logo.read()
 
-    try:
-        start_time = datetime.datetime.now()
-        console = serial.Serial(args.port, 9600)
-    except serial.SerialException:
-        print("Error: unable to open serial port. Exiting.")
-        graceful_exit(args)
-    except KeyboardInterrupt:
-        print("Exiting due to keyboard interrupt.")
-        graceful_exit(args)
-
-    while True:
-        try:
-            # Read line coming from device and parse it
-            line = console.readline().rstrip()
-            data = json.loads(line)
-
-            # Print it to console and (optionally) to logfile
-            if "msg" in data:
-                logline = "[{}] ({}) {}".format(data["time"], data["svrty"], data["msg"])
-            else:
-                logline = "[{}] (STATEMSG) {} = {}".format(data["time"], data["field"], data["val"])
-
-            data["time"] = start_time + datetime.timedelta(milliseconds=data["t"])
-        except json.JSONDecodeError:
-            logline = "[RAW] {}".format(line)
-        except serial.SerialException:
-            print("Error: unable to read serial port. Exiting.")
-            graceful_exit(args)
-        except KeyboardInterrupt:
-            print("Received keyboard interrupt. Exiting.")
-            graceful_exit(args)
-        except:
-            print("Unspecified error. Exiting.")
-            graceful_exit(args)
-        
-        print(logline)
-        if args.store_log:
-            logfile.write(logline + "\n")
+        cmd_prompt = FCStateCmdPrompt(args.port, logfile_name, args.store_log)
+        cmd_prompt.prompt = "> "
+        cmd_prompt.cmdloop(pan_logo_str)
