6431b1c0a4906a42d8f646ae4abd2ff42ad849db
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2019-10-04 17:19:23 -0400

    Nonfunctional demo

--- src/test_gomspace.cpp
+++ src/test_gomspace.cpp
@@ -1,18 +1,29 @@
 #include <Arduino.h>
 #include <Gomspace.hpp>
+#include <memory>
 
 using namespace Devices;
+
+
 
 Gomspace::eps_hk_t hk_data;
 Gomspace::eps_config_t config_data;
 Gomspace::eps_config2_t config2_data;
-Gomspace gs("gomspace", &hk_data, &config_data, &config2_data, Wire1, 0x02);
+std::shared_ptr<Gomspace> gs;
 
 void setup() {
-    gs.setup();
+    Wire.begin(I2C_MASTER, 0x00, I2C_PINS_18_19, I2C_PULLUP_EXT, 400000, I2C_OP_MODE_IMM);
+    Serial.begin(9600);
+    while(!Serial);
+    Serial.println("Hi!");
+    gs = std::make_shared<Gomspace>("gomspace", &hk_data, &config_data, &config2_data, Wire, 0x02);
+    gs->setup();
 }
 
 void loop() {
-    Serial.println(gs.is_functional());
-    delay(1000);
+    Serial.println(gs->is_functional());
+    digitalWrite(13, HIGH);
+    delay(500);
+    digitalWrite(13, LOW);
+    delay(500);
 }
