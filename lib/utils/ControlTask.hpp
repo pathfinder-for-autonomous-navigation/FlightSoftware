613e08f1b2a53ea1eaa5223a1fb4b419dcd51bc4
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2019-10-12 23:06:54 -0400

    Dummy Flight Software testing

--- lib/utils/ControlTask.hpp
+++ lib/utils/ControlTask.hpp
@@ -2,7 +2,6 @@
 #define CONTROL_TASK_HPP_
 
 #include <memory>
-#include <iostream>
 #include <string>
 #include <debug_console.hpp>
 #include <Nameable.hpp>
@@ -35,44 +34,32 @@ class ControlTask : protected debug_console {
   protected:
     StateFieldRegistry& _registry;
 
-    /**
-     * @brief Find a field of a given name within the state registry
-     * and return a pointer to it.
-     *
-     * @param name Name of state field.
-     *
-     * @return Pointer to field, or null pointer if field doesn't exist.
-     */
-    std::shared_ptr<StateFieldBase> find_field(const std::string& name) {
-        return _registry.find_field(name);
-    }
-
     template<typename U>
     void add_readable_field(ReadableStateField<U>& field) {
-        std::shared_ptr<ReadableStateField<U>> field_ptr(
-            std::shared_ptr<ReadableStateField<U>>{}, &field);
-        _registry.add_readable_field(field_ptr);
+        auto field_ptr_base = field.ptr();
+        _registry.add_readable_field(field_ptr_base);
     }
 
     template<typename U>
     void add_writable_field(WritableStateField<U>& field) {
-        std::shared_ptr<WritableStateField<U>> field_ptr(
-            std::shared_ptr<WritableStateField<U>>{}, &field);
-        _registry.add_writable_field(field_ptr);
+        auto field_ptr_base = field.ptr();
+        _registry.add_writable_field(field_ptr_base);
     }
 
     template<typename U>
-    void find_readable_field(const char* field, std::shared_ptr<ReadableStateField<U>>* field_ptr, const char* file, const unsigned int line) {
-        *field_ptr = std::static_pointer_cast<ReadableStateField<U>>(_registry.find_readable_field(field));
-        if (!(*field_ptr)) { 
+    void find_readable_field(const char* field, std::shared_ptr<ReadableStateField<U>>& field_ptr, const char* file, const unsigned int line) {
+        auto field_ptr_base = field_ptr->ptr();
+        bool found = _registry.find_readable_field(field, field_ptr_base);
+        if (!found) { 
             printf(debug_severity::error, "%s:%d: Readable field required is not present in state registry: %s\n", file, line, field);
         }
     }
 
     template<typename U>
-    void find_writable_field(const char* field, std::shared_ptr<WritableStateField<U>>* field_ptr, const char* file, const unsigned int line) {
-        *field_ptr = std::static_pointer_cast<WritableStateField<U>>(_registry.find_writable_field(field));
-        if (!(*field_ptr)) { 
+    void find_writable_field(const char* field, std::shared_ptr<WritableStateField<U>>& field_ptr, const char* file, const unsigned int line) {
+        auto field_ptr_base = field_ptr->ptr();
+        bool found = _registry.find_writable_field(field, field_ptr_base);
+        if (!found) { 
             printf(debug_severity::error, "%s:%d: Writable field required is not present in state registry: %s\n", file, line, field);
         }
     }
