5f113106181ce499a4535c5e07c592de9d735606
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2019-10-15 02:51:40 -0400

    Adding more testing

--- tools/travis_script.sh
+++ tools/travis_script.sh
@@ -1,9 +1,11 @@
 set -e # Exit on error
 
-# Compile and run native environments
+# Compile and run test for non-optimized native environments
 platformio run -e native_ci
 platformio test -e native_ci
 python -m unittest test_dummy_fsw
+
+# Verify compilation for optimized native environment
 platformio run -e native
 
 # Compile and run HOOTL/Flight Teensy environments
@@ -18,3 +20,9 @@ platformio run -e teensy_35_test_quake_network
 platformio run -e teensy_36_test_quake
 platformio run -e teensy_36_test_quake_no_network
 platformio run -e teensy_36_test_quake_network
+
+# Run tests for optimized native environment. These are
+# identical to the tests for the non-optimized environment,
+# so they're lower-priority in this CI build step (they're less
+# likely to fail the build sequence.)
+platformio test -e native
