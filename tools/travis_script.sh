a037ce2de036bea4f33ceb9828e311bc0b16006b
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2019-10-16 02:57:53 -0400

    Add cppcheck and valgrind

--- tools/travis_script.sh
+++ tools/travis_script.sh
@@ -1,5 +1,8 @@
 set -e # Exit on error
 
+# Run static analysis
+cppcheck test src lib/utils lib/Drivers lib/Devices lib/rwmutex
+
 # Compile and run functional test for non-optimized native environment
 platformio run -e native_ci
 python -m unittest -v test_dummy_fsw
@@ -28,3 +31,6 @@ platformio run -e teensy_36_test_quake_network
 #
 # We also need this step to run last so that code coverage is available.
 platformio test -e native_ci
+
+# Check for memory mismanagement
+valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose .pio/build/native_ci/program
