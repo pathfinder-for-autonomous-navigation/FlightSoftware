9923580c418a49a966584987945efb76f88b543d
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2019-10-25 18:38:46 -0400

    Update dummy control task test

--- test/test_fsw_control_task/test_control_task.cpp
+++ test/test_fsw_control_task/test_control_task.cpp
@@ -1,18 +1,47 @@
-#include <StateFieldRegistry.hpp>
-#include "DummyControlTask.hpp"
+#include "../StateFieldRegistryMock.hpp"
+#include <ControlTask.hpp>
 #include <unity.h>
 
+class DummyControlTask : public ControlTask<void> {
+  public:
+    DummyControlTask(StateFieldRegistry& registry) :
+      ControlTask<void>(registry),
+      x_sr(0, 10, 10),
+      x_f("dummy_field", x_sr)
+    {
+          add_writable_field(x_f);
+    }
+
+    void execute() { 
+      x_f.set(3);
+    }
+    
+    Serializer<unsigned int> x_sr;
+    WritableStateField<unsigned int> x_f;
+};
+
+class TestFixture {
+  public:
+    StateFieldRegistryMock registry;
+    std::unique_ptr<DummyControlTask> dummy_task;
+
+    std::shared_ptr<WritableStateField<unsigned int>> x_fp;
+
+    TestFixture() : registry() {
+        dummy_task = std::make_unique<DummyControlTask>(registry);
+        x_fp = std::static_pointer_cast<WritableStateField<unsigned int>>(registry.find_writable_field("dummy_field"));
+    }
+};
+
 void test_task_initialization() {
-    StateFieldRegistry registry;
-    DummyControlTask task(registry);
+    TestFixture tf;
 }
 
 void test_task_execute() {
-    StateFieldRegistry registry;
-    DummyControlTask task(registry);
-    TEST_ASSERT_EQUAL(2, task.x);
-    task.execute();
-    TEST_ASSERT_EQUAL(3, task.x);
+    TestFixture tf;
+    TEST_ASSERT_EQUAL(0, tf.x_fp->get());
+    tf.dummy_task->execute();
+    TEST_ASSERT_EQUAL(3, tf.x_fp->get());
 }
 
 int test_control_task() {
