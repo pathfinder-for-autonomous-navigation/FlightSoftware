a67c287756a9188d28121fd529d4362d4209b5fd
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2019-10-25 18:43:20 -0400

    Even better dummy test

--- test/test_fsw_control_task/test_control_task.cpp
+++ test/test_fsw_control_task/test_control_task.cpp
@@ -7,14 +7,18 @@ class DummyControlTask : public ControlTask<void> {
     DummyControlTask(StateFieldRegistry& registry) :
       ControlTask<void>(registry),
       x_sr(0, 10, 10),
-      x_f("dummy_field", x_sr)
+      x_f("dummy_x", x_sr)
     {
-          add_writable_field(x_f);
+        y_fp = find_writable_field<unsigned int>("dummy_y", __FILE__, __LINE__);
+        add_writable_field(x_f);
     }
 
     void execute() { 
-      x_f.set(3);
+      x_f.set(y_fp->get());
     }
+
+  protected:
+    std::shared_ptr<WritableStateField<unsigned int>> y_fp;
     
     Serializer<unsigned int> x_sr;
     WritableStateField<unsigned int> x_f;
@@ -23,23 +27,29 @@ class DummyControlTask : public ControlTask<void> {
 class TestFixture {
   public:
     StateFieldRegistryMock registry;
-    std::unique_ptr<DummyControlTask> dummy_task;
+    std::shared_ptr<WritableStateField<unsigned int>> y_fp;
 
+    std::unique_ptr<DummyControlTask> dummy_task;
     std::shared_ptr<WritableStateField<unsigned int>> x_fp;
 
     TestFixture() : registry() {
+        y_fp = registry.create_writable_field<unsigned int>("dummy_y", 0, 10, 10);
+        y_fp->set(3);
+
         dummy_task = std::make_unique<DummyControlTask>(registry);
-        x_fp = std::static_pointer_cast<WritableStateField<unsigned int>>(registry.find_writable_field("dummy_field"));
+        x_fp = std::static_pointer_cast<WritableStateField<unsigned int>>(registry.find_writable_field("dummy_x"));
     }
 };
 
 void test_task_initialization() {
     TestFixture tf;
+    TEST_ASSERT_EQUAL(3, tf.y_fp->get());
 }
 
 void test_task_execute() {
     TestFixture tf;
     TEST_ASSERT_EQUAL(0, tf.x_fp->get());
+    tf.y_fp->set(3);
     tf.dummy_task->execute();
     TEST_ASSERT_EQUAL(3, tf.x_fp->get());
 }
