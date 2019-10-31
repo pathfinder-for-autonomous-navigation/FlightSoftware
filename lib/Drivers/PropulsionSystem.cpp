0a431233ee55e0f5cf0b70b1afaac40808747b92
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2019-10-31 19:44:15 -0400

    Working on prop refactoring

--- lib/Drivers/PropulsionSystem.cpp
+++ lib/Drivers/PropulsionSystem.cpp
@@ -16,6 +16,9 @@ volatile bool PropulsionSystem::valve_start_locked_out = false;
 PropulsionSystem::PropulsionSystem() : Device("propulsion") {}
 
 bool PropulsionSystem::setup() {
+#ifdef DESKTOP
+// TODO
+#else
     for (unsigned char i = 0; i < 6; i++) {
         pinMode(valve_pins[i], OUTPUT);
     }
@@ -26,16 +29,25 @@ bool PropulsionSystem::setup() {
     pinMode(temp_sensor_outer_pin, INPUT);
 
     thrust_valve_loop_timer.begin(thrust_valve_loop, thrust_valve_loop_interval_us); // Every 5 ms
+#endif
     return true;
 }
 
 void PropulsionSystem::disable() {
+#ifdef DESKTOP
+// TODO
+#else
     for (unsigned char i = 0; i < 6; i++) digitalWrite(valve_pins[i], LOW);
     thrust_valve_loop_timer.end();
+#endif
 }
 
 void PropulsionSystem::enable() {
+#ifdef DESKTOP
+// TODO
+#else
     thrust_valve_loop_timer.begin(thrust_valve_loop, thrust_valve_loop_interval_us);
+#endif
 }
 
 void PropulsionSystem::reset() {
@@ -50,9 +62,15 @@ float PropulsionSystem::get_pressure() {
     static float pressure = 0;
     // set the two pressure pins as inputs
 
+    
+#ifdef DESKTOP
+    low_gain_read = 2; // TODO fix
+    high_gain_read = 2;
+#else
     // analog read
     low_gain_read = analogRead(pressure_sensor_low_pin);
     high_gain_read = analogRead(pressure_sensor_high_pin);
+#endif
 
     // convert to pressure [psia]
     if (high_gain_read < 1000){
@@ -63,25 +81,44 @@ float PropulsionSystem::get_pressure() {
 
     return pressure;
 }
+
 signed int PropulsionSystem::get_temp_inner() {
+#ifdef DESKTOP
     return 0;
+#else
+    return 0; // TODO replace with analogRead
+#endif
 }
 signed int PropulsionSystem::get_temp_outer() {
+#ifdef DESKTOP
     return 0;
+#else
+    return 0; // TODO replace with analogRead
+#endif
 }
 
-bool PropulsionSystem::is_functional() { return digitalRead(DCDC::dcdc_sph_enable_pin); }
+bool PropulsionSystem::is_functional() { 
+#ifdef DESKTOP
+    return true;
+#else
+    return digitalRead(DCDC::dcdc_sph_enable_pin);
+#endif
+}
 
 void PropulsionSystem::set_thrust_valve_schedule(const std::array<unsigned int, 4> &setting) {
     disable();
+#ifndef DESKTOP
     delayMicroseconds(10); // Wait for current cycle of the thrust valve loop to end
+#endif
     for(size_t i = 0; i < 4; i++) thrust_valve_schedule[i] = setting[i];
     enable();
 }
 
 void PropulsionSystem::set_tank_valve_state(bool valve, bool state) {
     disable();
+#ifndef DESKTOP
     delayMicroseconds(10); // Wait for current cycle of the thrust valve loop to end
+#endif
     digitalWrite(valve_pins[valve], state);
     enable();
 }
@@ -89,14 +126,18 @@ void PropulsionSystem::set_tank_valve_state(bool valve, bool state) {
 void PropulsionSystem::thrust_valve_loop() {
     for (unsigned char i = 2; i < 6; i++) {
         if (thrust_valve_schedule[i - 2] < thrust_valve_loop_interval_ms) {
+            #ifndef DESKTOP
             digitalWrite(valve_pins[i - 2], LOW);
+            #endif
             thrust_valve_schedule[i - 2] = 0;
             is_valve_opened[i] = false;
             continue;
         }
         else if (!is_valve_opened[i] && !valve_start_locked_out) {
             valve_start_locked_out = true;
+            #ifndef DESKTOP
             digitalWrite(valve_pins[i - 2], HIGH);
+            #endif
             is_valve_opened[i] = true;
         }
         else if (valve_start_locked_out) {
