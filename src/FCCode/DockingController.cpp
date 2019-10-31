614a195559d1c1bc6413f23004ae3925d1369846
Fatima Yousuf <fatimayousuf@dhcp-gs-1367.eduroam.cornell.edu>
2019-10-31 18:20:08 -0400

    updated docking controller

--- src/FCCode/DockingController.cpp
+++ src/FCCode/DockingController.cpp
@@ -1,22 +1,30 @@
 #include "DockingController.hpp"
 
 DockingController::DockingController(StateFieldRegistry& registry)
-    : ControlTask<void>(registry), step_angle_sr(0, 10, 10), step_angle_f("step_angle", step_angle_sr),
+    : ControlTask<void>(registry), set_mode_sr(0,10,10), set_mode_f("set_mode", set_mode_sr)
+    /**step_angle_sr(0, 10, 10), step_angle_f("step_angle", step_angle_sr),
     is_turning_clockwise_sr(), is_turning_clockwise_f("is_turning_clockwise", is_turning_clockwise_sr),
-    is_enabled_sr(), is_enabled_f("is_enabled", is_enabled_sr)
+    is_enabled_sr(), is_enabled_f("is_enabled", is_enabled_sr)**/
     {
-        step_angle_fp = find_writable_field<unsigned int>("step_angle", __FILE__, __LINE__);
+        check_mode_fp = find_writable_field<unsigned int>("check_mode", __FILE__, __LINE__);
+        add_writable_field(set_mode_f);
+        /**
+        step_angle_fp = find_writable_field<float>("step_angle", __FILE__, __LINE__);
         add_writable_field(step_angle_f);
 
-        is_turning_clockwise_fp = find_writable_field<unsigned int>("is_turning_clockwise", __FILE__, __LINE__);
+        is_turning_clockwise_fp = find_writable_field<bool>("is_turning_clockwise", __FILE__, __LINE__);
         add_readable_field(is_turning_clockwise_f);
 
-        is_enabled_fp = find_writable_field<unsigned int>("is_enabled", __FILE__, __LINE__);
+        is_enabled_fp = find_writable_field<bool>("is_enabled", __FILE__, __LINE__);
         add_readable_field(is_enabled_f);
+        **/
     }
 
-    void execute() { 
+    void DockingController::execute() { 
+      set_mode_f.set(check_mode_fp->get());
+      /**
       step_angle_f.set(step_angle_fp->get());
-      is_turning_f_clockwise.set(is_turning_clockwise_fp.get());
+      is_turning_clockwise_f.set(is_turning_clockwise_fp.get());
       is_enabled_f.set(is_enabled_fp.get());
+      **/
     }
--- src/FCCode/DockingController.hpp
+++ src/FCCode/DockingController.hpp
@@ -4,6 +4,8 @@
 #include "../../test/StateFieldRegistryMock.hpp"
 #include <ControlTask.hpp>
 #include <unity.h>
+//include the Device Class
+// I need to create a pointer to the input "Is Docked" and a field for output set ""
 
 class DockingController : public ControlTask<void> {
    public:
@@ -15,6 +17,12 @@ class DockingController : public ControlTask<void> {
     void set_step_angle();
     
 
+    Serializer<unsigned int> set_mode_sr;
+    WritableStateField<unsigned int>set_mode_f;
+
+    std::shared_ptr<WritableStateField<unsigned int>> check_mode_fp;
+
+    /**
     std::shared_ptr<WritableStateField<float>> step_angle_fp;
     Serializer<float> step_angle_sr;
     WritableStateField<float>step_angle_f;
@@ -23,9 +31,10 @@ class DockingController : public ControlTask<void> {
     Serializer<bool> is_turning_clockwise_sr;
     ReadableStateField<bool>is_turning_clockwise_f;
 
-    std::shared_ptr<WritableStateField<float>> is_enabled_fp;
+    std::shared_ptr<WritableStateField<bool>> is_enabled_fp;
     Serializer<bool> is_enabled_sr;
     ReadableStateField<bool>is_enabled_f;
+    **/
     
 };
 
