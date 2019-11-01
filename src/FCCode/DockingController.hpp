1e0d01477b3b04e73f82410fb998f53580812c2e
Fatima Yousuf <fatimayousuf@dhcp-rhodes-53.wifi-visitor.cit.cornell.edu>
2019-11-01 16:53:38 -0400

    updated docking controller

--- src/FCCode/DockingController.hpp
+++ src/FCCode/DockingController.hpp
@@ -4,8 +4,8 @@
 #include "../../test/StateFieldRegistryMock.hpp"
 #include <ControlTask.hpp>
 #include <unity.h>
+#include <Device.hpp>
 //include the Device Class
-// I need to create a pointer to the input "Is Docked" and a field for output set ""
 
 class DockingController : public ControlTask<void> {
    public:
@@ -21,20 +21,6 @@ class DockingController : public ControlTask<void> {
     WritableStateField<unsigned int>set_mode_f;
 
     std::shared_ptr<WritableStateField<unsigned int>> check_mode_fp;
-
-    /**
-    std::shared_ptr<WritableStateField<float>> step_angle_fp;
-    Serializer<float> step_angle_sr;
-    WritableStateField<float>step_angle_f;
-
-    std::shared_ptr<WritableStateField<bool>> is_turning_clockwise_fp;
-    Serializer<bool> is_turning_clockwise_sr;
-    ReadableStateField<bool>is_turning_clockwise_f;
-
-    std::shared_ptr<WritableStateField<bool>> is_enabled_fp;
-    Serializer<bool> is_enabled_sr;
-    ReadableStateField<bool>is_enabled_f;
-    **/
     
 };
 
