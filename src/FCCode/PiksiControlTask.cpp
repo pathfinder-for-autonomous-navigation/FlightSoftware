670b2a79aa014e9e3f1a7b00d19856a13db30f5b
Shihao Cao <shihaocao@gmail.com>
2019-11-02 15:59:27 -0400

    halfway copy Athena lol

---
+++ src/FCCode/PiksiControlTask.cpp
@@ -0,0 +1,170 @@
+#include "PiksiControlTask.hpp"
+
+using namespace Devices;
+
+void PiksiControlTask::set_downlink_msg(const char *_szMsg, size_t _len)
+{
+  szMsg = _szMsg;
+  len = _len;
+}
+
+int PiksiControlTask::get_current_state() const
+{
+  return currentState;
+}
+
+size_t PiksiControlTask::get_current_fn_number() const
+{
+  return fnSeqNum;
+}
+
+bool PiksiControlTask::request_state(int requested_state)
+{
+  if (requested_state == CONFIG)
+    currentState = CONFIG;
+  else if (currentState == IDLE)
+    currentState = requested_state;
+  else
+    return false;
+  fnSeqNum = 0;
+  return true;
+}
+
+int PiksiControlTask::execute()
+{
+  // TODO: allow any state to ignore state and call CONFIG, remember to reset fnSeqNum
+  int result = false;
+  switch (currentState)
+  {
+  case SBDWB:
+    result = dispatch_sbdwb();
+    break;
+  case SBDRB:
+    result = dispatch_sbdrb();
+    break;
+  case SBDIX:
+    result = dispatch_sbdix();
+    break;
+  case CONFIG:
+    result = dispatch_config();
+    break;
+  case IS_FUNCTIONAL:
+    result = dispatch_is_functional();
+    break;
+  case IDLE:
+    // TODO: check if statefield registry request has been set
+    // if so, set currentState and rerun execute
+    break;
+  default:
+    // TODO: set error message
+    currentState = IDLE;
+  }
+  // Reset currentState to idle if fnSeqNum == 0 since that means we executed the last function
+  if (fnSeqNum == 0)
+    currentState = IDLE;
+  return result;
+}
+
+int PiksiControlTask::dispatch_sbdwb()
+{
+  int errCode = -1;
+  switch (fnSeqNum)
+  {
+  case 0:
+    errCode = quake.query_sbdwb_1(len);
+    break;
+  case 1:
+    errCode = quake.query_sbdwb_2(szMsg, len);
+    break;
+  case 2:
+    errCode = quake.get_sbdwb();
+    break;
+  default:
+    return WRONG_FN_ORDER; // don't know why fnSeqNum would be wrong
+  }
+  if (errCode == OK)
+    fnSeqNum = (fnSeqNum + 1) % 3;
+  return errCode;
+}
+
+int PiksiControlTask::dispatch_sbdrb()
+{
+  int errCode = -1;
+  switch (fnSeqNum)
+  {
+  case 0:
+    errCode = quake.query_sbdrb_1();
+    break;
+  case 1:
+    errCode = quake.get_sbdrb();
+    break;
+  default:
+    return WRONG_FN_ORDER;
+  }
+  if (errCode == OK)
+    fnSeqNum = (fnSeqNum + 1) % 2;
+  return errCode;
+}
+
+int PiksiControlTask::dispatch_sbdix()
+{
+  int errCode = -1;
+  switch (fnSeqNum)
+  {
+  case 0:
+    errCode = quake.query_sbdix_1();
+    break;
+  case 1:
+    errCode = quake.get_sbdix();
+    break;
+  default:
+    return WRONG_FN_ORDER;
+  }
+  if (errCode == OK)
+    fnSeqNum = (fnSeqNum + 1) % 2;
+  return errCode;
+}
+
+int PiksiControlTask::dispatch_config()
+{
+  int errCode = -1;
+  switch (fnSeqNum)
+  {
+  case 0:
+    errCode = quake.query_config_1();
+    break;
+  case 1:
+    errCode = quake.query_config_2();
+    break;
+  case 2:
+    errCode = quake.query_config_3();
+    break;
+  case 3:
+    errCode = quake.get_config();
+    break;
+  default:
+    return WRONG_FN_ORDER;
+  }
+  if (errCode == OK)
+    fnSeqNum = (fnSeqNum + 1) % 4;
+  return errCode;
+}
+
+int PiksiControlTask::dispatch_is_functional()
+{
+  int errCode = -1;
+  switch (fnSeqNum)
+  {
+  case 0:
+    errCode = quake.get_is_functional();
+    break;
+  case 1:
+    errCode = quake.get_is_functional();
+    break;
+  default:
+    return WRONG_FN_ORDER;
+  }
+  if (errCode == OK)
+    fnSeqNum = (fnSeqNum + 1) % 2;
+  return errCode;
+}
---
+++ src/FCCode/PiksiControlTask.hpp
@@ -0,0 +1,79 @@
+#pragma once
+
+#include <ControlTask.hpp>
+//#include "../../lib/Drivers/QLocate.hpp"
+#include <Piksi.hpp>
+
+/**
+ * QLocate driver states
+ * QLocate is initialized to IDLE in setup()
+ * Operations are split into command sequences since many operations require
+ * multiple commands. Also, we want to account for differences in timing
+ * between sending commands and reading expected responses.
+ */
+static constexpr int IDLE = 0;
+static constexpr int SBDWB = 1;         // SBDWB operation
+static constexpr int SBDRB = 2;         // SBDRB operation
+static constexpr int SBDIX = 3;         // SBDIX operation
+static constexpr int CONFIG = 4;        // Config operation
+static constexpr int IS_FUNCTIONAL = 5; // Is_Functional operation
+
+class PiksiControlTask : public ControlTask<int>
+{
+public:
+  // TODO: decide how we want to initialize the Driver
+  #ifndef DESKTOP
+  PiksiControlTask(StateFieldRegistry &registry) : ControlTask<int>(registry),
+                                                   quake("Piksi", &Serial3,Devices::QLocate::DEFAULT_NR_PIN, Devices::QLocate::DEFAULT_TIMEOUT),
+                                                   currentState(IDLE),
+                                                   fnSeqNum(0),
+                                                   szMsg(nullptr),
+                                                   len(0){}
+  #else
+  PiksiControlTask(StateFieldRegistry &registry) : ControlTask<int>(registry),
+                                                   quake(),
+                                                   currentState(IDLE),
+                                                   fnSeqNum(0),
+                                                   szMsg(nullptr),
+                                                   len(0) {}
+  #endif
+  /** 
+   * execute is overriden from ControlTask 
+   * Calling execute() when the state is IDLE generates no effects. 
+  */
+  int execute();
+
+  /**
+   * Request to change the state of the driver. 
+   * This may only be done if the current state is IDLE or if the requested_state
+   * is CONFIG.
+   * Returns true if the state is succsfully changed to the requested_state.
+   * Returns false otherwise. 
+   */
+  bool request_state(int requested_state);
+
+  /** Returns the current state of the Piksi */
+  int get_current_state() const;
+
+  /** Returns the current function number (for testing purposes) */
+  size_t get_current_fn_number() const;
+
+  /**
+   * Set the message that Piksi should downlink.  */
+  void set_downlink_msg(const char *, size_t);
+
+private:
+  // all dispatch_x functions return 0 on success and an error code otherwise
+  int dispatch_sbdwb();
+  int dispatch_sbdrb();
+  int dispatch_sbdix();
+  int dispatch_config();
+  int dispatch_is_functional();
+  Devices::Piksi piksi;
+  // Piksi should never be in both IDLE and fnSeqNum != 0
+  int currentState; // the state of the Piksi
+  int fnSeqNum;     // the sequence we are on
+
+  const char *szMsg; // the message to downlink
+  size_t len;        // length of the message to downlink
+};
