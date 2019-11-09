d3072734ff98b2e29c147e4538782720a7cb29d8
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2019-11-08 23:34:05 -0500

    Matrix-ify Travis build

--- tools/generate_coverage.sh
+++ tools/generate_coverage.sh
@@ -1,3 +1,5 @@
+# Script to locally generate coverage reports.
+
 lcov --directory . --capture --output-file coverage.info
 lcov --remove coverage.info '*/test/*/*' '*/usr/*' '*/lib/Devices*' '*/lib/Drivers*' '*/lib/libsbp*' '*/lib/rwmutex*' '*/ArduinoJson/*' '*/.pio/*' '*/.platformio/*' --output-file coverage.info
 lcov --list coverage.info
