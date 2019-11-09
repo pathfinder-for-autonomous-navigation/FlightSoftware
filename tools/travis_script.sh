7b9d9f478f7011bd53978c7db06493e561073a2a
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2019-11-08 23:16:00 -0500

    Add automated flight software testing via remote Teensy CI

--- tools/travis_script.sh
+++ tools/travis_script.sh
@@ -32,5 +32,8 @@ platformio run -e teensy_36_test_quake_network
 # We also need this step to run last so that code coverage is available.
 platformio test -e native_ci
 
+# Run tests on a remotely connected Teensy
+platformio remote test -e teensy_ci -v
+
 # Check for memory mismanagement
 valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose .pio/build/native_ci/program
