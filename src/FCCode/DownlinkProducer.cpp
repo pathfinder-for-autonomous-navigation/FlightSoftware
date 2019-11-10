c78b1e4f50252f64a68df7836d34489772aa1df9
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2019-11-09 22:03:55 -0500

    Progress so far

--- src/FCCode/DownlinkProducer.cpp
+++ src/FCCode/DownlinkProducer.cpp
@@ -1,16 +1,26 @@
 #include "DownlinkProducer.hpp"
+#include <cobs.h>
+#ifdef DESKTOP
+    #include <cassert>
+#endif
 
 DownlinkProducer::DownlinkProducer(StateFieldRegistry& r,
-    std::vector<Flow>& _flows) : ControlTask<void>(r),
-                                 flows(_flows) {}
+    const std::vector<FlowData>& flow_data) : ControlTask<void>(r)
+{
+    unsigned char num_flows = flow_data.size();
+    for (auto const& flow : flow_data) {
+        Flow f(r, flow, num_flows);
+        flows.push_back(f);
+    }
+}
 
 void DownlinkProducer::execute() {
     // If any flow counters are equal to their flow rate,
     // produce a packet for those flows.
-    for(unsigned char i = 0; i < flows.size(); i++) {
+    for(size_t i = 0; i < flows.size(); i++) {
         Flow& f = flows[i];
         if (f.flow_counter == f.flow_rate) {
-            f.produce_flow_packet();
+            f.produce_flow_packet(nullptr);
             f.flow_counter = 0;
         }
         f.flow_counter++;
@@ -18,25 +28,72 @@ void DownlinkProducer::execute() {
 }
 
 DownlinkProducer::Flow::Flow(
-    const std::vector<std::shared_ptr<ReadableStateFieldBase>>& _field_list,
-    const unsigned int _flow_rate,
-    const unsigned int _flow_counter) : field_list(_field_list),
-                                        flow_rate(_flow_rate),
-                                        flow_counter(_flow_counter)
+    const StateFieldRegistry& r,
+    const FlowData& flow_data,
+    const unsigned char _num_flows) : flow_id(0, flow_data.flow_id),
+                                      flow_rate(flow_data.flow_rate),
+                                      flow_counter(0)
 {
-    flow_packet_size = 0;
-    flow_packet_size += 1; // Flow packet ID
+    for(auto const& field_name : flow_data.field_list) {
+        auto const field_ptr = r.find_readable_field(field_name);
+        #ifdef DESKTOP
+        assert(field_ptr);
+        #endif
+        field_list.push_back(field_ptr);
+    }
+
     flow_packet_size += 4; // Control cycle # of when data was collected.
 
     // Get bitcount of all fields in the flow
     unsigned int flow_packet_body_bitsize;
+    flow_packet_body_bitsize += flow_id.bitsize();
     for(auto const& field: field_list) {
-        flow_packet_body_bitsize += field->get_bitsize();
+        flow_packet_body_bitsize += field->get_bit_array().size();
     }
     unsigned int flow_packet_body_bytesize = (flow_packet_body_bitsize + 7) / 8;
     flow_packet_size += flow_packet_body_bytesize;
 
-    flow_packet_size = (flow_packet_size + 253) / 254; // Account for COBS encoding
+    #ifdef DESKTOP
+    assert(flow_packet_size <= 70);
+    #endif
 }
 
-void DownlinkProducer::Flow::produce_flow_packet() {}
+int DownlinkProducer::Flow::produce_flow_packet(char* packet_dest) {
+    unsigned int f_it = 0; // Iterates over the packet at the bit level
+
+    // Add flow ID to packet body
+    auto const& flow_id_bits = flow_id.get_bit_array();
+    flow_id_bits.to_string(packet_dest, f_it);
+    f_it += flow_id_bits.size();
+
+    // Add flow values to packet body
+    for(auto const& field: field_list) {
+        field->serialize();
+        auto const& field_bits = field->get_bit_array();
+        field_bits.to_string(packet_dest, f_it);
+        f_it += field_bits.size();
+    }
+
+    // Byte-align the flow body by setting the remaining bits to zero.
+    while(f_it % 8 != 0) {
+        bit_array::modify_bit(packet_dest[f_it / 8], f_it, 0);
+    }
+
+    // Add control cycle count to flow packet
+    f_it = f_it / 8; // Iterate over the byte level now
+    unsigned int control_cycle_no = 2;
+    memcpy(packet_dest + f_it, (char*)(&control_cycle_no), sizeof(unsigned int));
+
+    // COBS-encode the packet
+    auto cobs_encode_result = cobs_encode(packet_dest, flow_packet_size,
+                                packet_dest, flow_packet_size + 1);
+    if (cobs_encode_result.status == COBSR_ENCODE_OK) {
+        return cobs_encode_result.out_len;
+    }
+    else if (cobs_encode_result.status == COBSR_ENCODE_OUT_BUFFER_OVERFLOW) {
+        return -1;
+    }
+    else if (cobs_encode_result.status == COBSR_ENCODE_NULL_POINTER) {
+        return -2;
+    }
+}
--- src/FCCode/DownlinkProducer.hpp
+++ src/FCCode/DownlinkProducer.hpp
@@ -5,11 +5,54 @@
 #include <static_buffers.hpp>
 
 class DownlinkProducer : public ControlTask<void> {
-   protected:
+   public:
+    /**
+     * @brief Flow data object, used in order to specify the
+     * flow rate, the fields going into a flow, and the flow
+     * ID.
+     * 
+     * We can create a static list of these and use it to initialize the
+     * actual Flow object, which creates pointers to state fields and 
+     * contains a function to automatically generate a flow packet.
+     */
+    struct FlowData {
+        unsigned char flow_id;
+        std::vector<std::string> field_list;
+        unsigned char flow_rate;
+    };
+
+    DownlinkProducer(StateFieldRegistry& registry, std::vector<FlowData>& flow_data);
+    void execute() override;
+
+  protected:
+    /**
+     * @brief Flow object, which controls the construction and state of a telemetry
+     * flow:
+     * - Ensures a flow packet doesn't span more than 70 bytes.
+     * - Ensures all fields in in the flow packet are available in the state
+     * registry.
+     * - The flow counter tracks whether or not a flow packet should be produced
+     *   on the current control cycle.
+     */
     struct Flow {
-        Flow(const std::vector<std::shared_ptr<ReadableStateFieldBase>>& _field_list,
-             const unsigned int _flow_rate,
-             const unsigned int _flow_counter);
+
+        /**
+         * @brief Construct a new Flow object
+         * 
+         * Constructs the flow from the list of state fields
+         * and computes the size of the flow.
+         * 
+         * @param r          State field registry object.
+         * @param flow_data  Data about the flow.
+         * @param _num_flows Total number of flows. This is used to
+         *                   create the flow ID #
+         */
+        Flow(const StateFieldRegistry& r,
+             const FlowData& flow_data,
+             const unsigned char _num_flows);
+
+        //! Flow ID #
+        Serializer<unsigned char> flow_id;
 
         //! List of fields within the flow
         std::vector<std::shared_ptr<ReadableStateFieldBase>> field_list;
@@ -19,7 +62,7 @@ class DownlinkProducer : public ControlTask<void> {
         
         //! Counts up from the last time a flow packet of this kind of flow
         //! was produced. Once a flow packet of this kind of flow is produced,
-        //! the counter is reset. 
+        //! the counter is reset.
         //!
         //! For example, let's say the flow rate is 5 and we just produced a packet.
         //! This flow counter will increment from 0 to 1,2,3,4,5 on each control cycle.
@@ -27,21 +70,28 @@ class DownlinkProducer : public ControlTask<void> {
         //! and call produce_flow_packet on this flow.
         unsigned int flow_counter;
         
-        //! Number of characters in the entire flow packet, including COBS encoding and
-        //! the packet header.
+        //! Number of characters in the entire flow packet, including the packet header,
+        //! but excluding COBS encoding.
         //! This is computed upon construction of the flow.
-        size_t flow_packet_size;
+        size_t flow_packet_size = 0;
 
-        circular_stack<char*, >
-
-        //! Produces the flow packet 
-        void produce_flow_packet();
+        /**
+         * @brief Produces the serialized float packet, writing it to a provided character
+         * array.
+         * 
+         * @param dest Destination character array.
+         * 
+         * @return One of three possibilities:
+         *         - If creation and COBS encoding of the packet was successful, return
+         *           the number of bytes written to the destination.
+         *         - If the destination pointer was invalid (null), return -1.
+         *         - If the destination buffer wasn't large enough to store the packet,
+         *           return -2.
+         */
+        int produce_flow_packet(char* dest);
     };
-    std::vector<Flow>& flows;
 
-   public:
-    DownlinkProducer(StateFieldRegistry& registry, std::vector<Flow>& flows);
-    void execute() override;
+    std::vector<Flow>& flows;
 };
 
 #endif
