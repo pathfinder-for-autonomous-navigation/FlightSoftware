1af1213eb26b0779ea249c0b36a2b7f1e683a011
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2019-11-10 23:42:24 -0500

    Add build flags

--- tools/run_desktop_tests.sh
+++ tools/run_desktop_tests.sh
@@ -6,7 +6,7 @@ python -m unittest -v test_dummy_fsw
 
 # Verify compilation and unit tests for optimized native environment
 platformio run -e native
-platformio test -e native
+platformio test -e native -v
 
 # Run tests for non-optimized native environment. These are
 # identical to the tests for the optimized environment,
@@ -14,7 +14,7 @@ platformio test -e native
 # likely to fail the build sequence.)
 #
 # We also need this step to run last so that code coverage is available.
-platformio test -e native_ci
+platformio test -e native_ci -v
 
 # Check for memory mismanagement
 valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose .pio/build/native_ci/program
