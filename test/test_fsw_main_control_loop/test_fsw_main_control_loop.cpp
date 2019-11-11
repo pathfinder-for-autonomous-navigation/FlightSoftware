7425daa490c3b5761673dd1cf40687151b9b1c53
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2019-11-11 10:50:29 -0500

    Add some basic testing

---
+++ test/test_fsw_main_control_loop/test_fsw_main_control_loop.cpp
@@ -0,0 +1,41 @@
+#include <ControlTask.hpp>
+#include "../../src/FCCode/MainControlLoop.cpp"
+#include <unity.h>
+
+// Ensure that main control loop initializes properly and
+// doesn't segfault upon execution a few times.
+
+void test_task_initialization() {
+    StateFieldRegistry registry;
+    MainControlLoop fcp(registry);
+}
+
+void test_task_execute() {
+    StateFieldRegistry registry;
+    MainControlLoop fcp(registry);
+    for(int i = 0; i < 5; i++) {
+        task.execute();
+    }
+}
+
+int test_main_control_loop() {
+    UNITY_BEGIN();
+    RUN_TEST(test_task_initialization);
+    RUN_TEST(test_task_execute);
+    return UNITY_END();
+}
+
+#ifdef DESKTOP
+int main() {
+    return test_main_control_loop();
+}
+#else
+#include <Arduino.h>
+void setup() {
+    delay(2000);
+    Serial.begin(9600);
+    test_main_control_loop();
+}
+
+void loop() {}
+#endif
