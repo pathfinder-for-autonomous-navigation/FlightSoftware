4103334b0c424f5e46ad2d72e69ecfb52a57d4a5
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2019-11-11 10:48:06 -0500

    Removing nonsense

--- src/FCCode/DownlinkProducer.cpp
+++ src/FCCode/DownlinkProducer.cpp
@@ -5,63 +5,47 @@
 #endif
 
 DownlinkProducer::DownlinkProducer(StateFieldRegistry& r,
-    const std::vector<FlowData>& flow_data) : ControlTask<void>(r)
+    const std::vector<FlowData>& flow_data) : ControlTask<void>(r),
+    snapshot_ptr_f(), snapshot_size_bytes_f()
 {
-    unsigned char num_flows = flow_data.size();
+    // Add snapshot fields to the registry
+    r.add_field(snapshot_ptr_f);
+    r.add_field(snapshot_size_bytes_f);
+
+    // Create flow objects out of the flow data.
+    const size_t num_flows = flow_data.size();
+    size_t downlink_max_size_bits = 0;
     for (const FlowData& flow : flow_data) {
         Flow f(r, flow, num_flows);
-        
-        FlowGroup& group = flow_groups[flow.group_id];
-        group.group_id = flow.group_id;
-        group.flows.push_back(f);
+        flows[flow.id] = f;
+        downlink_max_size_bits += f.get_packet_size();
     }
 
-    for(auto& flow_group_it : flow_groups) {
-        FlowGroup& group = flow_group_it.second;
-        #ifdef DESKTOP
-            assert(group.get_downlink_size() <= 70);
-        #endif
-    }
+    // Create snapshot buffer.
+    const size_t downlink_max_size_bytes = (downlink_max_size_bits + 7) / 8;
+    snapshot = new char[downlink_max_size_bytes];
 }
 
 void DownlinkProducer::execute() {
-    // If any flow counters are equal to their flow rate,
-    // produce a packet for those flows.
-    for(auto& it : flow_groups) {
-        FlowGroup& flow_group = it.second;
-        for(Flow& f : flow_group.flows) {
-            if (f.counter == f.rate) {
-                f.produce_flow_packet(nullptr);
-                f.counter = 0;
-            }
-            f.counter++;
-        }
+    size_t downlink_frame_offset = 0;
+    for(auto& it : flows) {
+        // Add a flow one at a time to the snapshot, taking
+        // care to add a downlink packet delimeter every now
+        // and then.
     }
 }
 
-size_t DownlinkProducer::FlowGroup::get_downlink_size() const {
-    size_t downlink_size = 0;
-    downlink_size += 4; // Control cycle count
-    for(const Flow& flow : flows) {
-        downlink_size += flow.packet_size;
-        downlink_size += 1; // Packet delimiter
-    }
-    if (flows.size() > 0) downlink_size -= 1; // Extra packet delimeter
-    return downlink_size;
-}
-
-void DownlinkProducer::FlowGroup::produce_downlink_packet(unsigned char* dest) {
-    size_t dest_it = 0; // Pointer to iterate through the size of the 
+DownlinkProducer::~DownlinkProducer() {
+    delete[] snapshot;
 }
 
 DownlinkProducer::Flow::Flow(
     const StateFieldRegistry& r,
     const FlowData& flow_data,
-    const unsigned char _num_flows) : flow_id_sr(0, flow_data.flow_id),
-                                      group_id(flow_data.group_id),
-                                      flow_rate(flow_data.flow_rate),
-                                      flow_counter(0)
+    const size_t num_flows) : flow_id_sr(0, num_flows)
 {
+    flow_id_sr.serialize(flow_data.id);
+
     for(auto const& field_name : flow_data.field_list) {
         auto const field_ptr = r.find_readable_field(field_name);
         #ifdef DESKTOP
@@ -89,30 +73,18 @@ size_t DownlinkProducer::Flow::get_packet_size() const {
     return packet_size;
 }
 
-void DownlinkProducer::Flow::produce_flow_packet(unsigned char* packet_dest) {
-    unsigned int f_it = 0; // Iterates over the packet at the bit level
-
+void DownlinkProducer::Flow::produce_flow_packet(unsigned char* packet_dest,
+                                                 size_t offset) {
     // Add flow ID to packet body
-    auto const& flow_id_bits = flow_id.get_bit_array();
-    flow_id_bits.to_string(packet_dest, f_it);
-    f_it += flow_id_bits.size();
+    auto const& flow_id_bits = flow_id_sr.get_bit_array();
+    flow_id_bits.to_string(packet_dest, offset);
+    offset += flow_id_bits.size();
 
     // Add flow values to packet body
     for(auto const& field: field_list) {
         field->serialize();
         auto const& field_bits = field->get_bit_array();
-        field_bits.to_string(packet_dest, f_it);
-        f_it += field_bits.size();
+        field_bits.to_string(packet_dest, offset);
+        offset += field_bits.size();
     }
-
-    // Byte-align the flow body by setting the remaining bits to zero.
-    while(f_it % 8 != 0) {
-        bit_array::modify_bit(packet_dest[f_it / 8], f_it, 0);
-    }
-
-    // COBS-encode the packet. The encoding will never fail since
-    // the destination buffer is larger than the source buffer and
-    // the destination buffer will never be null.
-    cobs_encode(packet_dest, flow_packet_size, packet_dest,
-                flow_packet_size + 1);
 }
--- src/FCCode/DownlinkProducer.hpp
+++ src/FCCode/DownlinkProducer.hpp
@@ -2,7 +2,6 @@
 #define FLOW_MANAGER_HPP_
 
 #include <ControlTask.hpp>
-#include <static_buffers.hpp>
 
 class DownlinkProducer : public ControlTask<void> {
    public:
@@ -19,10 +18,7 @@ class DownlinkProducer : public ControlTask<void> {
      * contains a function to automatically generate a flow packet.
      */
     struct FlowData {
-        unsigned char flow_id;
-        unsigned char group_id;
-        unsigned char flow_rate;
-        unsigned int packet_cache_size;
+        unsigned char id;
         std::vector<std::string> field_list;
     };
 
@@ -40,7 +36,17 @@ class DownlinkProducer : public ControlTask<void> {
      */
     void execute() override;
 
+    ~DownlinkProducer();
+
   protected:
+    /**
+     * @brief Fields used by the Quake manager to know from where to copy a downlink
+     * snapshot, and the length of the snapshot.
+     */
+    char* snapshot = nullptr;
+    InternalStateField<char*> snapshot_ptr_f;
+    InternalStateField<size_t> snapshot_size_bytes_f;
+
     /**
      * @brief Flow object, which controls the construction and state of a telemetry
      * flow:
@@ -59,71 +65,37 @@ class DownlinkProducer : public ControlTask<void> {
          * 
          * @param r          State field registry object.
          * @param flow_data  Data about the flow.
-         * @param _num_flows Total number of flows. This is used to
-         *                   create the flow ID #
+         * @param num_flows  Total number of flows. This is used to
+         *                   create the flow ID # serializer
          */
         Flow(const StateFieldRegistry& r,
              const FlowData& flow_data,
-             const unsigned char _num_flows);
+             const unsigned char num_flows);
 
         //! Flow ID #
         Serializer<unsigned char> flow_id_sr;
-        unsigned char group_id;
 
         //! List of fields within the flow
         std::vector<std::shared_ptr<ReadableStateFieldBase>> field_list;
-
-        //! Rate of flow
-        unsigned int rate;
-        
-        //! Counts up from the last time a flow packet of this kind of flow
-        //! was produced. Once a flow packet of this kind of flow is produced,
-        //! the counter is reset.
-        //!
-        //! For example, let's say the flow rate is 5 and we just produced a packet.
-        //! This flow counter will increment from 0 to 1,2,3,4,5 on each control cycle.
-        //! After reaching 5, execute will reset it back to zero on the next control cycle
-        //! and call produce_flow_packet on this flow.
-        unsigned int counter;
         
         //! Number of characters in the entire flow packet, including the packet header,
         //! but excluding COBS encoding.
         size_t get_packet_size() const;
 
-        circular_stack<char*, flow_cache_size> flow_packets;
-
         /**
          * @brief Produces the serialized float packet, writing it to a provided character
-         * array.
+         * array at a given bit offset from the beginning of the array.
          * 
          * @param dest Destination character array.
+         * @param offset Bit offset.
          */
-        void produce_flow_packet(unsigned char* dest);
-    };
-
-    struct FlowGroup {
-        unsigned int group_id;
-        std::vector<Flow> flows;
-        circular_stack<char*, group_cache_size> downlink_packets;
-
-        /**
-         * @brief Compute the downlink size in bytes.
-         */
-        size_t get_downlink_size() const;
-
-        /**
-         * @brief Produces a downlink packet
-         * 
-         * @param dest Destination character array, writing it to a provided character
-         * array
-         */
-        void produce_downlink_packet(char* dest);
+        void produce_flow_packet(unsigned char* dest, size_t offset);
     };
 
     /**
      * @brief Actual flow data.
      */
-    std::map<unsigned char, FlowGroup>& flow_groups;
+    std::map<unsigned char, Flow>& flows;
 };
 
 #endif
