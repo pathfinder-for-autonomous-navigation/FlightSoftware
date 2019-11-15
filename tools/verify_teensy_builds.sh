307aac5eb252f91294a0e960347a4f5c27550f56
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2019-11-15 17:53:05 -0500

    Remove unnecessary Teensy building

--- tools/verify_teensy_builds.sh
+++ tools/verify_teensy_builds.sh
@@ -7,11 +7,3 @@ cppcheck test src lib/utils lib/Drivers lib/Devices lib/rwmutex
 platformio run -e teensy35_cli_hootl
 platformio run -e teensy36_cli_hootl
 platformio run -e teensy_cli_preflight
-
-# Compile hardware functional tests
-platformio run -e teensy_35_test_quake
-platformio run -e teensy_35_test_quake_no_network
-platformio run -e teensy_35_test_quake_network
-platformio run -e teensy_36_test_quake
-platformio run -e teensy_36_test_quake_no_network
-platformio run -e teensy_36_test_quake_network
