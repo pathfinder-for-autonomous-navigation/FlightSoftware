dc0e466f52a56dbf2f51a1247f78c8866e100548
Fatima Yousuf <fatimayousuf@dhcp-clara-2519.redrover.cornell.edu>
2019-11-16 17:37:25 -0500

    tests now pass

---
+++ test/test_fsw_gomspace/test_fsw_gomspace.cpp
@@ -0,0 +1,430 @@
+#include "../StateFieldRegistryMock.hpp"
+#include "../../src/FCCode/GomspaceController.hpp"
+
+#include <unity.h>
+#include <iostream>
+
+class TestFixture {
+  public:
+    StateFieldRegistryMock registry;
+
+    Devices::Gomspace::eps_hk_t hk = {
+        {1,2,3},
+        4,
+        {5,6,7},
+        8,
+        9,
+        10,
+        {11,12,13,14,15,16},
+        {17,18,19,20,21,22,23,24},
+        {25,26,27,28,29,30,31,32},
+        {33,34,35,36,37,38,39,40},
+        {41,42,43,44,45,46},
+        47,
+        48,
+        {49,50},
+        51,
+        52,
+        {53,54},
+        55,
+        {56,57,58,59,60,61},
+        62,
+        63,
+        64,
+        65
+    };
+    Devices::Gomspace::eps_config_t config;
+    Devices::Gomspace::eps_config2_t config2;
+    Devices::Gomspace gs;
+
+    std::unique_ptr<GomspaceController> gs_controller;
+
+    ReadableStateField<unsigned int>* vboost1_fp;
+    ReadableStateField<unsigned int>* vboost2_fp;
+    ReadableStateField<unsigned int>* vboost3_fp;
+
+    ReadableStateField<unsigned int>* vbatt_fp;
+
+    ReadableStateField<unsigned int>* curin1_fp;
+    ReadableStateField<unsigned int>* curin2_fp;
+    ReadableStateField<unsigned int>* curin3_fp;
+
+    ReadableStateField<unsigned int>* cursun_fp;
+
+    ReadableStateField<unsigned int>* cursys_fp;
+
+    ReadableStateField<unsigned int>* reserved1_fp;
+
+    ReadableStateField<unsigned int>* curout1_fp;
+    ReadableStateField<unsigned int>* curout2_fp;
+    ReadableStateField<unsigned int>* curout3_fp;
+    ReadableStateField<unsigned int>* curout4_fp;
+    ReadableStateField<unsigned int>* curout5_fp;
+    ReadableStateField<unsigned int>* curout6_fp;
+
+    ReadableStateField<unsigned int>* output1_fp;
+    ReadableStateField<unsigned int>* output2_fp;
+    ReadableStateField<unsigned int>* output3_fp;
+    ReadableStateField<unsigned int>* output4_fp;
+    ReadableStateField<unsigned int>* output5_fp;
+    ReadableStateField<unsigned int>* output6_fp;
+    ReadableStateField<unsigned int>* output7_fp;
+    ReadableStateField<unsigned int>* output8_fp;
+
+    ReadableStateField<unsigned int>* output_on_delta1_fp;
+    ReadableStateField<unsigned int>* output_on_delta2_fp;
+    ReadableStateField<unsigned int>* output_on_delta3_fp;
+    ReadableStateField<unsigned int>* output_on_delta4_fp;
+    ReadableStateField<unsigned int>* output_on_delta5_fp;
+    ReadableStateField<unsigned int>* output_on_delta6_fp;
+    ReadableStateField<unsigned int>* output_on_delta7_fp;
+    ReadableStateField<unsigned int>* output_on_delta8_fp;
+
+    ReadableStateField<unsigned int>* output_off_delta1_fp;
+    ReadableStateField<unsigned int>* output_off_delta2_fp;
+    ReadableStateField<unsigned int>* output_off_delta3_fp;
+    ReadableStateField<unsigned int>* output_off_delta4_fp;
+    ReadableStateField<unsigned int>* output_off_delta5_fp;
+    ReadableStateField<unsigned int>* output_off_delta6_fp;
+    ReadableStateField<unsigned int>* output_off_delta7_fp;
+    ReadableStateField<unsigned int>* output_off_delta8_fp;
+
+    ReadableStateField<unsigned int>* latchup1_fp;
+    ReadableStateField<unsigned int>* latchup2_fp;
+    ReadableStateField<unsigned int>* latchup3_fp;
+    ReadableStateField<unsigned int>* latchup4_fp;
+    ReadableStateField<unsigned int>* latchup5_fp;
+    ReadableStateField<unsigned int>* latchup6_fp;
+
+    ReadableStateField<unsigned int>* wdt_i2c_time_left_fp;
+
+    ReadableStateField<unsigned int>* wdt_gnd_time_left_fp;
+
+    ReadableStateField<unsigned int>* wdt_csp_pings_left1_fp;
+    ReadableStateField<unsigned int>* wdt_csp_pings_left2_fp;
+
+    ReadableStateField<unsigned int>* counter_wdt_i2c_fp;
+
+    ReadableStateField<unsigned int>* counter_wdt_gnd_fp;
+
+    ReadableStateField<unsigned int>* counter_wdt_csp1_fp;
+    ReadableStateField<unsigned int>* counter_wdt_csp2_fp;
+
+    ReadableStateField<unsigned int>* counter_boot_fp;
+
+    ReadableStateField<unsigned int>* temp1_fp;
+    ReadableStateField<unsigned int>* temp2_fp;
+    ReadableStateField<unsigned int>* temp3_fp;
+    ReadableStateField<unsigned int>* temp4_fp;
+    ReadableStateField<unsigned int>* temp5_fp;
+    ReadableStateField<unsigned int>* temp6_fp;
+
+    ReadableStateField<unsigned int>* bootcause_fp;
+
+    ReadableStateField<unsigned int>* battmode_fp;
+
+    ReadableStateField<unsigned int>* pptmode_fp;
+
+    ReadableStateField<unsigned int>* reserved2_fp;
+
+    TestFixture() : registry(), gs(&hk, &config, &config2) {
+        gs_controller = std::make_unique<GomspaceController>(registry, 0, gs);
+
+        vboost1_fp = registry.find_readable_field_t<unsigned int>("gomspace.vboost.output1");
+        vboost2_fp = registry.find_readable_field_t<unsigned int>("gomspace.vboost.output2");
+        vboost3_fp = registry.find_readable_field_t<unsigned int>("gomspace.vboost.output3");
+
+        vbatt_fp = registry.find_readable_field_t<unsigned int>("gomspace.vbatt");
+
+        curin1_fp = registry.find_readable_field_t<unsigned int>("gomspace.curin.output1");
+        curin2_fp = registry.find_readable_field_t<unsigned int>("gomspace.curin.output2");
+        curin3_fp = registry.find_readable_field_t<unsigned int>("gomspace.curin.output3");
+
+        cursun_fp = registry.find_readable_field_t<unsigned int>("gomspace.cursun");
+
+        cursys_fp = registry.find_readable_field_t<unsigned int>("gomspace.cursys");
+
+        reserved1_fp = registry.find_readable_field_t<unsigned int>("gomspace.reserved1");
+
+        curout1_fp = registry.find_readable_field_t<unsigned int>("gomspace.curout.output1");
+        curout2_fp = registry.find_readable_field_t<unsigned int>("gomspace.curout.output2");
+        curout3_fp = registry.find_readable_field_t<unsigned int>("gomspace.curout.output3");
+        curout4_fp = registry.find_readable_field_t<unsigned int>("gomspace.curout.output4");
+        curout5_fp = registry.find_readable_field_t<unsigned int>("gomspace.curout.output5");
+        curout6_fp = registry.find_readable_field_t<unsigned int>("gomspace.curout.output6");
+
+        output1_fp = registry.find_readable_field_t<unsigned int>("gomspace.output.output1");
+        output2_fp = registry.find_readable_field_t<unsigned int>("gomspace.output.output2");
+        output3_fp = registry.find_readable_field_t<unsigned int>("gomspace.output.output3");
+        output4_fp = registry.find_readable_field_t<unsigned int>("gomspace.output.output4");
+        output5_fp = registry.find_readable_field_t<unsigned int>("gomspace.output.output5");
+        output6_fp = registry.find_readable_field_t<unsigned int>("gomspace.output.output6");
+        output7_fp = registry.find_readable_field_t<unsigned int>("gomspace.output.output7");
+        output8_fp = registry.find_readable_field_t<unsigned int>("gomspace.output.output8");
+
+        output_on_delta1_fp = registry.find_readable_field_t<unsigned int>("gomspace.output_on_delta.output1");
+        output_on_delta2_fp = registry.find_readable_field_t<unsigned int>("gomspace.output_on_delta.output2");
+        output_on_delta3_fp = registry.find_readable_field_t<unsigned int>("gomspace.output_on_delta.output3");
+        output_on_delta4_fp = registry.find_readable_field_t<unsigned int>("gomspace.output_on_delta.output4");
+        output_on_delta5_fp = registry.find_readable_field_t<unsigned int>("gomspace.output_on_delta.output5");
+        output_on_delta6_fp = registry.find_readable_field_t<unsigned int>("gomspace.output_on_delta.output6");
+        output_on_delta7_fp = registry.find_readable_field_t<unsigned int>("gomspace.output_on_delta.output7");
+        output_on_delta8_fp = registry.find_readable_field_t<unsigned int>("gomspace.output_on_delta.output8");
+
+        output_off_delta1_fp = registry.find_readable_field_t<unsigned int>("gomspace.output_off_delta.output1");
+        output_off_delta2_fp = registry.find_readable_field_t<unsigned int>("gomspace.output_off_delta.output2");
+        output_off_delta3_fp = registry.find_readable_field_t<unsigned int>("gomspace.output_off_delta.output3");
+        output_off_delta4_fp = registry.find_readable_field_t<unsigned int>("gomspace.output_off_delta.output4");
+        output_off_delta5_fp = registry.find_readable_field_t<unsigned int>("gomspace.output_off_delta.output5");
+        output_off_delta6_fp = registry.find_readable_field_t<unsigned int>("gomspace.output_off_delta.output6");
+        output_off_delta7_fp = registry.find_readable_field_t<unsigned int>("gomspace.output_off_delta.output7");
+        output_off_delta8_fp = registry.find_readable_field_t<unsigned int>("gomspace.output_off_delta.output8");
+
+        latchup1_fp = registry.find_readable_field_t<unsigned int>("gomspace.latchup.output1");
+        latchup2_fp = registry.find_readable_field_t<unsigned int>("gomspace.latchup.output2");
+        latchup3_fp = registry.find_readable_field_t<unsigned int>("gomspace.latchup.output3");
+        latchup4_fp = registry.find_readable_field_t<unsigned int>("gomspace.latchup.output4");
+        latchup5_fp = registry.find_readable_field_t<unsigned int>("gomspace.latchup.output5");
+        latchup6_fp = registry.find_readable_field_t<unsigned int>("gomspace.latchup.output6");
+
+        wdt_i2c_time_left_fp = registry.find_readable_field_t<unsigned int>("gomspace.wdt_i2c_time_left");
+
+        wdt_gnd_time_left_fp = registry.find_readable_field_t<unsigned int>("gomspace.wdt_gnd_time_left");
+
+        wdt_csp_pings_left1_fp = registry.find_readable_field_t<unsigned int>("gomspace.wdt_csp_pings_left.output1");
+        wdt_csp_pings_left2_fp = registry.find_readable_field_t<unsigned int>("gomspace.wdt_csp_pings_left.output2");
+
+        counter_wdt_i2c_fp = registry.find_readable_field_t<unsigned int>("gomspace.counter_wdt_i2c");
+
+        counter_wdt_gnd_fp = registry.find_readable_field_t<unsigned int>("gomspace.counter_wdt_gnd");
+
+        counter_wdt_csp1_fp = registry.find_readable_field_t<unsigned int>("gomspace.counter_wdt_csp.output1");
+        counter_wdt_csp2_fp = registry.find_readable_field_t<unsigned int>("gomspace.counter_wdt_csp.output2");
+
+        counter_boot_fp = registry.find_readable_field_t<unsigned int>("gomspace.counter_boot");
+
+        temp1_fp = registry.find_readable_field_t<unsigned int>("gomspace.temp.output1");
+        temp2_fp = registry.find_readable_field_t<unsigned int>("gomspace.temp.output2");
+        temp3_fp = registry.find_readable_field_t<unsigned int>("gomspace.temp.output3");
+        temp4_fp = registry.find_readable_field_t<unsigned int>("gomspace.temp.output4");
+        temp5_fp = registry.find_readable_field_t<unsigned int>("gomspace.temp.output5");
+        temp6_fp = registry.find_readable_field_t<unsigned int>("gomspace.temp.output6");
+
+        bootcause_fp = registry.find_readable_field_t<unsigned int>("gomspace.bootcause");
+
+        battmode_fp = registry.find_readable_field_t<unsigned int>("gomspace.battmode");
+
+        pptmode_fp = registry.find_readable_field_t<unsigned int>("gomspace.pptmode");
+
+        reserved2_fp = registry.find_readable_field_t<unsigned int>("gomspace.reserved2");
+    }
+};
+
+void test_task_initialization() {
+    TestFixture tf;
+    //check that it is initialized correctly. tests pass
+    TEST_ASSERT_EQUAL(1, tf.gs.hk->vboost[0]);
+    TEST_ASSERT_EQUAL(2, tf.gs.hk->vboost[1]);
+    TEST_ASSERT_EQUAL(3, tf.gs.hk->vboost[2]);
+
+    TEST_ASSERT_EQUAL(4, tf.gs.hk->vbatt);
+
+    TEST_ASSERT_EQUAL(5, tf.gs.hk->curin[0]);
+    TEST_ASSERT_EQUAL(6, tf.gs.hk->curin[1]);
+    TEST_ASSERT_EQUAL(7, tf.gs.hk->curin[2]);
+
+    TEST_ASSERT_EQUAL(8, tf.gs.hk->cursun);
+
+    TEST_ASSERT_EQUAL(9, tf.gs.hk->cursys);
+
+    TEST_ASSERT_EQUAL(10, tf.gs.hk->reserved1);
+
+    TEST_ASSERT_EQUAL(11, tf.gs.hk->curout[0]);
+    TEST_ASSERT_EQUAL(12, tf.gs.hk->curout[1]);
+    TEST_ASSERT_EQUAL(13, tf.gs.hk->curout[2]);
+    TEST_ASSERT_EQUAL(14, tf.gs.hk->curout[3]);
+    TEST_ASSERT_EQUAL(15, tf.gs.hk->curout[4]);
+    TEST_ASSERT_EQUAL(16, tf.gs.hk->curout[5]);
+
+    TEST_ASSERT_EQUAL(17, tf.gs.hk->output[0]);
+    TEST_ASSERT_EQUAL(18, tf.gs.hk->output[1]);
+    TEST_ASSERT_EQUAL(19, tf.gs.hk->output[2]);
+    TEST_ASSERT_EQUAL(20, tf.gs.hk->output[3]);
+    TEST_ASSERT_EQUAL(21, tf.gs.hk->output[4]);
+    TEST_ASSERT_EQUAL(22, tf.gs.hk->output[5]);
+    TEST_ASSERT_EQUAL(23, tf.gs.hk->output[6]);
+    TEST_ASSERT_EQUAL(24, tf.gs.hk->output[7]);
+
+    TEST_ASSERT_EQUAL(25, tf.gs.hk->output_on_delta[0]);
+    TEST_ASSERT_EQUAL(26, tf.gs.hk->output_on_delta[1]);
+    TEST_ASSERT_EQUAL(27, tf.gs.hk->output_on_delta[2]);
+    TEST_ASSERT_EQUAL(28, tf.gs.hk->output_on_delta[3]);
+    TEST_ASSERT_EQUAL(29, tf.gs.hk->output_on_delta[4]);
+    TEST_ASSERT_EQUAL(30, tf.gs.hk->output_on_delta[5]);
+    TEST_ASSERT_EQUAL(31, tf.gs.hk->output_on_delta[6]);
+    TEST_ASSERT_EQUAL(32, tf.gs.hk->output_on_delta[7]);
+
+    TEST_ASSERT_EQUAL(33, tf.gs.hk->output_off_delta[0]);
+    TEST_ASSERT_EQUAL(34, tf.gs.hk->output_off_delta[1]);
+    TEST_ASSERT_EQUAL(35, tf.gs.hk->output_off_delta[2]);
+    TEST_ASSERT_EQUAL(36, tf.gs.hk->output_off_delta[3]);
+    TEST_ASSERT_EQUAL(37, tf.gs.hk->output_off_delta[4]);
+    TEST_ASSERT_EQUAL(38, tf.gs.hk->output_off_delta[5]);
+    TEST_ASSERT_EQUAL(39, tf.gs.hk->output_off_delta[6]);
+    TEST_ASSERT_EQUAL(40, tf.gs.hk->output_off_delta[7]);
+
+    TEST_ASSERT_EQUAL(41, tf.gs.hk->latchup[0]);
+    TEST_ASSERT_EQUAL(42, tf.gs.hk->latchup[1]);
+    TEST_ASSERT_EQUAL(43, tf.gs.hk->latchup[2]);
+    TEST_ASSERT_EQUAL(44, tf.gs.hk->latchup[3]);
+    TEST_ASSERT_EQUAL(45, tf.gs.hk->latchup[4]);
+    TEST_ASSERT_EQUAL(46, tf.gs.hk->latchup[5]);
+
+    TEST_ASSERT_EQUAL(47, tf.gs.hk->wdt_i2c_time_left);
+
+    TEST_ASSERT_EQUAL(48, tf.gs.hk->wdt_gnd_time_left);
+
+    TEST_ASSERT_EQUAL(49, tf.gs.hk->wdt_csp_pings_left[0]);
+    TEST_ASSERT_EQUAL(50, tf.gs.hk->wdt_csp_pings_left[1]);
+
+    TEST_ASSERT_EQUAL(51, tf.gs.hk->counter_wdt_i2c);
+
+    TEST_ASSERT_EQUAL(52, tf.gs.hk->counter_wdt_gnd);
+
+    TEST_ASSERT_EQUAL(53, tf.gs.hk->counter_wdt_csp[0]);
+    TEST_ASSERT_EQUAL(54, tf.gs.hk->counter_wdt_csp[1]);
+
+    TEST_ASSERT_EQUAL(55, tf.gs.hk->counter_boot);
+
+    TEST_ASSERT_EQUAL(56, tf.gs.hk->temp[0]);
+    TEST_ASSERT_EQUAL(57, tf.gs.hk->temp[1]);
+    TEST_ASSERT_EQUAL(58, tf.gs.hk->temp[2]);
+    TEST_ASSERT_EQUAL(59, tf.gs.hk->temp[3]);
+    TEST_ASSERT_EQUAL(60, tf.gs.hk->temp[4]);
+    TEST_ASSERT_EQUAL(61, tf.gs.hk->temp[5]);
+
+    TEST_ASSERT_EQUAL(62, tf.gs.hk->bootcause);
+    
+    TEST_ASSERT_EQUAL(63, tf.gs.hk->battmode);
+    
+    TEST_ASSERT_EQUAL(64, tf.gs.hk->pptmode);
+
+    TEST_ASSERT_EQUAL(65, tf.gs.hk->reserved2);
+}
+
+void test_task_execute() {
+    TestFixture tf;
+    tf.gs_controller->execute();
+
+    //check that the statefields are set to their respective value(s) in hk struct 
+    TEST_ASSERT_EQUAL(1, tf.vboost1_fp->get());
+    TEST_ASSERT_EQUAL(2, tf.vboost2_fp->get());
+    TEST_ASSERT_EQUAL(3, tf.vboost3_fp->get());
+
+    TEST_ASSERT_EQUAL(4, tf.vbatt_fp->get());
+
+    TEST_ASSERT_EQUAL(5, tf.curin1_fp->get());
+    TEST_ASSERT_EQUAL(6, tf.curin2_fp->get());
+    TEST_ASSERT_EQUAL(7, tf.curin3_fp->get());
+
+    TEST_ASSERT_EQUAL(8, tf.cursun_fp->get());
+
+    TEST_ASSERT_EQUAL(9, tf.cursys_fp->get());
+
+    TEST_ASSERT_EQUAL(10, tf.reserved1_fp->get());
+
+    TEST_ASSERT_EQUAL(11, tf.curout1_fp->get());
+    TEST_ASSERT_EQUAL(12, tf.curout2_fp->get());
+    TEST_ASSERT_EQUAL(13, tf.curout3_fp->get());
+    TEST_ASSERT_EQUAL(14, tf.curout4_fp->get());
+    TEST_ASSERT_EQUAL(15, tf.curout5_fp->get());
+    TEST_ASSERT_EQUAL(16, tf.curout6_fp->get());
+
+    TEST_ASSERT_EQUAL(17, tf.output1_fp->get());
+    TEST_ASSERT_EQUAL(18, tf.output2_fp->get());
+    TEST_ASSERT_EQUAL(19, tf.output3_fp->get());
+    TEST_ASSERT_EQUAL(20, tf.output4_fp->get());
+    TEST_ASSERT_EQUAL(21, tf.output5_fp->get());
+    TEST_ASSERT_EQUAL(22, tf.output6_fp->get());
+    TEST_ASSERT_EQUAL(23, tf.output7_fp->get());
+    TEST_ASSERT_EQUAL(24, tf.output8_fp->get());
+
+    TEST_ASSERT_EQUAL(25, tf.output_on_delta1_fp->get());
+    TEST_ASSERT_EQUAL(26, tf.output_on_delta2_fp->get());
+    TEST_ASSERT_EQUAL(27, tf.output_on_delta3_fp->get());
+    TEST_ASSERT_EQUAL(28, tf.output_on_delta4_fp->get());
+    TEST_ASSERT_EQUAL(29, tf.output_on_delta5_fp->get());
+    TEST_ASSERT_EQUAL(30, tf.output_on_delta6_fp->get());
+    TEST_ASSERT_EQUAL(31, tf.output_on_delta7_fp->get());
+    TEST_ASSERT_EQUAL(32, tf.output_on_delta8_fp->get());
+
+    TEST_ASSERT_EQUAL(33, tf.output_off_delta1_fp->get());
+    TEST_ASSERT_EQUAL(34, tf.output_off_delta2_fp->get());
+    TEST_ASSERT_EQUAL(35, tf.output_off_delta3_fp->get());
+    TEST_ASSERT_EQUAL(36, tf.output_off_delta4_fp->get());
+    TEST_ASSERT_EQUAL(37, tf.output_off_delta5_fp->get());
+    TEST_ASSERT_EQUAL(38, tf.output_off_delta6_fp->get());
+    TEST_ASSERT_EQUAL(39, tf.output_off_delta7_fp->get());
+    TEST_ASSERT_EQUAL(40, tf.output_off_delta8_fp->get());
+
+    TEST_ASSERT_EQUAL(41, tf.latchup1_fp->get());
+    TEST_ASSERT_EQUAL(42, tf.latchup2_fp->get());
+    TEST_ASSERT_EQUAL(43, tf.latchup3_fp->get());
+    TEST_ASSERT_EQUAL(44, tf.latchup4_fp->get());
+    TEST_ASSERT_EQUAL(45, tf.latchup5_fp->get());
+    TEST_ASSERT_EQUAL(46, tf.latchup6_fp->get());
+
+    TEST_ASSERT_EQUAL(47, tf.wdt_i2c_time_left_fp->get());
+
+    TEST_ASSERT_EQUAL(48, tf.wdt_gnd_time_left_fp->get());
+
+    TEST_ASSERT_EQUAL(49, tf.wdt_csp_pings_left1_fp->get());
+    TEST_ASSERT_EQUAL(50, tf.wdt_csp_pings_left2_fp->get());
+
+    TEST_ASSERT_EQUAL(51, tf.counter_wdt_i2c_fp->get());
+
+    TEST_ASSERT_EQUAL(52, tf.counter_wdt_gnd_fp->get());
+
+    TEST_ASSERT_EQUAL(53, tf.counter_wdt_csp1_fp->get());
+    TEST_ASSERT_EQUAL(54, tf.counter_wdt_csp2_fp->get());
+
+    TEST_ASSERT_EQUAL(55, tf.counter_boot_fp->get());
+
+    TEST_ASSERT_EQUAL(56, tf.temp1_fp->get());
+    TEST_ASSERT_EQUAL(57, tf.temp2_fp->get());
+    TEST_ASSERT_EQUAL(58, tf.temp3_fp->get());
+    TEST_ASSERT_EQUAL(59, tf.temp4_fp->get());
+    TEST_ASSERT_EQUAL(60, tf.temp5_fp->get());
+    TEST_ASSERT_EQUAL(61, tf.temp6_fp->get());
+
+    TEST_ASSERT_EQUAL(62, tf.bootcause_fp->get());
+    
+    TEST_ASSERT_EQUAL(63, tf.battmode_fp->get());
+    
+    TEST_ASSERT_EQUAL(64, tf.pptmode_fp->get());
+
+    TEST_ASSERT_EQUAL(65, tf.reserved2_fp->get());
+}
+
+int test_control_task() {
+    UNITY_BEGIN();
+    RUN_TEST(test_task_initialization);
+    RUN_TEST(test_task_execute);
+    return UNITY_END();
+}
+
+#ifdef DESKTOP
+int main() {
+    return test_control_task();
+}
+
+#else
+
+#include <Arduino.h>
+void setup() {
+    delay(2000);
+    Serial.begin(9600);
+    test_control_task();
+}
+
+#endif
