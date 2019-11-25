e08522708f7ed22ab03060a365fe53dadc8558b1
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2019-11-25 01:37:53 -0500

    Integrate Piksi into flight software

--- src/FCCode/MainControlLoop.cpp
+++ src/FCCode/MainControlLoop.cpp
@@ -12,11 +12,20 @@
     static constexpr unsigned int control_cycle_time = 120000;
 #endif
 
+#ifdef DESKTOP
+    #define PIKSI_INITIALIZATION piksi("piksi")
+#else
+    #include <HardwareSerial.h>
+    #define PIKSI_INITIALIZATION piksi("piksi", Serial4)
+#endif
+
 MainControlLoop::MainControlLoop(StateFieldRegistry& registry)
     : ControlTask<void>(registry), 
       field_creator_task(registry),
       clock_manager(registry, control_cycle_time),
       debug_task(registry, debug_task_offset),
+      PIKSI_INITIALIZATION,
+      piksi_control_task(registry, piksi_control_task_offset, piksi),
       mission_manager(registry, mission_manager_offset),
       docksys(),
       docking_controller(registry, docking_controller_offset, docksys),
@@ -31,6 +40,7 @@ void MainControlLoop::execute() {
     debug_task.execute_on_time(control_cycle_start);
     #endif
 
+    piksi_control_task.execute_on_time(control_cycle_start);
     mission_manager.execute_on_time(control_cycle_start);
     quake_manager.execute_on_time(control_cycle_start);
     docking_controller.execute_on_time(control_cycle_start);
--- src/FCCode/MainControlLoop.hpp
+++ src/FCCode/MainControlLoop.hpp
@@ -6,6 +6,7 @@
 #include <StateFieldRegistry.hpp>
 
 #include "ClockManager.hpp"
+#include "PiksiControlTask.hpp"
 #include "DebugTask.hpp"
 #include "FieldCreatorTask.hpp"
 #include "MissionManager.hpp"
@@ -25,6 +26,8 @@ class MainControlLoop : public ControlTask<void> {
     FieldCreatorTask field_creator_task;
     ClockManager clock_manager;
     DebugTask debug_task;
+    Devices::Piksi piksi;
+    PiksiControlTask piksi_control_task;
     MissionManager mission_manager;
     Devices::DockingSystem docksys;
     DockingController docking_controller;
@@ -33,11 +36,13 @@ class MainControlLoop : public ControlTask<void> {
     // Control cycle time offsets, in microseconds
     #ifdef HOOTL
         static constexpr unsigned int debug_task_offset         = 1000;
+        static constexpr unsigned int piksi_control_task_offset = 51000;
         static constexpr unsigned int mission_manager_offset    = 51000;
         static constexpr unsigned int quake_manager_offset      = 51100;
         static constexpr unsigned int docking_controller_offset = 53100;
     #else
         static constexpr unsigned int debug_task_offset         =  1000;
+        static constexpr unsigned int piksi_control_task_offset =  1010;
         static constexpr unsigned int mission_manager_offset    =  1010;
         static constexpr unsigned int quake_manager_offset      =  1110;
         static constexpr unsigned int docking_controller_offset = 21110;
