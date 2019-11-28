5074271f260435eaa0cd0062f6bcbd78dec795fc
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2019-11-27 22:47:00 -0500

    Change build names, integrate Piksi and Gomspace controller into flight software (#86)
    * Integrate Piksi into flight software
    
    * Add gomspace controller to flight software loop
    
    * Fix offsets
    
    * Simplify build names
    
    * Simplify build names,  integrate Piksi and Gomspace controller into flight software

--- src/FCCode/MainControlLoop.hpp
+++ src/FCCode/MainControlLoop.hpp
@@ -6,17 +6,15 @@
 #include <StateFieldRegistry.hpp>
 
 #include "ClockManager.hpp"
+#include "PiksiControlTask.hpp"
+#include "GomspaceController.hpp"
 #include "DebugTask.hpp"
 #include "FieldCreatorTask.hpp"
 #include "MissionManager.hpp"
 #include "QuakeManager.h"
 #include "DockingController.hpp"
 
-#ifdef HOOTL
-// OK
-#elif FLIGHT
-// OK
-#else
+#if (!defined(HOOTL) && !defined(FLIGHT))
 static_assert(false, "Need to define either the HOOTL or FLIGHT flags.");
 #endif
 
@@ -25,22 +23,38 @@ class MainControlLoop : public ControlTask<void> {
     FieldCreatorTask field_creator_task;
     ClockManager clock_manager;
     DebugTask debug_task;
+
+    Devices::Piksi piksi;
+    PiksiControlTask piksi_control_task;
+
+    Devices::Gomspace::eps_hk_t hk;
+    Devices::Gomspace::eps_config_t config; 
+    Devices::Gomspace::eps_config2_t config2; 
+    Devices::Gomspace gomspace;
+    GomspaceController gomspace_controller;
+
     MissionManager mission_manager;
+
     Devices::DockingSystem docksys;
     DockingController docking_controller;
+
     QuakeManager quake_manager;
 
     // Control cycle time offsets, in microseconds
     #ifdef HOOTL
-        static constexpr unsigned int debug_task_offset         = 1000;
-        static constexpr unsigned int mission_manager_offset    = 51000;
-        static constexpr unsigned int quake_manager_offset      = 51100;
-        static constexpr unsigned int docking_controller_offset = 53100;
+        static constexpr unsigned int debug_task_offset          =   5500;
+        static constexpr unsigned int piksi_control_task_offset  =  55000;
+        static constexpr unsigned int gomspace_controller_offset = 106500;
+        static constexpr unsigned int mission_manager_offset     = 111600;
+        static constexpr unsigned int docking_controller_offset  = 152400;
+        static constexpr unsigned int quake_manager_offset       = 153400;
     #else
-        static constexpr unsigned int debug_task_offset         =  1000;
-        static constexpr unsigned int mission_manager_offset    =  1010;
-        static constexpr unsigned int quake_manager_offset      =  1110;
-        static constexpr unsigned int docking_controller_offset = 21110;
+        static constexpr unsigned int debug_task_offset          =   5500;
+        static constexpr unsigned int piksi_control_task_offset  =   6000;
+        static constexpr unsigned int gomspace_controller_offset =  57500;
+        static constexpr unsigned int mission_manager_offset     =  62600;
+        static constexpr unsigned int docking_controller_offset  = 103400;
+        static constexpr unsigned int quake_manager_offset       = 104400;
     #endif
 
    public:
