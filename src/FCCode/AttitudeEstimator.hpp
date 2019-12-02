e0279d194cf5601d7b71caa1b97c0977f0cba022
Shihao Cao <shihaocao@gmail.com>
2019-12-01 23:47:12 -0500

    fix include

--- src/FCCode/AttitudeEstimator.hpp
+++ src/FCCode/AttitudeEstimator.hpp
@@ -1,8 +1,8 @@
 #ifndef ATTITUDE_ESTIMATOR_HPP_
 #define ATTITUDE_ESTIMATOR_HPP_
 
-#include "../../.pio/libdeps/teensy35_hootl/psim/include/gnc_attitude_estimation.hpp"
-//#include <gnc_attitude_estimation.hpp>
+//#include "../../.pio/libdeps/teensy35_hootl/psim/include/gnc_attitude_estimation.hpp"
+#include <gnc_attitude_estimation.hpp>
 #include <TimedControlTask.hpp>
 
 /**
