86c33dac375f3b34dd83530de5c10cd324776dc2
Shihao Cao <shihaocao@gmail.com>
2019-12-03 01:06:57 -0500

    add readablefields to constructor list

--- src/FCCode/AttitudeEstimator.hpp
+++ src/FCCode/AttitudeEstimator.hpp
@@ -23,25 +23,33 @@ class AttitudeEstimator : public TimedControlTask<void> {
      */
     void execute() override;
 
+    /**
+     * @brief Read data from field pointers and set the data object
+     * 
+     */
     void set_data();
 
+    /**
+     * @brief Set the estimate object from the outputs of the gnc estimate
+     * 
+     */
     void set_estimate();
 
    protected:
+    //! The time that all gnc calculations begin from
+    ReadableStateField<gps_time_t>* const pan_epoch_fp;
+    
     /**
      * @brief Inputs collected from Piksi and ADCSBoxMonitor.
      */
-
-    ReadableStateField<gps_time_t>* pan_epoch_fp;
-
     //! Time from Piksi
-    ReadableStateField<gps_time_t>* piksi_time_fp;
+    ReadableStateField<gps_time_t>* const piksi_time_fp;
     //! Position of this satellite, Vector of doubles
-    ReadableStateField<d_vector_t>* pos_vec_ecef_fp;
+    ReadableStateField<d_vector_t>* const pos_vec_ecef_fp;
     //! Sun vector of this satellite, in the body frame.
-    ReadableStateField<f_vector_t>* ssa_vec_rd_fp;
+    ReadableStateField<f_vector_t>* const ssa_vec_rd_fp;
     //! Magnetic field vector of this satellite in the body frame.
-    ReadableStateField<f_vector_t>* mag_vec_fp;
+    ReadableStateField<f_vector_t>* const mag_vec_fp;
 
     //kyle's gnc structs
     gnc::AttitudeEstimatorData data;
