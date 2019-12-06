8636834ae0e1502d78181855fb7e67e018d535f6
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2019-12-06 17:27:50 -0500

    Remove unnecessary desktop testing

--- tools/run_desktop_flight_tests.sh
+++ tools/run_desktop_flight_tests.sh
@@ -12,8 +12,6 @@ valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose .
 # Compile and run functional test for environments
 platformio run -e native_ci
 platformio run -e native
-platformio run -e downlink_parser_ci
-platformio run -e downlink_parser
 
 python -m unittest test.test_dummy_fsw
 python -m unittest test.test_real_fsw
