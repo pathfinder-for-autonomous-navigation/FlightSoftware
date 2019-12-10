693717fc9c20fd52db225291f9ee3eb0877157d3
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2019-12-10 16:51:11 -0500

    Update release script

--- tools/generate_release.sh
+++ tools/generate_release.sh
@@ -8,9 +8,11 @@ platformio run -e teensy35_hootl
 platformio run -e teensy36_hootl
 platformio run -e preflight
 platformio run -e flight
+platformio run -e downlink_parser
 
 cp .pio/build/native/program release/macOS
 cp .pio/build/teensy35_hootl/firmware.hex release/teensy35_hootl
 cp .pio/build/teensy36_hootl/firmware.hex release/teensy36_hootl
 cp .pio/build/preflight/firmware.hex release/preflight
 cp .pio/build/flight/firmware.hex release/flight
+cp .pio/build/downlink_parser/program release/downlink_parser
