6c189c9ad774d1a6ab970d599a8aa3c6a08347fa
Shihao Cao <shihaocao@gmail.com>
2019-12-14 01:46:58 -0500

    change some timings so that they make sense

--- src/FCCode/MainControlLoop.hpp
+++ src/FCCode/MainControlLoop.hpp
@@ -61,10 +61,10 @@ class MainControlLoop : public ControlTask<void> {
     #else
         static constexpr unsigned int debug_task_offset          =   5500;
         static constexpr unsigned int piksi_control_task_offset  =   6000;
-        static constexpr unsigned int adcs_monitor_offset        =  70500;
-        static constexpr unsigned int attitude_estimator_offset  =  85500;
-        static constexpr unsigned int gomspace_controller_offset =  57500;
-        static constexpr unsigned int mission_manager_offset     =  62600;
+        static constexpr unsigned int adcs_monitor_offset        =  20500;
+        static constexpr unsigned int attitude_estimator_offset  =  35500;
+        static constexpr unsigned int gomspace_controller_offset =  56500;
+        static constexpr unsigned int mission_manager_offset     =  61600;
         static constexpr unsigned int docking_controller_offset  = 103400;
         static constexpr unsigned int downlink_producer_offset   = 104400;
         static constexpr unsigned int quake_manager_offset       = 104500;
