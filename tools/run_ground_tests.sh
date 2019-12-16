28c05733c193655290df16bff8efa7b17e9e0029
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2019-12-16 14:19:26 -0500

    Update build script

--- tools/run_ground_tests.sh
+++ tools/run_ground_tests.sh
@@ -15,4 +15,5 @@ python -m unittest test.test_downlink_parser
 # Check that the telemetry info generator compiles, passes functional test, and Valgrind
 platformio run -e telem_info_generator
 python -m unittest test.test_telem_info_generator
-valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose .pio/build/telem_info_generator/program
+valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose .pio/build/telem_info_generator/program x.json
+rm x.json
