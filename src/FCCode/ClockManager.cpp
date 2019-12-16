2470d92eacca5cd8968f6f0650549d8942504596
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2019-12-16 16:51:16 -0500

    Clocking updates (#108)
    * Adding timing  and memory statistics
    * Add Piksi timestamps

--- src/FCCode/ClockManager.cpp
+++ src/FCCode/ClockManager.cpp
@@ -2,10 +2,9 @@
 
 ClockManager::ClockManager(StateFieldRegistry &registry,
                            const unsigned int _control_cycle_size) :
-    TimedControlTask<void>(registry, 0),
+    TimedControlTask<void>(registry, "clock_ct", 0),
     control_cycle_size(_control_cycle_size),
-    control_cycle_count_sr(),
-    control_cycle_count_f("pan.cycle_no", control_cycle_count_sr)
+    control_cycle_count_f("pan.cycle_no", Serializer<unsigned int>())
 {
     add_readable_field(control_cycle_count_f);
 }
@@ -13,11 +12,12 @@ ClockManager::ClockManager(StateFieldRegistry &registry,
 void ClockManager::execute() {
     if (has_executed) {
         sys_time_t earliest_start_time =
-            TimedControlTask<void>::control_cycle_start_time + control_cycle_size;
+            TimedControlTaskBase::control_cycle_start_time + control_cycle_size;
         wait_until_time(earliest_start_time);
     }
 
     has_executed = true;
-    TimedControlTask<void>::control_cycle_start_time = get_system_time();
-    control_cycle_count_f.set(control_cycle_count_f.get() + 1);
+    TimedControlTaskBase::control_cycle_start_time = get_system_time();
+    control_cycle_count++;
+    control_cycle_count_f.set(control_cycle_count);
 }
--- src/FCCode/PiksiControlTask.cpp
+++ src/FCCode/PiksiControlTask.cpp
@@ -1,50 +1,57 @@
 #include "PiksiControlTask.hpp"
+#include <limits>
+#include <cmath>
 
 PiksiControlTask::PiksiControlTask(StateFieldRegistry &registry, 
     unsigned int offset, Devices::Piksi &_piksi) 
-    : TimedControlTask<void>(registry, offset),
+    : TimedControlTask<void>(registry, "piksi", offset),
     piksi(_piksi),
-    pos_sr(0, 100000, 100),
-    pos_f("piksi.pos", pos_sr),
-    vel_sr(0, 100000, 100),
-    vel_f("piksi.vel", vel_sr),
-    baseline_pos_sr(0, 100000, 100),
-    baseline_pos_f("piksi.baseline_pos", baseline_pos_sr),
-    current_state_sr(0, 4, 2),
-    current_state_f("piksi.state", current_state_sr),
-    time_sr(),
-    time_f("piksi.time", time_sr)
+    pos_f("piksi.pos", Serializer<d_vector_t>(0,100000,100)),
+    vel_f("piksi.vel", Serializer<d_vector_t>(0,100000,100)),
+    baseline_pos_f("piksi.baseline_pos", Serializer<d_vector_t>(0,100000,100)),
+    current_state_f("piksi.state", Serializer<unsigned int>(4)),
+    fix_error_count_f("piksi.fix_error_count", Serializer<unsigned int>(1001)),
+    time_f("piksi.time", Serializer<gps_time_t>()),
+    last_fix_time_f("piksi.last_fix_time")
     {
         add_readable_field(pos_f);
         add_readable_field(vel_f);
         add_readable_field(baseline_pos_f);
         add_readable_field(current_state_f);
+        add_readable_field(fix_error_count_f);
         add_readable_field(time_f);
+        add_internal_field(last_fix_time_f);
 
         //register callbacks and begin the serial port
         piksi.setup();
 
+        // Set initial values
+        const double nan = std::numeric_limits<double>::quiet_NaN();
         current_state_f.set(static_cast<unsigned int>(piksi_mode_t::no_fix));
+        pos_f.set({nan, nan, nan});
+        vel_f.set({nan, nan, nan});
+        baseline_pos_f.set({nan, nan, nan});
     }
 
 void PiksiControlTask::execute()
-{  
+{
     int read_out = piksi.read_all();
 
     //4 means no bytes
     //3 means CRC error on serial
     //5 means timing error exceed
     if(read_out == 3|| read_out == 4|| read_out == 5)
-        since_good_cycles += 1;
-    else 
-        since_good_cycles = 0;
+        fix_error_count_f.set(fix_error_count_f.get() + 1);
+    else {
+        fix_error_count_f.set(0);
+    }
         
     //if we haven't had a good reading in ~120 seconds the piksi is probably dead
     //eventually replace with HAVT logic
-    if(since_good_cycles > DEAD_CYCLE_COUNT){
+    if(fix_error_count_f.get() > DEAD_CYCLE_COUNT){
         current_state_f.set(static_cast<unsigned int>(piksi_mode_t::dead));
         //prevent roll over
-        since_good_cycles = 1001;
+        fix_error_count_f.set(1001);
         return;
     }
 
@@ -101,14 +108,17 @@ void PiksiControlTask::execute()
 
         if(read_out == 0) {
             current_state_f.set(static_cast<unsigned int>(piksi_mode_t::spp));
+            last_fix_time_f.set(get_system_time());
         }
         if(read_out == 1){
             int baseline_flag = piksi.get_baseline_ecef_flags();
             if(baseline_flag == 1){
                 current_state_f.set(static_cast<unsigned int>(piksi_mode_t::fixed_rtk));
+                last_fix_time_f.set(get_system_time());
             }
             else if(baseline_flag == 0){
                 current_state_f.set(static_cast<unsigned int>(piksi_mode_t::float_rtk));
+                last_fix_time_f.set(get_system_time());
             }
             else{
                 //baseline flag unexpected value
@@ -124,7 +134,6 @@ void PiksiControlTask::execute()
         if(read_out == 1){
             baseline_pos_f.set(baseline_pos);
         }
-
     }
 
     //if read_out is unexpected value which it shouldn't do lol
@@ -132,5 +141,4 @@ void PiksiControlTask::execute()
         current_state_f.set(static_cast<unsigned int>(piksi_mode_t::data_error));
         return;
     }
-
 }
