6b60d91e87256c2acaca3d4842f278e2f858ccb0
Fatima Yousuf <fatimayousuf@dhcp-clara-146.redrover.cornell.edu>
2019-12-17 18:46:37 -0500

    added more testing

--- test/test_fsw_EEPROM/test_EEPROM.cpp
+++ test/test_fsw_EEPROM/test_EEPROM.cpp
@@ -51,29 +51,47 @@ void test_task_initialization() {
 
 void test_task_execute() {
     TestFixture tf;
-    // The period is set to 5. At the 45th control cycle, the EEPROM
+
+    // Let the statefields change over time
+    tf.mission_mode_fp->set(2);
+    tf.is_deployed_fp->set(false);
+    tf.sat_designation_fp->set(2);
+    tf.control_cycle_count_fp->set(50);
+
+    // The period is set to 5. At the 50th control cycle, the EEPROM
     // should write the values to the EEPROM
     tf.eeprom_controller->execute();
     #ifndef DESKTOP
-    TEST_ASSERT_EQUAL(1, EEPROM.read(tf.eeprom_controller->mission_mode_address));
+    TEST_ASSERT_EQUAL(2, EEPROM.read(tf.eeprom_controller->mission_mode_address));
     TEST_ASSERT_EQUAL(false, EEPROM.read(tf.eeprom_controller->is_deployed_address));
-    TEST_ASSERT_EQUAL(3, EEPROM.read(tf.eeprom_controller->sat_designation_address));
-    TEST_ASSERT_EQUAL(45, EEPROM.read(tf.eeprom_controller->control_cycle_count_address));
+    TEST_ASSERT_EQUAL(2, EEPROM.read(tf.eeprom_controller->sat_designation_address));
+    TEST_ASSERT_EQUAL(50, EEPROM.read(tf.eeprom_controller->control_cycle_count_address));
     #endif
 
-    // Now we have information stored in the EEPROM. If we change the statefield values and 
-    // let another period pass, then we should store the new values in the EEPROM
-    tf.mission_mode_fp->set(30);
-    tf.is_deployed_fp->set(true);
-    tf.sat_designation_fp->set(1);
-    tf.control_cycle_count_fp->set(50);
+    // Now we pretend the satellite just rebooted. Everytime the satellite reboots, another 
+    // eeprom constructor is instantiated.
+    TestFixture tf2;
 
-    tf.eeprom_controller->execute();
+    // Check if the new eeprom controller set the statefield values to the values that 
+    // were previously stored in the EEPROM
+    TEST_ASSERT_EQUAL(2, tf2.mission_mode_fp->get());
+    TEST_ASSERT_EQUAL(false, tf2.is_deployed_fp->get());
+    TEST_ASSERT_EQUAL(2, tf2.sat_designation_fp->get());
+    TEST_ASSERT_EQUAL(50, tf2.control_cycle_count_fp->get());
+
+    // Now we let the statefield values change over time and let another period pass.
+    tf2.mission_mode_fp->set(30);
+    tf2.is_deployed_fp->set(true);
+    tf2.sat_designation_fp->set(1);
+    tf2.control_cycle_count_fp->set(55);
+
+    // Check if those values were written to the EEPROM
+    tf2.eeprom_controller->execute();
     #ifndef DESKTOP
-    TEST_ASSERT_EQUAL(30, EEPROM.read(tf.eeprom_controller->mission_mode_address));
-    TEST_ASSERT_EQUAL(true, EEPROM.read(tf.eeprom_controller->is_deployed_address));
-    TEST_ASSERT_EQUAL(1, EEPROM.read(tf.eeprom_controller->sat_designation_address));
-    TEST_ASSERT_EQUAL(50, EEPROM.read(tf.eeprom_controller->control_cycle_count_address));
+    TEST_ASSERT_EQUAL(30, EEPROM.read(tf2.eeprom_controller->mission_mode_address));
+    TEST_ASSERT_EQUAL(true, EEPROM.read(tf2.eeprom_controller->is_deployed_address));
+    TEST_ASSERT_EQUAL(1, EEPROM.read(tf2.eeprom_controller->sat_designation_address));
+    TEST_ASSERT_EQUAL(55, EEPROM.read(tf2.eeprom_controller->control_cycle_count_address));
     #endif
 
 }
@@ -85,11 +103,7 @@ int test_control_task() {
     return UNITY_END();
 }
 
-#ifdef DESKTOP
-int main() {
-    return test_control_task();
-}
-#else
+#ifndef DESKTOP
 
 #include <Arduino.h>
 void setup() {
@@ -99,11 +113,10 @@ void setup() {
     ; // wait for serial port to connect.
     }
     test_control_task();
-    //Serial.println("hi!");
 }
 
 void loop(){
-    //Serial.println("hello!");
+    
 }
 
 #endif
