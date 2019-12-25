a5b4912808f71654bdade37b88158b73a1b06f49
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2019-12-25 03:10:57 -0500

    Cleanup items

--- src/FCCode/FieldCreatorTask.hpp
+++ src/FCCode/FieldCreatorTask.hpp
@@ -11,45 +11,36 @@
 // eventually become zero.
 class FieldCreatorTask : public ControlTask<void> {
     public:
-      WritableStateField<unsigned char> adcs_mode_f;
+      WritableStateField<unsigned char> adcs_state_f;
       WritableStateField<f_quat_t> adcs_cmd_attitude_f;
       ReadableStateField<float> adcs_ang_rate_f;
       WritableStateField<float> adcs_min_stable_ang_rate_f;
-      WritableStateField<bool> docking_config_cmd_f;
 
-      ReadableStateField<f_vector_t> ssa_vec_rd_f;
-      ReadableStateField<f_vector_t> mag_vec_f;
-
-      ReadableStateField<signed int> radio_err_f;
+      ReadableStateField<unsigned char> prop_mode_f;
+      ReadableStateField<d_vector_t> propagated_baseline_pos_f;
 
       FieldCreatorTask(StateFieldRegistry& r) : 
         ControlTask<void>(r),
-        adcs_mode_f("adcs.mode", Serializer<unsigned char>(10)),
+        adcs_state_f("adcs.state", Serializer<unsigned char>(10)),
         adcs_cmd_attitude_f("adcs.cmd_attitude", Serializer<f_quat_t>()),
         adcs_ang_rate_f("adcs.ang_rate", Serializer<float>(0, 10, 4)),
         adcs_min_stable_ang_rate_f("adcs.min_stable_ang_rate", Serializer<float>(0, 10, 4)),
-        docking_config_cmd_f("docksys.config_cmd", Serializer<bool>()),
-        ssa_vec_rd_f("adcs_box.sun_vec", Serializer<f_vector_t>(0,1,32*3)),
-        mag_vec_f("adcs_box.mag_vec", Serializer<f_vector_t>(0,1,32*3)),
-        radio_err_f("downlink_producer.radio_err_ptr", Serializer<signed int>(-90, 10))
+        prop_mode_f("prop.mode", Serializer<unsigned char>(1)),
+        propagated_baseline_pos_f("orbit.baseline_pos", Serializer<d_vector_t>(0, 100000, 100))
       {
           // Create the fields!
 
           // For MissionManager
-          add_writable_field(adcs_mode_f);
+          add_writable_field(adcs_state_f);
           add_writable_field(adcs_cmd_attitude_f);
           add_readable_field(adcs_ang_rate_f);
           add_writable_field(adcs_min_stable_ang_rate_f);
 
-          // For DockingController
-          add_writable_field(docking_config_cmd_f);
+          // For propulsion controller
+          add_readable_field(prop_mode_f);
 
-          // For AttitudeEstimator
-          add_readable_field(ssa_vec_rd_f);
-          add_readable_field(mag_vec_f);
-
-          // For QuakeManager
-          add_readable_field(radio_err_f);
+          // For orbit estimator
+          add_readable_field(propagated_baseline_pos_f);
       }
 
       void execute() {
