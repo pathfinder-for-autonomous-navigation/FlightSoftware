dc57c706b3cd56fee3a1ed5299d21ef488804678
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2019-12-26 18:03:38 -0500

    Fixed add_to_registry

--- src/FCCode/Fault.cpp
+++ src/FCCode/Fault.cpp
@@ -15,11 +15,10 @@ Fault::Fault(const std::string& name,
 }
 
 bool Fault::add_to_registry(StateFieldRegistry& r) {
-    bool added = false;
-    added |= r.add_readable_field(static_cast<ReadableStateFieldBase*>(this));
-    added |= r.add_writable_field(static_cast<WritableStateFieldBase*>(&suppress_f));
-    added |= r.add_writable_field(static_cast<WritableStateFieldBase*>(&override_f));
-    return added;
+    if (!r.add_readable_field(static_cast<ReadableStateFieldBase*>(this))) return false;
+    if (!r.add_writable_field(static_cast<WritableStateFieldBase*>(&suppress_f))) return false;
+    if (!r.add_writable_field(static_cast<WritableStateFieldBase*>(&override_f))) return false;
+    return true;
 }
 
 void Fault::signal() {
