5dff2bef837d160e6deed73511ced4dddbd7ee8d
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2019-12-27 21:39:52 -0500

    Telem info generator (#114)
    * Infrastructural changes for ground software
    
    * Telemetry info generator
    
    Co-authored-by: Athena  <afc65@cornell.edu>

---
+++ src/GroundCode/TelemetryInfoGenerator.cpp
@@ -0,0 +1,162 @@
+#include "TelemetryInfoGenerator.hpp"
+#include <type_traits>
+#include <typeinfo>
+#include <array>
+
+TelemetryInfoGenerator::TelemetryInfoGenerator(
+    const std::vector<DownlinkProducer::FlowData>& _flow_data) :
+        r(), fcp(r, _flow_data), flow_data(_flow_data) {}
+
+/************** Helper functions for telemetry info generation. ***********/
+using nlohmann::json;
+
+template<typename T>
+std::string type_name() {
+    if      (std::is_same<unsigned int, T>::value) return "unsigned int";
+    else if (std::is_same<signed int, T>::value) return "signed int";
+    else if (std::is_same<unsigned char, T>::value) return "unsigned char";
+    else if (std::is_same<signed char, T>::value) return "signed char";
+    else if (std::is_same<float, T>::value) return "float";
+    else if (std::is_same<double, T>::value) return "double";
+    else if (std::is_same<f_vector_t, T>::value) return "float vector";
+    else if (std::is_same<d_vector_t, T>::value) return "double vector";
+    else if (std::is_same<f_quat_t, T>::value) return "float quaternion";
+    else if (std::is_same<d_quat_t, T>::value) return "double quaternion";
+    else if (std::is_same<bool, T>::value) return "bool";
+    else if (std::is_same<gps_time_t, T>::value) return "gps_time_t";
+    else {
+        std::cout << "Invalid typename specified to stringify: " << typeid(T).name() << std::endl;
+        assert(false);
+    }
+}
+
+template<template<typename> class StateFieldType,
+         typename UnderlyingType,
+         class StateFieldBaseType>
+bool try_collect_field_info(const StateFieldBaseType* field, json& field_info) {
+    const StateFieldType<UnderlyingType>* ptr = dynamic_cast<const StateFieldType<UnderlyingType>*>(field);
+    if (!ptr) return false;
+
+    field_info["type"] = type_name<UnderlyingType>();
+    field_info["min"] = ptr->get_serializer_min();
+    field_info["max"] = ptr->get_serializer_max();
+    return true;
+}
+
+template<template<typename> class StateFieldType,
+         typename UnderlyingType,
+         class StateFieldBaseType>
+bool try_collect_vector_field_info(const StateFieldBaseType* field, json& field_info) {
+    static_assert(std::is_same<UnderlyingType, double>::value 
+                  || std::is_same<UnderlyingType, float>::value,
+        "Can't collect vector field info for a vector of non-float or non-double type.");
+    
+    using UnderlyingVectorType = std::array<UnderlyingType, 3>;
+
+    const StateFieldType<UnderlyingVectorType>* ptr =
+        dynamic_cast<const StateFieldType<UnderlyingVectorType>*>(field);
+    if (!ptr) return false;
+
+    field_info["type"] = type_name<UnderlyingVectorType>();
+    field_info["min"] = ptr->get_serializer_min()[0];
+    field_info["max"] = ptr->get_serializer_max()[0];
+    return true;
+}
+
+template<template<typename> class StateFieldType,
+         typename UnderlyingType,
+         class StateFieldBaseType>
+bool try_collect_unbounded_field_info(const StateFieldBaseType* field, json& field_info) {
+    static_assert(std::is_same<UnderlyingType, gps_time_t>::value
+                  || std::is_same<UnderlyingType, bool>::value
+                  || std::is_same<UnderlyingType, d_quat_t>::value
+                  || std::is_same<UnderlyingType, f_quat_t>::value,
+        "Can't collect unbounded field info for a non-bool, non-GPS time, or non-quaternion type.");
+
+    const StateFieldType<UnderlyingType>* ptr = dynamic_cast<const StateFieldType<UnderlyingType>*>(field);
+    if (!ptr) return false;
+
+    field_info["type"] = type_name<UnderlyingType>();
+    field_info["min"] = "N/A";
+    field_info["max"] = "N/A";
+    return true;
+}
+
+template<template<typename> class StateFieldType, class StateFieldBaseType>
+json get_field_info(const StateFieldBaseType* field) {
+    json field_info;
+    field_info["bitsize"] = field->bitsize();
+
+    bool found_field_type = false;
+    found_field_type |= try_collect_field_info<StateFieldType, unsigned int, StateFieldBaseType>(field, field_info);
+    found_field_type |= try_collect_field_info<StateFieldType, signed int, StateFieldBaseType>(field, field_info);
+    found_field_type |= try_collect_field_info<StateFieldType, unsigned char, StateFieldBaseType>(field, field_info);
+    found_field_type |= try_collect_field_info<StateFieldType, signed char, StateFieldBaseType>(field, field_info);
+    found_field_type |= try_collect_field_info<StateFieldType, float, StateFieldBaseType>(field, field_info);
+    found_field_type |= try_collect_field_info<StateFieldType, double, StateFieldBaseType>(field, field_info);
+    found_field_type |= try_collect_vector_field_info<StateFieldType, float, StateFieldBaseType>(field, field_info);
+    found_field_type |= try_collect_vector_field_info<StateFieldType, double, StateFieldBaseType>(field, field_info);
+    found_field_type |= try_collect_unbounded_field_info<StateFieldType, bool, StateFieldBaseType>(field, field_info);
+    found_field_type |= try_collect_unbounded_field_info<StateFieldType, gps_time_t, StateFieldBaseType>(field, field_info);
+    found_field_type |= try_collect_unbounded_field_info<StateFieldType, f_quat_t, StateFieldBaseType>(field, field_info);
+    found_field_type |= try_collect_unbounded_field_info<StateFieldType, d_quat_t, StateFieldBaseType>(field, field_info);
+    
+    if(!found_field_type) {
+        std::cout << "Could not find field type for field: " << field->name() << std::endl;
+        assert(false);
+    }
+
+    return field_info;
+}
+
+json get_writable_field_info(const WritableStateFieldBase* field) {
+    json field_info = get_field_info<WritableStateField, WritableStateFieldBase>(field);
+    field_info["writable"] = true;
+    return field_info;
+}
+
+json get_readable_field_info(const ReadableStateFieldBase* field) {
+    json field_info = get_field_info<ReadableStateField, ReadableStateFieldBase>(field);
+    field_info["writable"] = false;
+    return field_info;
+}
+/************** End helper functions. ***********/
+
+json TelemetryInfoGenerator::generate_telemetry_info() {
+    json ret;
+
+    // Get field data
+    ret["fields"] = json::object();
+    for(const WritableStateFieldBase* wf : r.writable_fields) {
+        const std::string& field_name = wf->name();
+        if (ret["fields"].find(field_name) != ret["fields"].end()) continue;
+        ret["fields"][field_name] = get_writable_field_info(wf);;
+    }
+
+    for(const ReadableStateFieldBase* rf : r.readable_fields) {
+        const std::string& field_name = rf->name();
+        if (ret["fields"].find(field_name) != ret["fields"].end()) continue;
+        ret["fields"][field_name] = get_readable_field_info(rf);
+    }
+
+    // Get flow data
+    ret["flows"] = json::array();
+    for(size_t i = 0; i < flow_data.size(); i++) {
+        const DownlinkProducer::FlowData& f = flow_data[i];
+        ret["flows"].push_back({
+            {"id", f.id},
+            {"priority", i},
+            {"fields", f.field_list},
+            {"active", f.is_active}
+        });
+        for (const std::string& field_name : f.field_list) {
+            ret["fields"][field_name]["flow_id"] = f.id;
+        }
+    }
+    for(auto& field : ret["fields"]) {
+        if (field.find("flow_id") == field.end())
+            field["flow_id"] = "undefined";
+    }
+
+    return ret;
+}
---
+++ tools/deploy_release.sh
@@ -0,0 +1,87 @@
+#!/bin/sh
+
+# This script is a work in progress
+
+# This should only be run on Mac!
+if [[ "$OSTYPE" != "darwin"* ]]; then
+    echo "This script should only be run on MacOS."
+    exit 1
+fi
+
+# A PlatformIO installation is required for this script
+if ! [ -x "$(command -v platformio)" ]; then
+  echo 'Error: platformio is not installed.' >&2
+  exit 1
+fi
+
+#function deploy_firmware () {}
+
+function deploy_to_satellite () {
+    full_satname = "{$1} satellite"
+    echo "Deploying code now to $full_satname. Ensure that $full_satname is UNPLUGGED from computer."
+
+### Preflight reset ####
+
+    echo  "First we'll reset the $full_satname's computers to a safe initial state."
+    input "1. Plug in the $full_satname's flight computer now. Press ENTER when plugged in."
+    echo  "   Restoring $full_satname's flight computer to preflight configuration."
+    deploy_firmware "preflight_$1_fc"
+    input "2. Unplug the $full_satname's flight computer. Press ENTER when complete."
+    
+    input "3. Plug in the $full_satname's ADCS computer now. Press ENTER when complete."
+    echo "   Restoring $full_satname's ADCS computer to preflight configuration."
+    deploy_firmware "preflight_$1_adcs"
+    input "4. Unplug the $full_satname's ADCS computer. Press ENTER when complete."
+
+    input "5. Power on the $full_satname to allow the preflight reset code to run. A light will turn on to signal completion. Press ENTER when complete."
+    input "6. Power off the satellite. Press ENTER when complete."
+
+### Flight software programming ###
+    echo  "-----------------"
+
+    echo  "Now we'll load flight software on to the $full_satname's computers."
+    input "7. Plug in the $full_satname's ADCS computer now. Press ENTER when plugged in."
+    echo  "   Loading flight software to $full_satname's ADCS computer."
+    deploy_firmware "flight_$1_adcs"
+    input "8. Unplug the $full_satname's ADCS computer. Press ENTER when complete."
+
+    input "9. Plug in the $full_satname's flight computer now. Press ENTER when plugged in."
+    echo  "   Loading flight software to $full_satname's flight computer."
+    deploy_firmware "flight_$1_fc"
+    input "10. Unplug the $full_satname's flight computer. Press ENTER when complete."
+
+### Complete! ###
+
+    echo "The $full_satname is now programmed. DO NOT turn it on prior to flight."
+    echo "--------------------------------------"
+}
+
+function deploy_gsw() {
+    echo "Shutting off mission control software."
+    echo "Deploying downlink parser asset to mission control."
+    echo "Deplying uplink parser asset to mission control."
+    echo "Starting mission control software."
+}
+
+echo "Generating release files. This might take up to 10 minutes."
+releasedir= $(dirname "$0")
+echo $releasedir
+releasepath= $releasedir/generate_release.sh
+echo $releasepath
+./releasepath
+if proc != 0:
+    echo "Error generating release. Run tools/generate_release.sh manually to see why."
+    echo 1
+echo "Completed release generation."
+
+echo "Downloading Teensy deployment tool."
+git clone https://github.com/PaulStoffregen/teensy_loader_cli
+cd teensy_loader_cli
+make
+cd ..
+cp teensy_loader_cli/teensy_loader_cli loader
+rm -rf teensy_loader_cli/
+
+deploy_to_satellite "leader"
+deploy_to_satellite "follower"
+deploy_gsw
