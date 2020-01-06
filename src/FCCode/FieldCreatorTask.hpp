e9d0e81f9ca6dd6d621c799463bb14557b2ae19b
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2020-01-06 17:36:06 -0500

    Fix field creator task

--- src/FCCode/FieldCreatorTask.hpp
+++ src/FCCode/FieldCreatorTask.hpp
@@ -11,23 +11,22 @@
 // eventually become zero.
 class FieldCreatorTask : public ControlTask<void> {
     public:
-      WritableStateField<f_quat_t> adcs_cmd_attitude_f;
-      WritableStateField<float> adcs_ang_rate_f;
-      WritableStateField<float> adcs_min_stable_ang_rate_f;
-
       ReadableStateField<d_vector_t> pos_f;
       ReadableStateField<d_vector_t> pos_baseline_f;
-
-      WritableStateField<bool> docking_config_cmd_f;
 
       ReadableStateField<unsigned char> prop_mode_f;
 
       FieldCreatorTask(StateFieldRegistry& r) : 
         ControlTask<void>(r),
-        prop_mode_f("prop.mode", Serializer<unsigned char>(1)),
-        propagated_baseline_pos_f("orbit.baseline_pos", Serializer<d_vector_t>(0, 100000, 100))
+        pos_f("orbit.pos", Serializer<d_vector_t>(0, 100000, 100)),
+        pos_baseline_f("orbit.baseline_pos", Serializer<d_vector_t>(0, 100000, 100)),
+        prop_mode_f("prop.mode", Serializer<unsigned char>(1))
       {
           // Create the fields!
+
+          // For orbit estimator
+          add_readable_field(pos_f);
+          add_readable_field(pos_baseline_f);
 
           // For propulsion controller
           add_readable_field(prop_mode_f);
