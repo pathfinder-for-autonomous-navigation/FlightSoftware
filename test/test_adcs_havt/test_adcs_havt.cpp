78f54dc4b0cab07c039198fa3b9915a890553bc3
Shihao Cao <shihaocao@gmail.com>
2020-01-16 16:55:20 -0500

    pio unit test of havt

--- test/test_adcs_havt/test_adcs_havt.cpp
+++ test/test_adcs_havt/test_adcs_havt.cpp
@@ -4,6 +4,8 @@
 // over I2c to a 3.5 ADCSC with NO devices connected to it to
 // verify MOST of HAVT functionality
 
+// Start with a clean boot of the ADCSC with no failures
+
 // Define the below block or add these build flags
 // -llibc -lc
 // in order to overcome the undefined reference to _write()
@@ -25,10 +27,15 @@ Devices::ADCS adcs(Wire, Devices::ADCS::ADDRESS);
 #include <string>
 #include <unity.h>
 
+// tests have passed where this is 10, try increment higher if not passing
+constexpr static unsigned int wait_for_ADCSC = 10;
+
+// personal sanity check
 void test_trivial(){
     TEST_ASSERT_TRUE(true);
 }
 
+// test to make sure you're connected correctly to i2c
 void test_get_who_am_i(){
     unsigned char temp;
     adcs.get_who_am_i(&temp);
@@ -36,6 +43,8 @@ void test_get_who_am_i(){
     TEST_ASSERT_EQUAL(Devices::ADCS::WHO_AM_I_EXPECTED, temp);
 }
 
+// initial test of read capability, will fail if not a clean boot
+// ex if there was a previous set_havt command that failed
 void test_read_table(){
     std::bitset<havt::max_devices> read_table_read(0);
     std::bitset<havt::max_devices> rt_expected("00000000000000000000001110111000");
@@ -44,10 +53,43 @@ void test_read_table(){
     adcs.get_havt(&read_table_read);
     Serial.printf("read havt: %u\n", read_table_read.to_ulong());
 
+    //.to_string().c_str() turns a bitset into a string of 0 and 1 like
+    //"00000000000000000000001110111000"
     TEST_ASSERT_EQUAL_STRING(rt_expected.to_string().c_str(), read_table_read.to_string().c_str());
 }
 
-void test_setup() {
+// disables then re-enables the RWs
+void test_cmd_table(){
+    //rt stands for read_table
+    std::bitset<havt::max_devices> rt_read(0);
+    std::bitset<havt::max_devices> rt_expected("00000000000000000000001110111000");
+
+    adcs.get_havt(&rt_read);
+
+    TEST_ASSERT_EQUAL_STRING(rt_expected.to_string().c_str(), rt_read.to_string().c_str());
+
+    //command table 1 - disable all the reaction wheels, but leave MTR's up
+    std::bitset<havt::max_devices> cmd_t1("00000000000000000000000000111000");
+
+    adcs.set_havt(cmd_t1);
+    
+    //ensure ADCSC has enough time to implement the commanded havt_table
+    delay(wait_for_ADCSC);
+    adcs.get_havt(&rt_read);
+
+    //check that cmd_t1 was applied
+    TEST_ASSERT_EQUAL_STRING(cmd_t1.to_string().c_str(), rt_read.to_string().c_str());
+
+
+    //NOW TEST RESET CAPABILITY
+    std::bitset<havt::max_devices> cmd_t2("00000000000000000000001110111000");
+    adcs.set_havt(cmd_t2);
+    delay(wait_for_ADCSC);
+    adcs.get_havt(&rt_read);
+    TEST_ASSERT_EQUAL_STRING(cmd_t2.to_string().c_str(), rt_read.to_string().c_str());
+}
+
+void setup_test() {
     Serial.begin(9600);
     while(!Serial)
         ;
@@ -57,11 +99,12 @@ void test_setup() {
 
 int main(void) {
     delay(1000);
-    test_setup();
+    setup_test();
     UNITY_BEGIN();
     RUN_TEST(test_trivial);
     RUN_TEST(test_get_who_am_i);
     RUN_TEST(test_read_table);
+    RUN_TEST(test_cmd_table);
     UNITY_END();
     return 0;
 }
