91559a519a2f502f8de26a33ed9c822499ba2523
kkrol27 <33558436+kkrol27@users.noreply.github.com>
2020-01-21 10:30:42 -0500

    Fully Integrated ADCS Software (#158)

--- tools/run_desktop_flight_tests.sh
+++ tools/run_desktop_flight_tests.sh
@@ -3,14 +3,14 @@
 set -e # Exit on any error
 
 # Verify compilation and unit tests for CI environment
-platformio test -e native_ci -v
+platformio test -e fsw_native_ci -v
 
 # Check for memory mismanagement
-valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose .pio/build/native_ci/program
+valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose .pio/build/fsw_native_ci/program
 
 # Compile and run functional test for environments
-platformio run -e native_ci
-platformio run -e native
+platformio run -e fsw_native_ci
+platformio run -e fsw_native
 
 python -m unittest test.test_dummy_fsw
 python -m unittest test.test_real_fsw
