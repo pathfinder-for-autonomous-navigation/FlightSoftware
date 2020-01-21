2c7d346beee67e89b70ec6b81ac1cef3ac81f463
kkrol27 <kyle.krol.27@gmail.com>
2020-01-21 02:18:07 -0500

    Make CI happy again

--- tools/run_desktop_flight_tests.sh
+++ tools/run_desktop_flight_tests.sh
@@ -3,7 +3,7 @@
 set -e # Exit on any error
 
 # Verify compilation and unit tests for CI environment
-platformio test -e native_ci -v
+platformio test -e fsw_native_ci -v
 
 # Check for memory mismanagement
 valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose .pio/build/fsw_native_ci/program
