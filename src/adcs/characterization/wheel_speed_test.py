a4495de50befc95d71139ad1964bce88aad3b79e
unknown <saa243@cornell.edu>
2020-01-28 17:09:08 -0500

    Moved to python2

--- src/adcs/characterization/wheel_speed_test.py
+++ src/adcs/characterization/wheel_speed_test.py
@@ -5,42 +5,33 @@ src/adcs/characterization/wheel_speed_test.py
 Kyle Krol         (kpk63@cornell.edu)
 Nathan Zimmerberg (nhz2@cornell.edu)
 
-Some utility functions to test the magnetometers.
+A utility function to record data from wheel speed sensor characterization
+tests. Note, this is written in python 2.7 to conform with platfomio.
 """
 
+from __future__ import print_function
+from serial.tools import list_ports
 import serial
 import sys
 
 
 def main():
 
-    # Search for a teensy on serial
-    teensy = None
-    for port in serial.tools.list_ports.comports():
-        if port.manufacturer == 'Teensyduino':
-            teensy = port.device
-            break
-
-    # Ensure a teensy was found
-    if not teensy:
-        print('Could not find a teensy to connect to!', file=sys.stderr)
-        sys.exit(-1)
-
     # Ensure an output file was specified
-    if len(sys.argv) < 2:
+    if len(sys.argv) < 3:
         print('Need to specify an output file like so: python <script> <file>', file=sys.stderr)
         sys.exit(-1)
 
     # Open the output file and serial port
-    with serial.Serial(teensy, 4000000, timeout=20) as istream:
-      with open(sys.argv[1], 'w') as ostream:
+    with serial.Serial(sys.argv[1], 4000000, timeout=20) as istream:
+      with open(sys.argv[2], 'w') as ostream:
 
         # Read until complete is reached
-        line = istream.readline()
+        line = istream.readline().rstrip()
         while not line.startswith('#COMPLETE'):
             print(line, file=sys.stdout)
             print(line, file=ostream)
-            line = istream.readline()
+            line = istream.readline().rstrip()
 
     # Exit on success
     sys.exit(0)
