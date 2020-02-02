3653046bb61e810896f27d719ed3193e61f99f9a
Shihao Cao <shihaocao@gmail.com>
2020-02-01 19:44:56 -0500

    Fault Update (#185)
    * fault cast
    
    * consider pulls
    
    * save work
    
    * bool sr
    
    * fault 2 proper
    
    * big spam
    
    * holy hell
    
    * debug comment
    
    * try to make travis realize
    
    * partial changes
    
    * partial signal remove
    
    * finish fault upgrades
    
    * read to write
    
    * comments update
    
    * remove default
    
    * instant update
    
    * simplifications and comments
    
    * new comments sorry

--- src/fsw/FCCode/Fault.cpp
+++ src/fsw/FCCode/Fault.cpp
@@ -1,50 +1,80 @@
 #include "Fault.hpp"
 
 Fault::Fault(const std::string& name,
-      const size_t _persistence, unsigned int& control_cycle_count,
-      const bool default_setting) :
+      const size_t _persistence, unsigned int& control_cycle_count) :
     WritableStateField<bool>(name, Serializer<bool>()),
     cc(control_cycle_count),
-    persistence(_persistence),
-    suppress_f(name + ".suppress", Serializer<bool>()),
-    override_f(name + ".override", Serializer<bool>())
+    fault_bool_sr(),
+    suppress_f(name + ".suppress", fault_bool_sr),
+    override_f(name + ".override", fault_bool_sr),
+    unsignal_f(name + ".unsignal", fault_bool_sr),
+    // 65536 = 2^16 -1
+    persist_sr(65535),
+    persistence_f(name + ".persistence", persist_sr)
 {
-  set(default_setting);
+  set(false);
   override_f.set(false);
   suppress_f.set(false);
+  unsignal_f.set(false);
+  persistence_f.set(_persistence);
 }
 
 bool Fault::add_to_registry(StateFieldRegistry& r) {
     if (!r.add_writable_field(static_cast<WritableStateFieldBase*>(this))) return false;
     if (!r.add_writable_field(static_cast<WritableStateFieldBase*>(&suppress_f))) return false;
     if (!r.add_writable_field(static_cast<WritableStateFieldBase*>(&override_f))) return false;
+    if (!r.add_writable_field(static_cast<WritableStateFieldBase*>(&unsignal_f))) return false;
+    if (!r.add_writable_field(static_cast<WritableStateFieldBase*>(&persistence_f))) return false;
     return true;
 }
 
 void Fault::signal() {
     if (cc > static_cast<unsigned int>(last_fault_time) || cc == 0) {
-        num_consecutive_faults++;
+        num_consecutive_signals++;
         last_fault_time = cc;
-    }
-
-    if (num_consecutive_faults > persistence) {
-        set(true);
     }
 }
 
 void Fault::unsignal() {
-    set(false);
-    num_consecutive_faults = 0;
+    num_consecutive_signals = 0;
 }
 
-bool Fault::is_faulted() const {
+bool Fault::is_faulted() {
+    process_commands();
+
+    if (num_consecutive_signals > persistence_f.get()) {
+        set(true);
+    }
+    else
+        set(false);
+
     if (override_f.get()) {
-        num_consecutive_faults = 0;
         return true;
     }
     else if (suppress_f.get()) {
-        num_consecutive_faults = 0;
         return false;
     }
     else return get();
 }
+
+void Fault::process_commands(){
+    if(prev_override == false && override_f.get()){
+        num_consecutive_signals = 0;
+    }
+    if(prev_suppress == false && suppress_f.get()){
+        num_consecutive_signals = 0;
+    }
+    prev_override = override_f.get();
+    prev_suppress = suppress_f.get();
+
+    if(unsignal_f.get()){
+        unsignal_f.set(false);
+        num_consecutive_signals = 0;
+    }
+}
+
+#ifdef UNIT_TEST
+unsigned int Fault::get_num_consecutive_signals(){
+    return num_consecutive_signals;
+}
+#endif
--- src/fsw/FCCode/Fault.hpp
+++ src/fsw/FCCode/Fault.hpp
@@ -11,11 +11,9 @@ class Fault : public WritableStateField<bool> {
      * @param name Name of base fault state field.
      * @param _persistence Persistence threshold before signaling fault.
      * @param control_cycle_count Reference to the control cycle count.
-     * @param default_setting Default setting of fault signal.
      */
     Fault(const std::string& name,
-          const size_t _persistence, unsigned int& control_cycle_count,
-          const bool default_setting = false);
+          const size_t _persistence, unsigned int& control_cycle_count);
 
     /**
      * @brief Add fault-related flags to the registry.
@@ -24,27 +22,34 @@ class Fault : public WritableStateField<bool> {
 
     /**
      * @brief Client-facing function to signal an occurrence of the
-     * fault-related condition.
+     * fault-related condition. Attempts to increment num_consecutive_signals by 1
      */
     void signal();
 
     /**
      * @brief Client-facing function to signal a non-occurrence of the
-     * fault-related condition. Resets the persistence of the fault.
+     * fault-related condition. Resets the num_consecutive_signals to 0
      */
     void unsignal();
 
     /**
-     * @brief If the fault-related condition has been persistent or if
-     * the fault is overridden by the ground, the fault is signaled.
-     * Otherwise, if the fault-related condition has not been persistent,
-     * or if the fault is suppressed by the ground, the fault is not signaled.
+     * @brief Calls process_commands, sets the fault true if num_consecutive_faults > persistence_f
+     * returns true or false if override or suppress are true respectivly,
+     * Otherwise, it returns the state of the fault itself.
      *
-     * @return true 
-     * @return false 
+     * @return true if: override is true, or num_consecutive_faults > persistence_f
+     * @return false if: suppress is true, or num_consecutive_faults < persistence_f 
      */
-    bool is_faulted() const;
+    bool is_faulted();
 
+    #ifdef UNIT_TEST
+    /**
+     * @brief a debug return that tells the current consecutive signals
+     * 
+     * @return unsigned int 
+     */
+    unsigned int get_num_consecutive_signals();
+    #endif
   private:
     // Make the get() and set() methods of the state field private,
     // so that the user is forced to use the signal() and unsignal()
@@ -56,16 +61,30 @@ class Fault : public WritableStateField<bool> {
     unsigned int last_fault_time = 0; // Last control cycle # that the fault condition
                                       // occurred
 
-    unsigned int persistence; // Persistence threshold for fault signal
-    mutable unsigned int num_consecutive_faults = 0; // Number of consecutive fault condition
+    mutable unsigned int num_consecutive_signals = 0; // Number of consecutive signal condition
                                                      // occurrences at the current moment.
+    
+    // Keeps track of the previous suppress or override state
+    bool prev_suppress = false;
+    bool prev_override = false;
 
     /**
-     * @brief State fields that can be set by the ground to suppress
-     * or forcibly signal the fault.
+     * @brief State fields that can be set by the ground to
+     * suppress, override, signal and unsignal the fault
      */
+    Serializer<bool> fault_bool_sr;
     WritableStateField<bool> suppress_f;
     WritableStateField<bool> override_f;
+    WritableStateField<bool> unsignal_f;
+
+    Serializer<unsigned int> persist_sr;
+    WritableStateField<unsigned int> persistence_f;
+
+    /**
+     * @brief Resets consecutive faults to 0, whenever pulls transition from false to true,
+     * or if unsignal_f is true.
+     */
+    void process_commands();
 };
 
 #endif
