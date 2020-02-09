c462b080c4a3059f730e4e1fef95e35a8d7fe364
Shihao Cao <shihaocao@gmail.com>
2020-02-09 00:10:11 -0500

    emplace

--- src/fsw/FCCode/ADCSBoxController.cpp
+++ src/fsw/FCCode/ADCSBoxController.cpp
@@ -42,7 +42,7 @@ ADCSBoxController::ADCSBoxController(StateFieldRegistry &registry,
             std::memset(buffer, 0, sizeof(buffer));
             sprintf(buffer,"adcs_cmd.havt_reset");
             sprintf(buffer + strlen(buffer), "%u", idx);
-            havt_cmd_reset_vector_fp.push_back(find_writable_field<bool>(buffer, __FILE__, __LINE__));
+            havt_cmd_reset_vector_fp.emplace_back(find_writable_field<bool>(buffer, __FILE__, __LINE__));
         }
         havt_cmd_disable_vector_fp.reserve(adcs::havt::Index::_LENGTH);
         for(unsigned int idx = adcs::havt::Index::IMU_GYR; idx < adcs::havt::Index::_LENGTH; idx++)
@@ -50,7 +50,7 @@ ADCSBoxController::ADCSBoxController(StateFieldRegistry &registry,
             std::memset(buffer, 0, sizeof(buffer));
             sprintf(buffer,"adcs_cmd.havt_disable");
             sprintf(buffer + strlen(buffer), "%u", idx);
-            havt_cmd_disable_vector_fp.push_back(find_writable_field<bool>(buffer, __FILE__, __LINE__));
+            havt_cmd_disable_vector_fp.emplace_back(find_writable_field<bool>(buffer, __FILE__, __LINE__));
         }
     }
 
