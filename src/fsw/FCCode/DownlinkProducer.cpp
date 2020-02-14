37d48974384a70a252d343ef4297944cf7ee5da0
fatimayousuf <fatima.yousuf.0@gmail.com>
2020-02-14 16:59:56 -0500

    .

--- src/fsw/FCCode/DownlinkProducer.cpp
+++ src/fsw/FCCode/DownlinkProducer.cpp
@@ -275,6 +275,6 @@ void DownlinkProducer::shift_flow_priorities(unsigned char id1, unsigned char id
     flows[idx2].is_active = temp;
 
     flows.insert(flows.begin() + min(idx1, idx2), flows[max(idx1, idx2)]);
-    flows.erase(max(idx1,idx2));
+    flows.erase(flows.begin()+max(idx1,idx2)+1);
 
 }
