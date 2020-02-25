92b813ebfd8191728bcebf2d1cddc55cb8bb83d2
fatimayousuf <fatima.yousuf.0@gmail.com>
2020-02-25 02:49:49 -0500

    .

--- test/test_fsw_state_field_registry_mock/test_fsw_state_field_registry_mock.cpp
+++ test/test_fsw_state_field_registry_mock/test_fsw_state_field_registry_mock.cpp
@@ -154,10 +154,6 @@ void test_create_fault() {
 
     registry.create_fault("foo", 1, 300);
     TEST_ASSERT_NOT_NULL(registry.find_fault("foo"));
-    TEST_ASSERT(registry.find_writable_field("fault"));
-    TEST_ASSERT(registry.find_writable_field("fault.override"));
-    TEST_ASSERT(registry.find_writable_field("fault.suppress"));
-    TEST_ASSERT(registry.find_writable_field("fault.unsignal"));
 }
 
 /**
