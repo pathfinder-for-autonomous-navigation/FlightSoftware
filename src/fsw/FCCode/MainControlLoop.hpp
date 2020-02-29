1af94ba308f4fa2062bb6d6650cc56a652f1d239
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2020-02-28 20:00:20 -0500

    Betta numba

--- src/fsw/FCCode/MainControlLoop.hpp
+++ src/fsw/FCCode/MainControlLoop.hpp
@@ -64,8 +64,8 @@ class MainControlLoop : public ControlTask<void> {
     #ifdef FUNCTIONAL_TEST
     // https://cornellprod-my.sharepoint.com/:x:/r/personal/saa243_cornell_edu/_layouts/15/Doc.aspx?sourcedoc=%7B04C55BBB-7AED-410B-AC43-67352393D6D5%7D&file=Flight%20Software%20Cycle.xlsx&action=default&mobileredirect=true&cid=e2b9bd89-7037-47bf-ad2a-fd8b25808939
         static constexpr unsigned int piksi_control_task_offset  =   5500;
-        static constexpr unsigned int adcs_monitor_offset        =  55000;
-        static constexpr unsigned int debug_task_offset          =  70500;
+        static constexpr unsigned int adcs_monitor_offset        =   7500;
+        static constexpr unsigned int debug_task_offset          =  35500;
         static constexpr unsigned int attitude_estimator_offset  =  85500;
         static constexpr unsigned int gomspace_controller_offset = 106500;
         static constexpr unsigned int uplink_consumer_offset     = 111500;
@@ -80,7 +80,7 @@ class MainControlLoop : public ControlTask<void> {
         static constexpr unsigned int eeprom_controller_offset   = 153500;  // fix this later
     #else
         static constexpr unsigned int piksi_control_task_offset  =   5500;
-        static constexpr unsigned int adcs_monitor_offset        =   6500;
+        static constexpr unsigned int adcs_monitor_offset        =   7500;
         static constexpr unsigned int debug_task_offset          =  35000;
         static constexpr unsigned int attitude_estimator_offset  =  35500;
         static constexpr unsigned int gomspace_controller_offset =  56500;
