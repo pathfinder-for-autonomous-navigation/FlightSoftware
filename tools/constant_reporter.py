535e79cc308cdb35e88af2513ac0d5f9f2462ba5
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2020-03-09 00:29:11 -0400

    Added warnings about incomplete regexes

--- tools/constant_reporter.py
+++ tools/constant_reporter.py
@@ -1,14 +1,21 @@
 import os,re
 
-tracked_const_finder = r"TRACKED_CONSTANT_?S?C?\(\s*(?P<type>[\w<>:\s]*[\w<>:])\s*,\s*(?P<id>[\w]+)\s*,\s*(?P<val>.*)\)"
+tracked_const_finder = r"TRACKED_CONSTANT_?S?C?\(\s*(?P<type>[\w<>&:\s]*[\w<>&:])\s*,\s*(?P<id>[\w]+)\s*,\s*(?P<val>.*)\)"
 consts = []
+log = "Keep this file in version control so that changes to constants are well-known.\n\n"
 
 for root, dirs, files in os.walk("src"):
     for file in files:
         path = os.path.join(root, file)
         if path.endswith((".cpp", ".hpp", ".c", ".h")):
             with open(path, 'r') as f:
-                found_consts = re.findall(tracked_const_finder, f.read())
+                text = f.read()
+                found_decls = re.findall(r"TRACKED_CONSTANT", text)
+                found_consts = re.findall(tracked_const_finder, text)
+
+                if len(found_consts) != len(found_decls):
+                    log += f"{path}: regex error: only found {len(found_consts)} out of {len(found_decls)} tracked constant declarations.\n"
+
                 for const in found_consts:
                     consts.append({
                         "name" : const[1].strip(),
@@ -17,7 +24,7 @@ for root, dirs, files in os.walk("src"):
                         "file" : path
                     })
 
-log = "Keep this file in version control so that changes to constants are well-known.\n\n"
+log += "\n"
 for const in consts:
     log += f"{const['file']}: \"{const['name']}\" = \"{const['val']}\"\n"
 
