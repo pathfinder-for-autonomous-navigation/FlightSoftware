74c063d365de8583a9301af77462e4052c7427f8
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2020-03-10 13:32:36 -0400

    Fix unit tests

--- src/flow_data.cpp
+++ src/flow_data.cpp
@@ -1,6 +1,6 @@
 #include "flow_data.hpp"
 
 const std::vector<DownlinkProducer::FlowData> PAN::flow_data = {
-{1, "true", {"pan.state", "pan.deployed", "pan.sat_designation"}},
+{1, "true", {"pan.state", "pan.sat_designation"}},
 {2, "true", {"docksys.docked", "docksys.dock_config", "docksys.is_turning"}}
 };
