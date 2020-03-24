43aa9258257245b44d953c3b49dfef51d3df6cdb
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2020-03-24 16:25:30 -0400

    Remove valgrind

--- tools/run_common_software_tests.sh
+++ tools/run_common_software_tests.sh
@@ -4,6 +4,3 @@ set -e
 # Run unit tests
 platformio test -e native_common -v
 platformio test -e native_common_ci -v
-
-# Check for memory mismanagement
-valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose .pio/build/native_common_ci/program
