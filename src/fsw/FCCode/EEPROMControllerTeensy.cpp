4bee38673c584e396a7428097e5880b24279f624
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2020-03-25 23:39:59 -0400

    Convert .at() to []

--- src/fsw/FCCode/EEPROMControllerTeensy.cpp
+++ src/fsw/FCCode/EEPROMControllerTeensy.cpp
@@ -9,12 +9,12 @@ EEPROMController::EEPROMController(StateFieldRegistry &registry, unsigned int of
 
 void EEPROMController::read_EEPROM(){
   for (unsigned int i = 0; i<_registry.eeprom_saved_fields.size(); i++){
-    _registry.eeprom_saved_fields.at(i)->set_from_eeprom(EEPROM.read(addresses.at(i)));
+    _registry.eeprom_saved_fields[i]->set_from_eeprom(EEPROM.read(addresses[i]));
   }
 }
 
 void EEPROMController::update_EEPROM(unsigned int position){
-  EEPROM.put(addresses.at(position), _registry.eeprom_saved_fields.at(position)->get_eeprom_repr());
+  EEPROM.put(addresses[position], _registry.eeprom_saved_fields[position]->get_eeprom_repr());
 }
 
 bool EEPROMController::check_empty(){
