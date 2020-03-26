fa7b3c5337f36478d67d916f4a346243b441a7a4
Shihao Cao <shihaocao@gmail.com>
2020-03-26 17:59:18 -0400

    begin work

---
+++ ptest/cases/adcs_checkout_case.py
@@ -0,0 +1,13 @@
+# ADCSCheckoutCase. Verifies the functionality of the ADCS.
+from .base import SingleSatOnlyCase
+
+class ADCSCheckoutCase(SingleSatOnlyCase):
+    def setup_case_singlesat(self):
+        self.sim.flight_controller.write_state("pan.state", 11) # Mission State = Manual
+        self.sim.flight_controller.write_state("adcs.state", 5) # ADCS State = Manual
+        self.sim.flight_controller.write_state("adcs_cmd.rwa_mode", 1) # Speed Control
+        self.sim.flight_controller.write_state("adcs_cmd.rwa_speed_cmd", "0, 0, 0") # 0 speed to begin with
+        self.sim.flight_controller.write_state("dcdc.ADCSMotor_cmd", "true")
+
+    def run_case_singlesat(self):
+        self.cycle_no = self.sim.flight_controller.read_state("pan.cycle_no")
