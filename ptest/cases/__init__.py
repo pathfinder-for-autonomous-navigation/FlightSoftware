c30e455f91e16ca416946ac73bc615951fc7e3da
athena255 <athena.cheung97@gmail.com>
2020-03-26 17:36:05 -0400

    Added PropStateMachineCase

--- ptest/cases/__init__.py
+++ ptest/cases/__init__.py
@@ -3,4 +3,5 @@ from .mission import DeploymentToStandby
 from .gomspace_checkout_case import GomspaceCheckoutCase
 from .dcdc_checkout_case import DCDCCheckoutCase
 from .prop_checkout_case import PropCheckoutCase
+from .prop_state_machine_case import PropStateMachineCase
 from .spin_motors_case import SpinMotorsCase
