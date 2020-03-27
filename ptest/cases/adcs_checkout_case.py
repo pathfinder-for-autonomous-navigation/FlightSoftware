7e4e05f7e173cf9d4f1fd73232565c209f0178ed
Shihao Cao <shihaocao@gmail.com>
2020-03-26 20:17:15 -0400

    use property

--- ptest/cases/adcs_checkout_case.py
+++ ptest/cases/adcs_checkout_case.py
@@ -2,10 +2,25 @@
 from .base import SingleSatOnlyCase
 
 class ADCSCheckoutCase(SingleSatOnlyCase):
-    def setup_case_singlesat(self):
-        self.cycle_no = -1
-        self.adcs_func = False
 
+    @property
+    def adcs_func(self):
+        return self.sim.flight_controller.read_state("adcs_monitor.functional")
+    
+    @property
+    def cycle_no(self):
+        return self.sim.flight_controller.read_state("pan.cycle_no")
+
+    @property
+    def havt_read(self):
+        self.havt_length = 18 # _LENGTH in havt_devices.hpp
+        read_list = [False for x in range(self.havt_length)]
+        for x in range(self.havt_length):
+            read_list = self.sim.flight_controller.read_state("adcs_monitor.havt_device"+str(x))
+        read_list = [True if x is true else False for x in read_list]
+        return read_list
+
+    def setup_case_singlesat(self):
         self.havt_length = 18 # _LENGTH in havt_devices.hpp
         self.havt_device = [False for x in range(self.havt_length)]
 
@@ -15,19 +30,12 @@ class ADCSCheckoutCase(SingleSatOnlyCase):
         self.sim.flight_controller.write_state("adcs_cmd.rwa_speed_cmd", "0, 0, 0") # 0 speed to begin with
         self.sim.flight_controller.write_state("dcdc.ADCSMotor_cmd", "true")
 
+    """Steps the FC forward by one step"""
     def step(self):
         self.sim.flight_controller.write_state("cycle.start", "true")
 
-    def update_vars(self):
-        self.cycle_no = self.sim.flight_controller.read_state("pan.cycle_no")
-        self.adcs_func = self.sim.flight_controller.read_state("adcs_monitor.functional")
-
-        for x in range(self.havt_length):
-            self.havt_device[x] = self.sim.flight_controller.read_state("adcs_monitor.havt_device"+str(x))
-
     def run_case_singlesat(self):
-        self.update_vars()
         print("Cycle Number: "+str(self.cycle_no))
+        print("ADCS Functional: "+str(type(self.adcs_func)))
         self.step()
-        self.update_vars()
         print("Cycle Number: "+str(self.cycle_no))
