e3f0d38fd91f0a802c03814087fcdd3f3d0e8c34
Shihao Cao <shihaocao@gmail.com>
2020-03-31 00:16:37 -0400

    Add auto logic

--- src/fsw/FCCode/DebugTask.cpp
+++ src/fsw/FCCode/DebugTask.cpp
@@ -7,6 +7,7 @@ DebugTask::DebugTask(StateFieldRegistry &registry, unsigned int offset)
       auto_cycle_f("cycle.auto", Serializer<bool>()) {
   add_writable_field(start_cycle_f);
   add_writable_field(auto_cycle_f);
+  auto_cycle_f.set(false);
   init();
 }
 #else
@@ -19,13 +20,13 @@ DebugTask::DebugTask(StateFieldRegistry &registry, unsigned int offset)
 void DebugTask::execute() {
 #ifdef FUNCTIONAL_TEST
   start_cycle_f.set(false);
-
   if(auto_cycle_f.get()){
-  while (!start_cycle_f.get())
     process_commands(_registry);
   }
   else{
-    process_commands(_registry);
+    // !auto_cycle_f.get() is false once auto_cycle_f is true, ending the while loop
+    while (!start_cycle_f.get() && !auto_cycle_f.get()) 
+      process_commands(_registry);
   }
 #endif
 }
