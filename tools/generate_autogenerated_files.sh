437617dab2233344b0fc91b99b3b29852826793a
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2020-04-10 20:35:48 -0400

    Add comment

--- tools/generate_autogenerated_files.sh
+++ tools/generate_autogenerated_files.sh
@@ -1,3 +1,6 @@
+# Run this file prior to opening a PR, so that your code will not fail
+# on the file diff checks in tools/run_ground_tests.sh
+
 # Generate `telemetry` file
 pio run -e gsw_telem_info_generator
 .pio/build/gsw_telem_info_generator/program telemetry
