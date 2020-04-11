2b5c6189fbc7f0388fa5c9577362a0c7d3cd50df
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2020-04-10 20:51:33 -0400

    Remove pre-existing files

--- tools/generate_autogenerated_files.sh
+++ tools/generate_autogenerated_files.sh
@@ -1,12 +1,15 @@
 # Run this file prior to opening a PR, so that your code will not fail
 # on the file diff checks in tools/run_ground_tests.sh
 
-# Generate `telemetry` file
-pio run -e gsw_telem_info_generator
-.pio/build/gsw_telem_info_generator/program telemetry
+# Generate flow_data.cpp
+rm -f src/flow_data.cpp
+python src/flow_data_generator.py
 
 # Generate `constants` file
+rm -f constants
 python tools/constant_reporter.py
 
-# Generate flow_data.cpp
-python src/flow_data_generator.py
+# Generate `telemetry` file
+rm -f telemetry
+pio run -e gsw_telem_info_generator
+.pio/build/gsw_telem_info_generator/program telemetry
