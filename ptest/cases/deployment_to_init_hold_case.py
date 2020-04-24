85ea76b459b893bdb61ae394f1d4e32a5008d1fc
fatimayousuf <fatima.yousuf.0@gmail.com>
2020-04-23 19:26:05 -0700

    small change :)

--- ptest/cases/deployment_to_init_hold_case.py
+++ ptest/cases/deployment_to_init_hold_case.py
@@ -69,7 +69,6 @@ class DeploymentToInitHoldCheckoutCase(SingleSatOnlyCase):
         self.finish()
 
     def reset_faults(self):
-        self.mission_mode = 11
         self.adcs_is_functional = "true"
         self.wheel1_is_functional = "true"
         self.wheel2_is_functional = "true"
