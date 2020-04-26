eb700dfcc7eafe8dee845c18adc391ed66612d2f
Kirin Kambon <nirik8801@gmail.com>
2020-04-26 00:22:41 -0400

    adding battery object with handlers and battery telemetry

---
+++ MCT/server-files/server.js
@@ -0,0 +1,39 @@
+/**
+ * Basic implementation of a history and realtime server.
+ */
+
+
+var Spacecraft = require('./spacecraft');
+var RealtimeServer = require('./realtime-server');
+var HistoryServer = require('./history-server');
+var Battery = require('./battery-telemetry');
+
+var expressWs = require('express-ws');
+var express = require('express');
+var app = require('express')();
+
+expressWs(app);
+
+if (process.argv[2] == "test") {
+    var battery = new Battery();
+
+}
+else {
+    var spacecraft = new Spacecraft();
+}
+
+var realtimeServer = new RealtimeServer(battery);
+var historyServer = new HistoryServer(battery);
+
+app.use('/realtime', realtimeServer);
+app.use('/history', historyServer);
+app.use('/openmct', express.static('node_modules/openmct/dist/'));
+app.use('/', express.static("public"));
+
+var port = process.env.PORT || 8080
+
+app.listen(port, function () {
+    console.log('Open MCT hosted at http://localhost:' + port);
+    console.log('History hosted at http://localhost:' + port + '/history');
+    console.log('Realtime hosted at ws://localhost:' + port + '/realtime');
+});
