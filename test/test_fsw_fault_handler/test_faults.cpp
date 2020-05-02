1ac94c04c8263774f8160ac2a9438a92cb863afe
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2020-05-02 14:45:14 -0400

    Remove PFH comment

--- test/test_fsw_fault_handler/test_faults.cpp
+++ test/test_fsw_fault_handler/test_faults.cpp
@@ -10,7 +10,7 @@ void test_faults() {
     // TODO
     // RUN_TEST(test_prop_overpressure_fault_handler);
     RUN_TEST(test_quake_fault_handler);
-    // RUN_TEST(test_piksi_fault_handler);
+    RUN_TEST(test_piksi_fault_handler);
     RUN_TEST(test_fault_handling_end_to_end);
 }
 
