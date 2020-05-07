8e30c74bc0d87a2f853fd1d61a09d4e876f7477d
fatimayousuf <fatima.yousuf.0@gmail.com>
2020-05-07 11:32:42 -0700

    command prompt doesn't work?

--- ptest/http_cmd.py
+++ ptest/http_cmd.py
@@ -4,6 +4,7 @@ from flasgger.utils import swag_from
 from datetime import datetime
 from argparse import ArgumentParser
 import json, sys, logging
+import os, threading
 
 """
 This file contains HTTP endpoint factories for StateSession and RadioSession.
@@ -38,13 +39,28 @@ def create_radio_session_endpoint(radio_session):
     app.logger.disabled = True
     app.config["radio_session"] = radio_session
 
-    app.config["SWAGGER"]={"title": "PAN Radio Session Command Endpioint", "uiversion": 2}
+    app.config["SWAGGER"]={"title": "PAN Radio Session Command Endpoint", "uiversion": 2}
     swagger=Swagger(app, config=swagger_config)
 
-    @app.route("/request", methods=["GET"])
+    @app.route("/request", methods=["GET", "POST"])
     @swag_from("endpoint_configs/radio_session/request.yml")
-    def request():
-        return "Hello world!"
+    def send_telem():
+        uplink=request.get_json()
+
+        fields, vals=list(), list()
+        for field_val in uplink:
+            fields.append(field_val["field"])
+            vals.append(field_val["value"])
+
+        uplink_console = app.config["uplink_console"]
+        success = uplink_console.create_uplink(fields, vals, "uplink.sbd") and os.path.exists("uplink.sbd")
+
+        # Send the uplink to FlightSoftware
+
+
+        if success:
+            return "Successfully sent telemetry to State Session"
+        return "Unable to send telemetry"
 
     return app
 
@@ -53,12 +69,51 @@ def create_state_session_endpoint(state_session):
     app.logger.disabled = True
     app.config["state_session"] = state_session
 
-    app.config["SWAGGER"]={"title": "PAN State Session Command Endpioint", "uiversion": 2}
+    app.config["SWAGGER"]={"title": "PAN State Session Command Endpoint", "uiversion": 2}
     swagger=Swagger(app, config=swagger_config)
 
-    @app.route("/request", methods=["GET"])
+    @app.route("/request", methods=["POST"])
     @swag_from("endpoint_configs/state_session/request.yml")
-    def request():
-        return "Hello world!"
+    def send_telem():
+        # uplink=request.get_json()
+
+        # # Create an uplink packet
+        # fields, vals=list(), list()
+        # for field_val in uplink:
+        #     fields.append(field_val["field"])
+        #     vals.append(field_val["value"])
+
+        # uplink_console = app.config["uplink_console"]
+        # console = app.config["console"]
+        # success = uplink_console.create_uplink(fields, vals, "uplink.sbd") and os.path.exists("uplink.sbd")
+
+        # if success:
+        #     # Extract uplink data from created sbd file
+        #     try:
+        #         file = open("uplink.sbd", "rb")
+        #     except:
+        #         return "Unable to send telemetry"
+
+        #     uplink_packet = file.read()
+        #     uplink_packet_length = len(uplink_packet)
+        #     file.close() 
+        #     uplink_packet = str(''.join(r'\x'+hex(byte)[2:] for byte in uplink_packet)) #get the hex representation of the packet bytes
+
+            # Send a command to the Flight Software console to process the uplink packet
+            # json_cmd = {
+            #     'mode': ord('u'),
+            #     'val': uplink_packet,
+            #     'length': uplink_packet_length
+            # }
+            # json_cmd = json.dumps(json_cmd) + "\n"
+
+            # device_write_lock = threading.Lock()
+            # device_write_lock.acquire()
+            # console.write(json_cmd.encode())
+            # device_write_lock.release()
+
+            # return "Successfully sent telemetry to State Session"
+
+        return "Unable to send telemetry"
 
     return app
--- ptest/state_session.py
+++ ptest/state_session.py
@@ -96,7 +96,9 @@ class StateSession(object):
 
         try:
             self.flask_app = create_state_session_endpoint(self)
-            self.http_thread = Process(name=f"{self.device_name} HTTP Command Endpoint", target=self.flask_app.run, kwargs={"port": self.port})
+            #self.flask_app.config["uplink_console"] = self.uplink_console
+            #self.flask_app.config["console"] = self.console
+            self.http_thread = Process(name=f"{self.device_name} HTTP Command Endpoint", target=self.flask_app.run, kwargs={"debug":True,"port": self.port})
             self.http_thread.start()
             print(f"{self.device_name} HTTP command endpoint is running at http://localhost:{self.port}")
             return True
