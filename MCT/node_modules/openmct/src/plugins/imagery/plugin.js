4c161c77df9219f5855d875891a7e30311071c6a
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2020-06-13 09:47:09 -0400

    Don't let node_modules get committed to Git

--- MCT/node_modules/openmct/src/plugins/imagery/plugin.js
+++
@@ -1,8 +0,0 @@
-import ImageryViewProvider from './ImageryViewProvider';
-
-export default function () {
-    return function install(openmct) {
-        openmct.objectViews.addProvider(new ImageryViewProvider(openmct));
-    };
-}
-
