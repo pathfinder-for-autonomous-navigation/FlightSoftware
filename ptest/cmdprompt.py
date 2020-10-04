ca99f3370b60ed8266a899f964668f23172c593f
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2020-10-04 14:40:48 -0400

    Remove unused USB Session functions

--- ptest/cmdprompt.py
+++ ptest/cmdprompt.py
@@ -29,9 +29,8 @@ class StateCmdPrompt(Cmd):
     Teensies and simulation devices.
     '''
 
-    def __init__(self, devices, radios, sim, exit_fn):
+    def __init__(self, devices, radios, exit_fn):
         self.devices = {**devices, **radios}
-        self.sim = sim
         self.exit_fn = exit_fn
 
         if not self.devices:
@@ -88,21 +87,6 @@ class StateCmdPrompt(Cmd):
             return
 
         print(f"Switched to {self.cmded_device.device_name}")
-
-    def do_checksim(self, args):
-        '''
-        Check the running status of the simulation.
-        '''
-        if self.sim.running:
-            print("Running ({} of {}s)".format(format(self.sim.sim_time,"0.2f"), self.sim.sim_duration))
-        else:
-            print("Not running")
-
-    def do_endsim(self, args):
-        '''
-        End the simulation, if it's running.
-        '''
-        self.sim.running = False
 
     def do_rs(self, args):
         '''
@@ -227,27 +211,6 @@ class StateCmdPrompt(Cmd):
 
         write_succeeded = "Succeeded" if write_succeeded else "Failed"
         print(f"{write_succeeded} \t\t\t\t\t\t(Completed in {elapsed_time} us)")
-
-    @USBSessionOnly
-    def do_os(self, args):
-        '''
-        Override simulation state. See usb_session.py for documentation.
-        '''
-        args = args.split()
-        start_time = timeit.default_timer()
-        override_succeeded = self.cmded_device.override_state(args[0], args[1])
-        elapsed_time = int((timeit.default_timer() - start_time) * 1E6)
-
-        override_succeeded = "Succeeded" if override_succeeded else "Failed"
-        print(f"{override_succeeded} \t\t\t\t\t\t(Completed in {elapsed_time} us)")
-
-    @USBSessionOnly
-    def do_ro(self, args):
-        '''
-        Release override of simulation state. See usb_session.py for documentation.
-        '''
-        args = args.split()
-        self.cmded_device.release_override(args[0])
 
     def do_plot(self, args):
         '''
