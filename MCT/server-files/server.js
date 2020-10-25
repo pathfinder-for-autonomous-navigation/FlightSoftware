10fc86b3e88c8d3fbde8ccd4686c95a0d9c39c75
Duncan McDonald <dam342@cornell.edu>
2020-10-25 14:42:38 -0400

    Made Server Https

--- MCT/server-files/server.js
+++ MCT/server-files/server.js
@@ -6,11 +6,15 @@ var Battery = require('./battery-telemetry');
 var Spacecraft = require('./spacecraft');
 var RealtimeServer = require('./realtime-server');
 var HistoryServer = require('./history-server');
-
-
 var expressWs = require('express-ws');
 var express = require('express');
-var app = require('express')();
+var fs = require('fs');
+var https = require('https');
+
+var privateKey  = fs.readFileSync('./key.pem', 'utf8');
+var certificate = fs.readFileSync('./cert.pem', 'utf8');
+var credentials = {key: privateKey, cert: certificate};
+var app = express()
 
 expressWs(app);
 
@@ -28,10 +32,11 @@ app.use('/history', historyServer);
 app.use('/openmct', express.static('node_modules/openmct/dist/'));
 app.use('/', express.static("public"));
 
+var httpsServer = https.createServer(credentials, app);
 var port = process.env.PORT || 8080
 
-app.listen(port, function () {
-    console.log('Open MCT hosted at http://localhost:' + port);
-    console.log('History hosted at http://localhost:' + port + '/history');
+httpsServer.listen(port, function () {
+    console.log('Open MCT hosted at https://localhost:' + port);
+    console.log('History hosted at https://localhost:' + port + '/history');
     console.log('Realtime hosted at ws://localhost:' + port + '/realtime');
 });
