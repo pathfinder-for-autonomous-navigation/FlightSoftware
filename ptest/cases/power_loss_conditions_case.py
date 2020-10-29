25493496469231da8d6db39c354fefc12d3e01ee
Mahika Kudlugi <mk895@cornell.edu>
2020-10-29 18:57:46 -0400

    power loss case updates

--- ptest/cases/power_loss_conditions_case.py
+++ ptest/cases/power_loss_conditions_case.py
@@ -8,10 +8,13 @@ class PowerLossCheckoutCase(SingleSatOnlyCase):
     def sim_duration(self):
         return float("inf")
 
+    @property
+    def desired_initial_state(self):
+        return "standby"
+
     def setup_post_bootsetup(self):
         self.ws("fault_handler.enabled", True)
 
-    # is str_to_bool necessary?
     def str_to_bool(self, string):
             if string == "true":
                 return True
@@ -24,9 +27,6 @@ class PowerLossCheckoutCase(SingleSatOnlyCase):
     def check_powercycle(self):
         return self.str_to_bool(self.read_state("gomspace.power_cycle_output3_cmd"))
 
-    def read_radio_state(self):
-        return int(self.read_state("radio.state"))
-
     # QFH should want to power cycle radio, but if QM is not in wait, it should not power cycle
     def check_faulty_powercycle(self):
         check_powercycle = self.check_powercycle()
@@ -34,6 +34,9 @@ class PowerLossCheckoutCase(SingleSatOnlyCase):
         if radio_state > 0 and check_powercycle:
             self.failed = True
             self.logger.put("QuakeFaultHandler power cycled the output channel when QuakeManager was not in wait.")
+        if radio_state == 0 and not check_powercycle:
+            self.failed = True
+            self.logger.put("QuakeFaultHandler failed to power cycle the output channel when QuakeManager in wait.")
 
     # What seems to be happening at least in the instances I've run this is
     # radio state is always > 0 by the time we are checking for power cycle
@@ -45,9 +48,11 @@ class PowerLossCheckoutCase(SingleSatOnlyCase):
     # starting states?
 
     def diagnostics(self):
-        self.read_state('radio.state')
-        self.read_state('qfh.state')
-        self.read_state('radio.last_comms_ccno')
+        self.read_state("radio.state")
+        self.read_state("qfh.state")
+        self.read_state("radio.last_comms_ccno")
+        self.read_state("gomspace.power_cycle_output3_cmd")
+        self.read_state("gomspace.output.output3")
 
     def run_case_singlesat(self):
         self.failed = False
@@ -57,9 +62,6 @@ class PowerLossCheckoutCase(SingleSatOnlyCase):
         # Check that if QM is a non-wait state and QFH wants to powercycle the 
         # radio, QFH is unable to until QM returns to a wait state
 
-
-        # QFH States 2,3,4,5 represent powercycling has occurred 
-
         # The satellite has been in a blackout since startup.
         #
         # Note: we subtract 1 because the control cycle count starts at 1.
@@ -67,22 +69,51 @@ class PowerLossCheckoutCase(SingleSatOnlyCase):
         # cycles since the blackout started.
         self.cycles_since_blackout_start = self.rs("pan.cycle_no") - 1
 
-        # Simulate one day of no comms (is this necessary idk)
-        while self.cycles_since_blackout_start <= self.one_day_ccno:
+        # Simulate one day of no comms
+        self.logger.put(f"Creating a comms blackout of 24 hours (measured from control cycle #1.) Current control cycle: {self.rs('pan.cycle_no')}")
+        while self.cycles_since_blackout_start < self.one_day_ccno:
             self.cycle()
             self.diagnostics()
             self.cycles_since_blackout_start += 1
+            
+            #pug = self.read_state("qfh.state")
+            #self.logger.put(f"qfh state: {pug}")
         
         # Reset cycles
         self.cycles_since_blackout_start = 0
 
+        last_qfh_state = self.read_state("qfh.state")
+
         # Simulate second day of no comms
-        while self.cycles_since_blackout_start <= self.one_day_ccno:
+        self.logger.put(f"Creating another comms blackout of 24 hours, starting on control cycle {self.rs('pan.cycle_no')}")
+        while self.cycles_since_blackout_start < self.one_day_ccno:
             self.cycle()
+            
+
+            curr_cycle = self.rs("pan.cycle_no")
+            curr_qfh_state = self.read_state("qfh.state")
+            if curr_qfh_state > last_qfh_state:
+                powercycle_flag = self.check_powercycle()
+                self.logger.put(f"qfh state incremented on cycle {curr_cycle}")
+                self.logger.put(f"qfh state incremented {curr_qfh_state}")
+                self.logger.put(f"powercycle flag was set to {powercycle_flag}")
+
+            last_qfh_state = curr_qfh_state
+
+
             self.diagnostics()
             self.cycles_since_blackout_start += 1
 
+
+        # After second day of no comms, qfh should powercycle to 1st PC state
+        curr_cycle = self.rs("pan.cycle_no")
+        qfh_state = self.read_state("qfh.state")
+        self.logger.put(f"Second day of no comms ends on cycle {curr_cycle}")
+        self.logger.put(f"QFH state after second day is {qfh_state}")
+
+        # Qfh should want to powercycle into state powercycle_1
         self.check_faulty_powercycle()
+       
 
         self.powercycles_count = 0
 
@@ -90,22 +121,45 @@ class PowerLossCheckoutCase(SingleSatOnlyCase):
         for x in range(3):
             # Reset cycles
             self.cycles_since_blackout_start = 0
+            self.logger.put(f"Power cycle round  {x+1}:")
 
-            # Cycle a whole buncha times
-            while self.cycles_since_blackout_start <= self.one_day_ccno // 3:
+            curr_cycle = self.rs("pan.cycle_no")
+            qfh_state = self.read_state("qfh.state")
+            self.logger.put(f"1/3 day of powercycle state ends on cycle {curr_cycle}")
+            self.logger.put(f"QFH state after 1/3 day is {qfh_state}")
+
+            # Cycle until QFH is ready to powercycle
+            while self.cycles_since_blackout_start < self.one_day_ccno / 3:
                 self.cycle()
                 self.diagnostics()
                 self.cycles_since_blackout_start += 1
 
-            # Debugging
-            self.logger.put(f"Power cycle round  {x+1}:")
-            self.logger.put(f"Radio state is {self.read_state('radio.state')}")
-            self.logger.put(f"QFH state is {self.read_state('qfh.state')}")
+                curr_cycle = self.rs("pan.cycle_no")
+                curr_qfh_state = self.read_state("qfh.state")
+                if curr_qfh_state > last_qfh_state:
+                    powercycle_flag = self.check_powercycle()
+                    self.logger.put(f"qfh state incremented on cycle {curr_cycle}")
+                    self.logger.put(f"qfh state incremented {curr_qfh_state}")
+                    self.logger.put(f"powercycle flag was set to {powercycle_flag}")
 
+                last_qfh_state = curr_qfh_state
+            
+
+            # Debugging
+            # self.logger.put(f"Power cycle round  {x+1}:")
+            # self.logger.put(f"Radio state is {self.read_state('radio.state')}")
+            # self.logger.put(f"QFH state is {self.read_state('qfh.state')}")
+
+            # Qfh should want to powercycle
             self.check_faulty_powercycle()
+
+        
+            
             
         if not self.failed:
             self.logger.put("QuakeFaultHandler did not inappropriately power cycle when QuakeManager was in a non-wait state.")
+        else:
+            self.logger.put("Powercycling behavior did not match expected behavior.")
         self.logger.put("Testcase finished.")
 
         self.finish()
