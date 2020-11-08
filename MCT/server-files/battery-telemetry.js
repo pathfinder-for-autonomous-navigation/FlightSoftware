f7e2d520971e0a27514933daf02890e0960f185a
Kirin Kambon <nirik8801@gmail.com>
2020-11-08 16:56:57 -0500

    adding base code again

---
+++ MCT/server-files/battery-telemetry.js
@@ -0,0 +1,143 @@
+/*
+ battery-telemetry.js simulates the gomspace generating telemetry.
+*/
+const variables = require('./state-variables.js')
+const url = require('url');
+const request = require('request');
+const axios = require('axios');
+const { isObject } = require('util');
+var searchURl = 'http://localhost:5000/search-es';
+var searchIndex = 'statefield_report_123';
+function Battery() {
+    //This state function takes in initial values from the state-variables.js file
+    this.state = {
+        "gomspace.modes": variables.modes,
+        "gomspace.counters": variables.counters,
+        "gomspace.commands": variables.commands,
+        "gomspace.curin": variables.curin,
+        "gomspace.curout": variables.curout,
+        "gomspace.get_hk": variables.get_hk,
+        "gomspace.low_batt": variables.low_batt,
+        "gomspace.output": variables.output,
+        "gomspace.power_cycle_output": variables.power_cycle_output,
+        "gomspace.pv": variables.pv,
+        "gomspace.temp": variables.temp,
+        "gomspace.vboost": variables.vboost,
+        "gomspace.vbatt": variables.vbatt,
+        "gomspace.batt_threshold": variables.batt_threshold,
+        "gomspace.cursun": variables.cursun,
+        "gomspace.cursys": variables.cursys,
+        "gomspace.bootcause": variables.bootcause
+    };
+    this.history = {};
+    this.listeners = [];
+    Object.keys(this.state).forEach(function (k) {
+        this.history[k] = [];
+    }, this);
+    /*function addtoHistory(key){
+      console.log(this.state.key)
+
+    }*/
+
+    setInterval(function () {
+        this.updateState();
+        this.generateTelemetry();
+    }.bind(this), 1000);
+    console.log("Spacecraft Launched")
+
+};
+
+/**
+*   Tests the functionality of updateState by incrementing the counter boot time
+*   and setting the battery voltage to 24
+**/
+Battery.prototype.updateState = function () {
+  
+  Object.keys(this.state).forEach(function (id) {
+
+    if(typeof(this.state[id]) == 'object'){//if state is an object
+      
+      Object.keys(this.state[id]).forEach(function (subId){
+  
+        (this.state[id])[subId] = this.getValue(searchURl, searchIndex, subId);
+      },this)
+      console.log("pee")
+
+
+
+    }else{// if state is a primitive type
+      console.log(searchIndex)
+      this.state[id] = this.getValue(searchURl, searchIndex, id);
+    }
+
+  }, this);
+
+};
+
+Battery.prototype.getValue = function(myurl, i, f){
+  
+  var propertiesObject = { index: i, field:f };
+  request({url:myurl, qs:propertiesObject}, function(err, response, body) {//make anonymous function part of the class
+    if(err) { console.log(err); return; }
+    console.log("Get response: " + response.statusCode);
+    if(err) { console.log("error in http GET"); return; }
+    value = body;
+  });
+ 
+};
+
+
+/**
+ * Takes a measurement of battery state, determines its type, stores in history, and notifies
+ * listeners.
+ *
+ *This method has two cases:
+ * - if the state is an object, meaning it has substates (output1, output2, etc.)
+ *   then generate telemetry for each substate
+ * - if the state is a primitive type, int,bool, or char then generate Telemetry
+ *   for the state value directly
+ */
+Battery.prototype.generateTelemetry = function () {
+    var timestamp = Date.now(), sent = 0;
+    //make two cases one that updates objects and one that directly updates field
+    Object.keys(this.state).forEach(function (id) {
+
+      if(typeof(this.state[id]) == 'object'){//if state is an object
+
+        var telempoint = { timestamp: timestamp, id: id};
+        for (const output in this.state[id]){
+          telempoint[output] = this.state[id][output];
+        }
+        this.notify(telempoint);
+        this.history[id].push(telempoint);
+
+      }else{// if state is a primitive type
+
+        var telempoint = { timestamp: timestamp, value: this.state[id], id: id};
+
+        this.notify(telempoint);
+        this.history[id].push(telempoint);
+      }
+
+    }, this);
+
+};
+
+Battery.prototype.notify = function (point) {
+    this.listeners.forEach(function (l) {
+        l(point);
+    });
+};
+
+Battery.prototype.listen = function (listener) {
+    this.listeners.push(listener);
+    return function () {
+        this.listeners = this.listeners.filter(function (l) {
+            return l !== listener;
+        });
+    }.bind(this);
+};
+
+module.exports = function () {
+    return new Battery()
+};
---
+++ ptest/cases/reset_for_flight.py
@@ -0,0 +1,9 @@
+from .base import SingleSatOnlyCase, TestCaseFailure
+from .utils import Enums
+import math
+
+class ResetforFlight(SingleSatOnlyCase):
+
+  def run_case_singlesat(self):
+      self.ws("gomspace.ppt_mode_cmd", 1)#max ppt
+      self.ws( "cycle.auto", True )
