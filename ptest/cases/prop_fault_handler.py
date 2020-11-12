19acd5e2ce709551704544d1fcdaa246d66dd5d3
athena255 <afc65@cornell.edu>
2020-11-11 20:49:16 -0500

    ptest

--- ptest/cases/prop_fault_handler.py
+++ ptest/cases/prop_fault_handler.py
@@ -54,6 +54,22 @@ class PropFaultHandler(SingleSatOnlyCase):
         self.rs("pan.state")
         self.rs("pan.cycle_no")
 
+        self.rs("prop.tank2.pressure")
+        self.rs("prop.tank2.temp")
+        self.rs("prop.tank1.temp")
+        
+        self.rs("prop.pressurize_fail.suppress")
+        self.rs("prop.pressurize_fail.override")
+
+        self.rs("prop.overpressured.suppress")
+        self.rs("prop.overpressured.override")
+        
+        self.rs("prop.tank1_temp_high.suppress")
+        self.rs("prop.tank1_temp_high.override")
+
+        self.rs("prop.tank2_temp_high.suppress")
+        self.rs("prop.tank2_temp_high.override")
+
     @property
     def state(self):
         return str(self.read_state("prop.state"))
