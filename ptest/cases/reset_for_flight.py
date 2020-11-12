b429947879d67bc6cd7a7ca9f48cd2baeff5bc2f
Kirin Kambon <nirik8801@gmail.com>
2020-11-11 20:50:21 -0500

    formatting and comments

--- ptest/cases/reset_for_flight.py
+++ ptest/cases/reset_for_flight.py
@@ -4,7 +4,19 @@ import math
 
 class ResetforFlight(SingleSatOnlyCase):
 
+  #Sets gomspace ppt mode to MPPT(1) and fails if it is not successfully updated
+  def set_ppt(self):
+    ppt_mode_cmd = int(self.read_state("gomspace.pptmode_cmd"))
+    ppt_mode_updated = int(self.ws("gomspace.pptmode_cmd", 1))
+    if ppt_mode_cmd == ppt_mode_updated:
+      self.logger.put("Could not update pptmode")
+      self.failed = True
+
   def run_case_singlesat(self):
-      self.ws("gomspace.ppt_mode_cmd", 1)#max ppt
+      self.set_ppt()
+
+      #ensure autocycling is happening
       self.ws( "cycle.auto", True )
+
+      #set bootcount to zero
       self.ws("pan.bootcount", 0)
