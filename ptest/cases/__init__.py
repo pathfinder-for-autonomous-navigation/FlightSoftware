a6a1b4eaf366b5c8d53db81a8d29b02d60e27e3c
Kirin Kambon <nirik8801@gmail.com>
2020-11-13 14:44:13 -0500

    fixing up case and testing it

--- ptest/cases/__init__.py
+++ ptest/cases/__init__.py
@@ -23,3 +23,4 @@ from .ci_case import CICase
 from .simple_fault_handlers import ADCSWheelFaultHandler, LowBattFaultHandler
 from .safehold_reboot import SafeholdReboot
 from .torquer_rotate_case import MTorquerCase
+from  .reset_for_flight import ResetforFlight
--- ptest/cases/reset_for_flight.py
+++ ptest/cases/reset_for_flight.py
@@ -6,17 +6,35 @@ class ResetforFlight(SingleSatOnlyCase):
 
   #Sets gomspace ppt mode to MPPT(1) and fails if it is not successfully updated
   def set_ppt(self):
-    ppt_mode_cmd = int(self.read_state("gomspace.pptmode_cmd"))
-    ppt_mode_updated = int(self.ws("gomspace.pptmode_cmd", 1))
+    ppt_mode_cmd = int(self.rs("gomspace.pptmode_cmd"))
+    self.ws("gomspace.pptmode_cmd", 1)
+    self.cycle()
+    ppt_mode_updated = int(self.rs("gomspace.pptmode_cmd"))
     if ppt_mode_cmd == ppt_mode_updated:
       self.logger.put("Could not update pptmode")
       self.failed = True
 
+  #sets the bootcount to 0 and confirms it was changed correctly
+  def set_bootcount(self):
+    bootcount = self.rs("pan.bootcount")
+    self.ws("pan.bootcount", 0)
+    self.cycle()
+    bootcount_updated = self.rs("pan.bootcount")
+
+    if bootcount == bootcount_updated:
+      self.logger.put("Could not update bootcount")
+      self.failed = True
+    
   def run_case_singlesat(self):
-      self.set_ppt()
+    self.print_header("Starting Reset")
+    self.ws( "cycle.auto", False )
+    self.set_ppt()
+    self.set_bootcount()
 
-      #ensure autocycling is happening
-      self.ws( "cycle.auto", True )
+    #ensure autocycling is happening 
+    #make sure spacecraft is shut down right after this test case so mission doesnt start executing
+    self.ws( "cycle.auto", True )
+    self.print_rs("cycle.auto")
 
-      #set bootcount to zero
-      self.ws("pan.bootcount", 0)
+    self.print_header("Flight Reset Complete")
+    self.finish()
