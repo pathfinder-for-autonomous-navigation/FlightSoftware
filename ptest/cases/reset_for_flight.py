f7ddad4588ca3830a1cbb57c8ea47291a034753a
Kirin Kambon <nirik8801@gmail.com>
2020-11-13 17:57:04 -0500

    address issues and clean up code some

--- ptest/cases/reset_for_flight.py
+++ ptest/cases/reset_for_flight.py
@@ -4,36 +4,32 @@ import math
 
 class ResetforFlight(SingleSatOnlyCase):
 
-  #Sets gomspace ppt mode to MPPT(1) and fails if it is not successfully updated
-  def set_ppt(self):
-    ppt_mode_cmd = int(self.rs("gomspace.pptmode_cmd"))
-    self.ws("gomspace.pptmode_cmd", 1)
+  #takes in field name and value to set, then confirms the value was correctly
+  #set 
+  # @param field: state field to update
+  # @param value: value to update field with
+  def set_confirm(self, field, value):
+    initialstate = self.rs( field )
+    self.ws( field, value )
     self.cycle()
-    ppt_mode_updated = int(self.rs("gomspace.pptmode_cmd"))
-    if ppt_mode_cmd == ppt_mode_updated:
-      self.logger.put("Could not update pptmode")
+    updatestate = self.rs( field )
+    if ( updatestate == initialstate ):
+      self.logger.put("Could not update" + str(field))
       self.failed = True
-
-  #sets the bootcount to 0 and confirms it was changed correctly
-  def set_bootcount(self):
-    bootcount = self.rs("pan.bootcount")
-    self.ws("pan.bootcount", 0)
-    self.cycle()
-    bootcount_updated = self.rs("pan.bootcount")
-
-    if bootcount == bootcount_updated:
-      self.logger.put("Could not update bootcount")
-      self.failed = True
+    self.logger.put("Reset " + str(field) + " to " + str(value))
     
   def run_case_singlesat(self):
     self.print_header("Starting Reset")
     self.ws( "cycle.auto", False )
-    self.set_ppt()
-    self.set_bootcount()
+    self.set_confirm("gomspace.pptmode_cmd", 1)
+    self.set_confirm("pan.bootcount", 0)
+    self.set_confirm("pan.deployment.elapsed", 0)
+    self.set_confirm("pan.deployed", False)
 
     #ensure autocycling is happening 
     #make sure spacecraft is shut down right after this test case so mission doesnt start executing
     self.ws( "cycle.auto", True )
+    self.cycle()
     self.print_rs("cycle.auto")
 
     self.print_header("Flight Reset Complete")
