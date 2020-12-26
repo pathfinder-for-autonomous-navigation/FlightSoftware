6e2cc91a303abfd7c45f175ac81de61c2a5c09d7
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2020-12-25 22:20:29 -0500

    It works on Ubuntu but not Mac...

--- src/gsw/tools/downlink_testing/downlink_tester.cpp
+++ src/gsw/tools/downlink_testing/downlink_tester.cpp
@@ -11,15 +11,15 @@ int main(int argc, char** argv) {
 
     std::string basepath = argv[1];
 
-    // auto dump_to_file = [&](const nlohmann::json& data, const std::string& filename, unsigned int i) {
-    //     std::stringstream pathstream;
-    //     pathstream << basepath << "/" << filename << i << ".txt";
-    //     std::string path = pathstream.str();
+    auto dump_to_file = [&](const nlohmann::json& data, const std::string& filename, unsigned int i) {
+        std::stringstream pathstream;
+        pathstream << basepath << "/" << filename << i << ".txt";
+        std::string path = pathstream.str();
 
-    //     std::ofstream outfile(path);
-    //     outfile << data.dump(4);
-    //     outfile.close();
-    // };
+        std::ofstream outfile(path);
+        outfile << data.dump(4);
+        outfile.close();
+    };
 
     size_t num_iterations = std::stoi(argv[2]);
 
@@ -29,9 +29,9 @@ int main(int argc, char** argv) {
         TelemetryInfoGenerator::TelemetryInfo info;
         DownlinkTestFixture::generate_telemetry_info(info);
 
-        // nlohmann::json telem_info;
-        // to_json(telem_info, info);
-        // dump_to_file(telem_info, "teleminfo", i+1);
+        nlohmann::json telem_info;
+        to_json(telem_info, info);
+        dump_to_file(telem_info, "teleminfo", i+1);
 
         DownlinkTestFixture dtf(info);
         DownlinkTestFixture::test_input_t input;
@@ -41,10 +41,10 @@ int main(int argc, char** argv) {
         dtf.parse(input, output);
         dtf.compare(input, output, result);
         
-        // nlohmann::json test_output;
-        // test_output["input"] = input;
-        // test_output["result"] = result;
-        // dump_to_file(test_output, "output", i+1);
+        nlohmann::json test_output;
+        test_output["input"] = input;
+        test_output["result"] = result;
+        dump_to_file(test_output, "output", i+1);
 
         std::cout << "Completed test " << i + 1 << " of " << num_iterations << std::endl;
     }
