a243ead400ad01ea6ecd6089597dffebe27b1701
afoarce <foarceandreea@gmail.com>
2021-01-12 23:35:30 -0500

    Update tlm/email_processor.py
    Co-authored-by: Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>

--- tlm/email_processor.py
+++ tlm/email_processor.py
@@ -109,7 +109,6 @@ class IridiumEmailProcessor(object):
         mail_ids = data[0]
         id_list = mail_ids.split()
 
-        print(self.confirmation_mtmsn, self.mtmsn, self.send_uplinks)
 
         for num in id_list:
             #.fetch() fetches the mail for given id where 'RFC822' is an Internet 
