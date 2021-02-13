c2330c8de6fd0e1270adfbb1b6b98fb381c5e2f3
Shihao Cao <shehowcow@gmail.com>
2021-02-13 17:08:05 -0500

    abstraction

--- src/fsw/FCCode/QuakeManager.cpp
+++ src/fsw/FCCode/QuakeManager.cpp
@@ -77,6 +77,30 @@ QuakeManager::~QuakeManager()
     delete[] mo_buffer_copy;
 }
 
+#ifndef FLIGHT
+void QuakeManager::dump_debug_telemetry(char *buffer, size_t size){
+    #ifdef DESKTOP
+    std::cout << "{\"t\":" << debug_console::_get_elapsed_time() << ",\"telem\":\"";
+    for (size_t i = 0; i < size->get(); i++)
+    {
+        std::ostringstream out;
+        out << "\\\\x";
+        out << std::hex << std::setfill('0') << std::setw(2) << (0xFF & buffer[i]);
+        std::cout << out.str();
+    }
+    std::cout << "\"}\n";
+    #else
+    Serial.printf("{\"t\":%d,\"telem\":\"", debug_console::_get_elapsed_time());
+    for (size_t i = 0; i < size; i++)
+    {
+        Serial.print("\\\\x");
+        Serial.print((0xFF & buffer[i]), HEX);
+    }
+    Serial.print("\"}\n");
+    #endif
+}
+#endif
+
 void QuakeManager::execute()
 {
 #ifndef FLIGHT
@@ -85,25 +109,7 @@ void QuakeManager::execute()
         dump_telemetry_f.set(false);
         char *snapshot = radio_mo_packet_fp->get();
 
-#ifdef DESKTOP
-        std::cout << "{\"t\":" << debug_console::_get_elapsed_time() << ",\"telem\":\"";
-        for (size_t i = 0; i < snapshot_size_fp->get(); i++)
-        {
-            std::ostringstream out;
-            out << "\\\\x";
-            out << std::hex << std::setfill('0') << std::setw(2) << (0xFF & snapshot[i]);
-            std::cout << out.str();
-        }
-        std::cout << "\"}\n";
-#else
-        Serial.printf("{\"t\":%d,\"telem\":\"", debug_console::_get_elapsed_time());
-        for (size_t i = 0; i < snapshot_size_fp->get(); i++)
-        {
-            Serial.print("\\\\x");
-            Serial.print((0xFF & snapshot[i]), HEX);
-        }
-        Serial.print("\"}\n");
-#endif
+        dump_debug_telemetry(snapshot, snapshot_size_fp->get());
     }
 #endif
 
