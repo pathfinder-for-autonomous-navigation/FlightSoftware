0545d90bbb0e2fbaa17881102135ed3bcb57695b
Shihao Cao <shehowcow@gmail.com>
2021-02-18 17:16:44 -0500

    push dual

---
+++ ptest/cases/dual_psim.py
@@ -0,0 +1,44 @@
+from .base import MissionCase
+import time
+from psim.sims import DualAttitudeOrbitGnc
+import lin
+from .utils import str_to_val, Enums
+
+class AutonomousMissionManagerCase(MissionCase):
+
+    @property
+    def sim_configs(self):
+        configs = ["truth/ci", "truth/base"]
+        configs += ["sensors/base"]
+        return configs
+
+    @property
+    def sim_model(self):
+        return DualAttitudeOrbitGnc
+
+    @property
+    def sim_mapping(self):
+        return "ci_mapping.json"
+        
+    @property
+    def sim_duration(self):
+        return float("inf")
+
+    @property
+    def initial_state_leader(self):
+        return "leader"
+    @property
+    def initial_state_follower(self):
+        return "follower"
+
+    @property
+    def fast_boot_leader(self):
+        return True
+    @property
+    def fast_boot_follower(self):
+        return True
+
+    def run_case_fullmission(self):
+
+        self.flight_controller_leader.read_state("pan.cycle_no")
+        self.flight_controller_follower.read_state("pan.cycle_no")
