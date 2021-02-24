8efd514bcc18ee50e9dd9210bb03be3a5b3b2e56
Kyle Krol <kyle.krol.27@gmail.com>
2021-02-24 12:29:11 -0500

    Updated DCDC checkout case

--- ptest/cases/dcdc_checkout_case.py
+++ ptest/cases/dcdc_checkout_case.py
@@ -1,120 +1,168 @@
 from .base import SingleSatOnlyCase
+from .utils import Enums, TestCaseFailure
 
 class DCDCCheckoutCase(SingleSatOnlyCase):
-    @property
-    def adcs_cmd(self): 
-        return self.flight_controller.read_state("dcdc.ADCSMotor_cmd")
+
+    def __init__(self, *args, **kwargs):
+        super(DCDCCheckoutCase, self).__init__(*args, **kwargs)
+
+        self.__adcs_command = None
+        self.__adcs = None
+        self.__prop_command = None
+        self.__prop = None
+        self.__disable_command = None
+        self.__reset_command = None
 
     @property
-    def sph_cmd(self): 
-        return self.flight_controller.read_state("dcdc.SpikeDock_cmd")
-    
-    @property
-    def disable_cmd(self): 
-        return self.flight_controller.read_state("dcdc.disable_cmd")
+    def adcs_command(self) -> bool:
+        assert self.__adcs_command is not None
+        return self.__adcs_command
+
+    @adcs_command.setter
+    def adcs_command(self, value: bool):
+        self.ws("dcdc.ADCSMotor_cmd", value)
 
     @property
-    def reset_cmd(self): 
-        return self.flight_controller.read_state("dcdc.reset_cmd")
+    def adcs(self) -> bool:
+        assert self.__adcs is not None
+        return self.__adcs
 
     @property
-    def adcs_rd(self): 
-        return self.flight_controller.read_state("dcdc.ADCSMotor")
-    
+    def prop_command(self) -> bool:
+        assert self.__prop_command is not None
+        return self.__prop_command
+
+    @prop_command.setter
+    def prop_command(self, value: bool):
+        self.ws("dcdc.SpikeDock_cmd", value)
+
     @property
-    def sph_rd(self): 
-        return self.flight_controller.read_state("dcdc.SpikeDock")
+    def prop(self) -> bool:
+        assert self.__prop is not None
+        return self.__prop
 
-    @adcs_cmd.setter 
-    def adcs_cmd(self, value): 
-        assert(value == "true" or value == "false")
-        self.flight_controller.write_state("dcdc.ADCSMotor_cmd", value)
+    @property
+    def disable_command(self) -> bool:
+        assert self.__disable_command is not None
+        return self.__disable_command
 
-    @sph_cmd.setter 
-    def sph_cmd(self, value): 
-        assert(value == "true" or value == "false")
-        self.flight_controller.write_state("dcdc.SpikeDock_cmd", value)
+    @disable_command.setter
+    def disable_command(self, value: bool):
+        self.ws("dcdc.disable_cmd", value)
 
-    @disable_cmd.setter 
-    def disable_cmd(self, value): 
-        assert(value == "true" or value == "false")
-        self.flight_controller.write_state("dcdc.disable_cmd", value)
+    @property
+    def reset_command(self) -> bool:
+        assert self.__reset_command is not None
+        return self.__reset_command
 
-    @reset_cmd.setter 
-    def reset_cmd(self, value): 
-        assert(value == "true" or value == "false")
-        self.flight_controller.write_state("dcdc.reset_cmd", value)
+    @reset_command.setter
+    def reset_command(self, value: bool):
+        self.ws("dcdc.reset_cmd", value)
+
+    def cycle(self):
+        super(DCDCCheckoutCase, self).cycle()
+
+        self.__adcs_command = self.rs("dcdc.ADCSMotor_cmd")
+        self.__adcs = self.rs("dcdc.ADCSMotor")
+        self.__prop_command = self.rs("dcdc.SpikeDock_cmd")
+        self.__prop = self.rs("dcdc.SpikeDock")
+        self.__disable_command = self.rs("dcdc.disable_cmd")
+        self.__reset_command = self.rs("dcdc.reset_cmd")
 
     def run_case_singlesat(self):
-        self.cycle_no = self.flight_controller.read_state("pan.cycle_no")
 
-        if self.adcs_cmd==self.adcs_rd and self.sph_cmd==self.sph_rd and self.disable_cmd=="false" and self.reset_cmd=="false":
-            self.logger.put("Control task initialized correctly") 
+        def abort(*args, **kwargs):
+            self.logger.put(f"Testcase failed:")
+            self.logger.put(f"\tadcs_command   = {self.adcs_command}")
+            self.logger.put(f"\tadcs           = {self.adcs}")
+            self.logger.put(f"\tprop_command   = {self.prop_command}")
+            self.logger.put(f"\tprop           = {self.prop}")
+            self.logger.put(f"\tdisable_command = {self.disable_command}")
+            self.logger.put(f"\treset_command   = {self.reset_command}")
 
-        print ("Test case 1: Try both DCDCs on. Turn on all systems (ADCS motors, prop valves, docking motor).")
-
-        self.adcs_cmd="true"
-        self.sph_cmd="true"
-
-        if self.adcs_rd=="true" and self.sph_rd=="true":
-            self.logger.put("Passed")
-        else:
-            self.logger.put("Failed")
-
-        self.logger.put("Test case 2: ADCS DCDC on, SPH + Prop DCDC off. Turn on all systems.")
-
-        self.adcs_cmd="true"
-        self.sph_cmd="false"
-        if self.adcs_rd=="true" and self.sph_rd=="false":
-            self.reset_cmd="true"
-            # Reset takes at least one control cycle to complete
+            self.adcs_command = False
+            self.prop_command = False
             self.cycle()
-            if (self.adcs_rd == "true" and self.sph_rd == "true"):
-                self.logger.put("Passed")
-            else:
-                self.logger.put("Unable to reset pins")
-                self.logger.put("ADCS Motor pin: "+self.adcs_rd)
-                self.logger.put("Spike and Hold pin: "+self.sph_rd)
-        else:
-            self.logger.put("Unable to turn ADCS Motor on and SpikeDock off")
-            self.logger.put("ADCS Motor pin: "+self.adcs_rd)
-            self.logger.put("Spike and Hold pin: "+self.sph_rd)
 
-        self.logger.put("Test Case 3: ADCS DCDC off, SPH + Prop DCDC on. Turn on all systems. ")
+            raise TestCaseFailure(*args, **kwargs)
 
-        self.adcs_cmd="false"
-        self.sph_cmd="true"
-        if self.adcs_rd=="false" and self.sph_rd=="true":
-            self.reset_cmd="true"
-            # Reset takes at least one control cycle to complete
+        def reset(has_enabled = True):
+            self.reset_command = True
             self.cycle()
-            if (self.adcs_rd == "true" and self.sph_rd == "true"):
-                self.logger.put("Passed")
-            else:
-                self.logger.put("Unable to reset pins")
-                self.logger.put("ADCS Motor pin: "+self.adcs_rd)
-                self.logger.put("Spike and Hold pin: "+self.sph_rd)
-        else:
-            self.logger.put("Unable to turn ADCS Motor off and SpikeDock on")
-            self.logger.put("ADCS Motor pin: "+self.adcs_rd)
-            self.logger.put("Spike and Hold pin: "+self.sph_rd)
 
-        self.logger.put("Test Case 4: ADCS DCDC off, SPH + Prop DCDC off. Turn on all systems. ")
+            if has_enabled:
+                if not self.reset_command:
+                    abort("Reset command should persist for one cycle is some elements are enabled.")
 
-        self.disable_cmd="true"
-        if self.adcs_rd=="false" and self.sph_rd=="false":
-            self.reset_cmd="true"
-            # Reset takes at least one control cycle to complete
-            self.cycle()
-            if (self.adcs_rd == "true" and self.sph_rd == "true"):
-                self.logger.put("Passed")
-            else:
-                self.logger.put("Unable to reset pins")
-                self.logger.put("ADCS Motor pin: "+self.adcs_rd)
-                self.logger.put("Spike and Hold pin: "+self.sph_rd)
+                self.cycle()
+
+            if not self.adcs_command or not self.adcs or \
+                    not self.prop_command or not self.prop or self.reset_command:
+               abort("Failed to reset the DCDCs.")
+
+        self.ws("pan.state", Enums.mission_states["manual"])
+        self.cycle()
+
+        if self.adcs_command or self.adcs or self.prop or self.prop_command or \
+                self.disable_command or self.reset_command:
+            abort("All fields should be false on initialization; " +
+                  "ensure the spacecraft was rebooted before running this case.")
         else:
-            self.logger.put("Unable to disable pins")
-            self.logger.put("ADCS Motor pin: "+self.adcs_rd)
-            self.logger.put("Spike and Hold pin: "+self.sph_rd)
+            self.logger.put("Testcase initialized correctly.")
+            self.logger.put("")
+
+        self.logger.put("Step 1: Set both DCDCs to on...")
+
+        self.adcs_command = True
+        self.prop_command = True
+        self.cycle()
+
+        if self.adcs_command and self.adcs and self.prop_command and self.prop:
+            self.logger.put("Success!")
+        else:
+            abort("Failed to enable both DCDCs.")
+
+        self.logger.put("")
+        self.logger.put("Step 2: Set only ADCS to be on and then attempt a reset command...")
+
+        self.adcs_command = True
+        self.prop_command = False
+        self.cycle()
+
+        if not self.adcs_command or not self.adcs or self.prop_command or self.prop:
+            abort("Failed to enable only the ADCS DCDC.")
+        
+        reset()
+        self.logger.put("Success!")
+        self.logger.put("")
+
+        self.logger.put("Step 3: Enable only Prop to be on and then attempt a reset command...")
+
+        self.adcs_command = False
+        self.prop_command = True
+        self.cycle()
+
+        if self.adcs_command or self.adcs or not self.prop or not self.prop_command:
+            abort("Failed to enable only the Prop DCDC.")
+        
+        reset()
+        self.logger.put("Success!")
+        self.logger.put("")
+
+        self.logger.put("Step 4: Disabled both DCDCs and attempt a reset command...")
+
+        self.disable_command = True
+        self.cycle()
+
+        if self.adcs_command or self.adcs or self.prop_command or self.prop or self.disable_command:
+            abort("Failed to disable the DCDCs.")
+        
+        reset(False)
+        self.logger.put("Success!")
+        self.logger.put("")
+
+        self.adcs_command = False
+        self.prop_command = False
+        self.cycle()
 
         self.finish()
