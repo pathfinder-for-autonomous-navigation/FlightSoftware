c81a8d8f69513d4cc23a6a3cf92ab908daf0affc
Samuel Qian <samuelqian@dhcp-vl2041-7744.eduroam.cornell.edu>
2021-04-03 11:54:58 -0400

    EEPROM with printing b4 testing and added changes to fix compiling/running errors

--- src/fsw/FCCode/EEPROMControllerTeensy.cpp
+++ src/fsw/FCCode/EEPROMControllerTeensy.cpp
@@ -21,9 +21,10 @@ void EEPROMController::read_EEPROM(){
 }
 
 void EEPROMController::update_EEPROM(unsigned int position){
-
-  while (EEPROM.read(addresses[position]) != _registry.eeprom_saved_fields[position]->get_eeprom_repr()){
+  
+  if (EEPROM.read(addresses[position]) != _registry.eeprom_saved_fields[position]->get_eeprom_repr()){
     EEPROM.update(addresses[position], _registry.eeprom_saved_fields[position]->get_eeprom_repr());
+    debug_console::printf("\nEEPROM updated\n");
   }
 }
 
