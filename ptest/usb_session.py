dbc0c473b08c01b3ab45f7774b77bfb473907556
Samuel Qian <samuelqian@dhcp-vl2041-7744.eduroam.cornell.edu>
2021-04-10 02:00:59 -0400

    Fork vs Spawn restoring USB session

--- ptest/usb_session.py
+++ ptest/usb_session.py
@@ -7,7 +7,7 @@ import queue
 import os
 import pty
 import subprocess
-import multiprocessing
+from multiprocessing import Process
 import glob
 from elasticsearch import Elasticsearch
 import email
@@ -117,7 +117,7 @@ class USBSession(object):
             self.flask_app = create_usb_session_endpoint(self)
             self.flask_app.config["uplink_console"] = self.uplink_console
             self.flask_app.config["console"] = self.console
-            self.http_thread = multiprocessing.Process(name=f"{self.device_name} HTTP Command Endpoint", target=self.flask_app.run, kwargs={"host":'0.0.0.0', "port": self.port})
+            self.http_thread = Process(name=f"{self.device_name} HTTP Command Endpoint", target=self.flask_app.run, kwargs={"host":'0.0.0.0', "port": self.port})
             self.http_thread.start()
             print(f"{self.device_name} HTTP command endpoint is running at http://localhost:{self.port}")
             return True
@@ -532,11 +532,11 @@ class USBSession(object):
         self.console.close()
         self.dp_console.close()
 
-        # self.http_thread.terminate()
-        # self.http_thread.join()
+        self.http_thread.terminate()
+        self.http_thread.join()
 
-        # self.http_thread.terminate()
-        # self.http_thread.join()
+        self.http_thread.terminate()
+        self.http_thread.join()
 
         self.datastore.stop()
         self.logger.stop()
