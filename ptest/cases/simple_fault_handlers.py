62307de3e421afa74ac920be731a61dd25b0b9a0
Shihao Cao <shehowcow@gmail.com>
2021-04-14 18:58:20 -0400

    adcs standby update

--- ptest/cases/simple_fault_handlers.py
+++ ptest/cases/simple_fault_handlers.py
@@ -5,7 +5,8 @@ class ADCSWheelFaultHandler(SingleSatCase, PSimCase):
     def __init__(self, *args, **kwargs):
         super(ADCSWheelFaultHandler, self).__init__(*args, **kwargs)
         self.initial_state = "standby"
-        self.psim_configs = ['truth/standby']
+        self.psim_configs += ['truth/standby']
+        self.psim_config_overrides["truth.leader.attitude.w"] = lin.Vector3([0.01,0.0711,-0.01])
 
     def post_boot(self):
         self.ws("fault_handler.enabled", True)
