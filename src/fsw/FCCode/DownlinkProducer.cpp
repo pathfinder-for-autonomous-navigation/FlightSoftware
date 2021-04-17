dfe3a9139a37e510281d5e561760860a2194080f
Shihao Cao <shehowcow@gmail.com>
2021-04-17 12:11:46 -0400

    still going flow 30

--- src/fsw/FCCode/DownlinkProducer.cpp
+++ src/fsw/FCCode/DownlinkProducer.cpp
@@ -225,8 +225,8 @@ DownlinkProducer::Flow::Flow(const StateFieldRegistry& r,
 
     const bool flow_too_large = get_packet_size() > num_bits_in_packet - 1 - 32;
     if (flow_too_large) {
-        printf(debug_severity::error, 
-            "Flow %d is too large, with a size of %d bits.",
+        fprintf(stderr, 
+            "Flow %d is too large, with a size of %lu bits.\n",
             flow_data.id, get_packet_size());
         assert(false);
     }
