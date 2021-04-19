2e232ba96f776252effe6b48eb8b91b31591df2c
Kyle Krol <33558436+kylekrol@users.noreply.github.com>
2021-04-19 00:29:29 -0400

    Far Field Testcase (#736)

--- ptest/cases/mission/__init__.py
+++ ptest/cases/mission/__init__.py
@@ -1,3 +1,4 @@
-from .startup import SingleSatStartupCase, DualSatStartupCase
 from .detumble import SingleSatDetumbleCase, DualSatDetumbleCase
+from .far_field import DualSatFarFieldCase
 from .standby import SingleSatStandbyCase, DualSatStandbyCase
+from .startup import SingleSatStartupCase, DualSatStartupCase
