c35bdf8ebe585631fd6c05ed89875419c3c0a7e9
Duncan-McD <dam342@cornell.edu>
2021-05-08 13:32:37 -0400

    initial test viewings

--- MCT/server-files/server.js
+++ MCT/server-files/server.js
@@ -1,3 +1,5 @@
+const path = require('path')
+const mct = require(path.join( __dirname, 'mct_secret.json'))
 /**
  * Basic implementation of a history and realtime server for MCT.
  */
@@ -29,10 +31,43 @@ var spacecraft = new Telemetry(myArgs[0]);
 var realtimeServer = new RealtimeServer(spacecraft);
 var historyServer = new HistoryServer(spacecraft);
 
+app.use((req, res, next) => {
+
+  const auth = {login: mct.login.username, password: mct.login.password}
+
+  const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+  const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':')
+
+  if (login && password && login === auth.login && password === auth.password) {
+    return next()
+  }
+
+  res.set('WWW-Authenticate', 'Basic realm="401"')
+  res.status(401).send('Authentication required.')
+
+})
 app.use('/realtime', realtimeServer);
 app.use('/history', historyServer);
 app.use('/openmct', express.static('node_modules/openmct/dist/'));
 app.use('/', express.static("public"));
+if (mct.devices.follower.enabled == true && mct.devices.leader.enabled == true){
+  app.get('/',function(req,res) {
+    res.sendFile(path.join(__dirname, '../public/views/dual.html'));
+  });
+}else if (mct.devices.follower.enabled == false && mct.devices.leader.enabled == true){
+  app.get('/',function(req,res) {
+    res.sendFile(path.join(__dirname, '../public/views/leader.html'));
+  });
+}else if (mct.devices.follower.enabled == true && mct.devices.leader.enabled == false){
+  app.get('/',function(req,res) {
+    res.sendFile(path.join(__dirname, '../public/views/follower.html'));
+  });
+}else {
+  app.use('/', express.static("public"));
+}
+
+
+
 
 var httpsServer = https.createServer(credentials, app);
 var port = process.env.PORT || 8080
