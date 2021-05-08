293958e63d26671778b6b06c0a17c134b8cc9d6f
Angela Loh <angelaloh09@gmail.com>
2021-05-08 10:28:26 -0400

    clean up

--- ptest/cases/ditl.py
+++ ptest/cases/ditl.py
@@ -13,35 +13,13 @@ class Ditl(SingleSatDetumbleCase):
         self.rs("pan.state")
         self.rs("radio.state")
         self.rs("pan.cycle_no")
-        #self.rs("pan.bootcount")
         self.rs("adcs.state")
-        #self.rs("piksi.time")
-        #self.rs("orbit.pos")
-        #self.rs("orbit.vel")
-        #self.rs('adcs_monitor.ssa_vec')
-        #self.rs('adcs_monitor.mag1_vec')
-        #self.rs('adcs_monitor.gyr_vec')
-        
-        #self.rs('adcs_monitor.ssa_mode')
-
-        #self.rs("attitude_estimator.q_body_eci")
-        #self.rs("attitude_estimator.L_body")
-
-        #self.rs("adcs_cmd.mtr_cmd")
-        #self.rs("adcs_cmd.rwa_torque_cmd")
         self.rs("attitude_estimator.valid")
-        #self.rs("attitude_estimator.w_bias_body"),
         self.rs("time.valid")
         self.rs("orbit.valid")
         
         self.rs("sys.memory_use")
 
-    # def run_case_singlesat(self):
-    #     #self.rs_psim("truth.leader.attitude.w")
-    #     #self.rs_psim("truth.t.ns")
-    #     #self.rs_psim("truth.dt.ns")
-
-    #     self.data_logs()
 
     def post_boot(self):
         self.ws("fault_handler.enabled", True)
@@ -50,4 +28,3 @@ class Ditl(SingleSatDetumbleCase):
     def run(self):
         self.cycle()
         self.data_logs()
-        # self.ws("adcs.state", Enums.adcs_states["startup"])
