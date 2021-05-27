9f11bdff97400666a4c9b7d47f7302fa559b6d9c
Shihao Cao <shehowcow@gmail.com>
2021-05-26 21:42:30 -0400

    add include

--- test/test_fsw_prop_controller/prop_shared.h
+++ test/test_fsw_prop_controller/prop_shared.h
@@ -2,6 +2,7 @@
 #include <fsw/FCCode/PropController.hpp>
 #include <fsw/FCCode/PropFaultHandler.h>
 #include <fsw/FCCode/prop_state_t.enum>
+#include "../StateFieldRegistryMock.hpp"
 #include "fsw/FCCode/OrbitController.hpp"
 
 #define assert_fault_state(state, x) TEST_ASSERT_EQUAL(state, tf.pc->x.is_faulted())
