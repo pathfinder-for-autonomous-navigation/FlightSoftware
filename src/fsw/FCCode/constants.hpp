98e7b3950258b45bafa86fd431305bf34a76f0aa
Shihao Cao <shehowcow@gmail.com>
2021-07-07 23:57:53 -0700

    update constants 120 to 170 (#791)

--- src/fsw/FCCode/constants.hpp
+++ src/fsw/FCCode/constants.hpp
@@ -8,7 +8,7 @@ namespace PAN {
     // control_cycle_time is the value actually used for timing. The
     // other constants are just informational.
     #ifdef FLIGHT
-        TRACKED_CONSTANT_SC(unsigned int, control_cycle_time_ms, 120);
+        TRACKED_CONSTANT_SC(unsigned int, control_cycle_time_ms, 170);
         TRACKED_CONSTANT_SC(unsigned int, control_cycle_time_us, control_cycle_time_ms * 1000);
         TRACKED_CONSTANT_SC(unsigned int, control_cycle_time_ns, control_cycle_time_us * 1000);
         TRACKED_CONSTANT_SC(unsigned int, control_cycle_time, control_cycle_time_us);
