5c6888998d3eb9f97e3b795f28be5d3bdfee584b
Duncan McDonald <dam342@cornell.edu>
2021-08-31 01:36:44 -0400

    Historical/Realtime Telemetry Provider Fixes (#793)

--- MCT/server-files/server.js
+++ MCT/server-files/server.js
@@ -8,19 +8,18 @@ const mct = require(path.join( __dirname, 'mct_secret.json'))
 var myArgs = process.argv.slice(2);
 
 var Telemetry= require('./telemetry');
-var RealtimeServer = require('./boilerplate-mct-servers/realtime-server');
 var HistoryServer = require('./boilerplate-mct-servers/history-server');
 var expressWs = require('express-ws'); 
 var express = require('express');
 var fs = require('fs');
 var https = require('https');
+var ws = require('ws');
 
 var privateKey  = fs.readFileSync('./key.pem', 'utf8');
 var certificate = fs.readFileSync('./cert.pem', 'utf8');
 var credentials = {key: privateKey, cert: certificate};
 
 var app = express()
-expressWs(app);
 
 //sets default config file to mct_secret.json
 if(myArgs[0] == undefined){
@@ -28,7 +27,6 @@ if(myArgs[0] == undefined){
 }
 
 var spacecraft = new Telemetry(myArgs[0]);
-var realtimeServer = new RealtimeServer(spacecraft);
 var historyServer = new HistoryServer(spacecraft);
 
 app.use((req, res, next) => {
@@ -46,7 +44,10 @@ app.use((req, res, next) => {
   res.status(401).send('Authentication required.')
 
 })
-app.use('/realtime', realtimeServer);
+
+  
+
+
 app.use('/history', historyServer);
 app.use('/openmct', express.static('node_modules/openmct/dist/'));
 app.use('/', express.static("public"));
@@ -66,10 +67,48 @@ if (mct.devices.follower.enabled == true && mct.devices.leader.enabled == true){
 
 
 var httpsServer = https.createServer(credentials, app);
+const socket = new ws.Server({server: httpsServer})
+
+socket.on('connection', (ws, req) => {
+  var subscribed = {}; // Active subscriptions for this connection
+  function notifySubscribers(point) {
+     if (subscribed[point.id]) {
+          ws.send(JSON.stringify(point));
+      }
+  }
+  var unlisten = spacecraft.listen(notifySubscribers);
+
+  var handlers = { // Handlers for specific requests
+        subscribe: function (id) {
+            subscribed[id] = true;
+        },
+        unsubscribe: function (id) {
+            delete subscribed[id];
+        }
+    };
+
+
+
+// Listen for requests
+  ws.on('message', function (message) {
+      var parts = message.split(' '),
+          handler = handlers[parts[0]];
+      if (handler) {
+         handler.apply(handlers, parts.slice(1));
+      }
+  });
+
+  // Stop sending telemetry updates for this connection when closed
+  ws.on('close', unlisten);
+})
+
+
+
+
 var port = process.env.PORT || 8080
 
 httpsServer.listen(port, function () {
     console.log('Open MCT hosted at https://localhost:' + port);
     console.log('History hosted at https://localhost:' + port + '/history');
-    console.log('Realtime hosted at wss://localhost:' + port + '/realtime');
+    console.log('Realtime hosted at wss://localhost:' + port);
 });
