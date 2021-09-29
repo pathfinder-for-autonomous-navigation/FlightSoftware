5d70afed6f902b43df8e622609a8398e497fc559
Govind Chari <govindchari1@gmail.com>
2021-09-28 22:07:57 -0400

    Working HITL with intertank press

--- ptest/cases/propsystem_firing.py
+++ ptest/cases/propsystem_firing.py
@@ -17,20 +17,24 @@ class PropFiringCase(DualSatNearFieldCase):
         self.follower.ws("dcdc.ADCSMotor_cmd", False)
 
     def run(self):
-        self.follower.ws("prop.threshold_firing_pressure", 10)
+        i=0
         propNum = self.rs("prop.state")
         self.print_rs("orbit.control.valve1")
         while(propNum != 7):
+            self.follower.ws("prop.threshold_firing_pressure", 200)
             self.print_rs("prop.state")
             propNum = self.rs("prop.state")
+            self.print_rs("prop.tank2.pressure")
+            self.print_rs("prop.threshold_firing_pressure")
             self.print_rs("orbit.control.valve1")
             self.print_rs("prop.cycles_until_firing")
             self.print_rs("orbit.control.J_ecef")
             self.print_rs("prop.state")
-            self.print_rs("orbit.control.valve1")
             self.print_rs("prop.cycles_until_firing")
             self.print_rs("gomspace.vbatt")
-            self.print_rs("dcdc.SpikeDock")
-            self.print_rs("adcs.state")
-
+            if (propNum == 3 and i<100):
+                i += 1
+            elif (propNum == 3 and i==100):
+                self.follower.ws("prop.threshold_firing_pressure", 10)
+                i = 0
             self.cycle()
