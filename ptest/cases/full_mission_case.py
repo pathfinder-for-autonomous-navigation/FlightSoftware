1048af88f3a80c7daf4d6f4c66d2d7d8f977b166
Duncan-McD <dam342@cornell.edu>
2021-10-02 13:25:45 -0400

    Changed structure of case

--- ptest/cases/full_mission_case.py
+++ ptest/cases/full_mission_case.py
@@ -11,98 +11,88 @@ class FullMissionCase(DualSatCase, PSimCase):
 
   def __init__(self, *args, **kwargs):
     super(FullMissionCase, self).__init__(*args, **kwargs)
-    self.leader_skip_deployment_wait = False
-    self.follower_skip_deployment_wait = False
-    self.psim_configs += ["truth/deployment"]
+    self.leader_skip_deployment_wait = True
+    self.follower_skip_deployment_wait = True
+    self.psim_configs += ["truth/standby"]
     
   def post_boot(self):
     """
       This prints whether the piksi_off is valid after booting
     """
     leader_piksi_off = self.leader.print_rs("gomspace.piksi_off")
-    self.soft_assert( leader_piksi_off == True, f"Leader Piksi_off is {leader_piksi_off} when it should be True")
+    if not leader_piksi_off:
+      raise TestCaseFailure(f"Leader Piksi_off is {leader_piksi_off} when it should be True")
     follower_piksi_off = self.follower.print_rs("gomspace.piksi_off")
-    self.soft_assert( follower_piksi_off == True, f"Follower Piksi_off is {follower_piksi_off} when it should be True")
+    if not follower_piksi_off:
+      raise TestCaseFailure( f"Follower Piksi_off is {follower_piksi_off} when it should be True")
+
+    # Disable to radio to see if it can reenable by standby
+    self.logger.put("SETTING FOLLOWER RADIO TO DISABLED:")
+    self.follower.print_ws("radio.state", 5)
+    self.logger.put("SETTING LEADER RADIO TO DISABLED:")
+    self.leader.print_ws("radio.state", 5)
 
   def run(self):
+
+    self.logger.put("Follower and Leader Starting Up")
+
+    # wait until detumbling
+    while self.follower.rs("pan.state") != Enums.mission_states["startup"] or self.leader.rs("pan.state") != Enums.mission_states["startup"]:
+      self.cycle()
     
-    # During each run, run different actions based on the current follower and leader states
-    self.full_mission_checkout(self.follower.rs("pan.state"), self.leader.rs("pan.state"))
+    self.cycle()
+    self.logger.put("Follower and Leader Detumbling")
+
+    #self.leader.ws("pan.state", 3)
+    #self.follower.ws("pan.state", 3)
+    
+    # wait until standby
+    while self.follower.rs("pan.state") != Enums.mission_states["standby"] or self.leader.rs("pan.state") != Enums.mission_states["standby"]:
+      self.cycle()
+    
+    self.logger.put("Follower and Leader in Standby")
+
+    self.cycle()
     self.cycle()
 
+    # Check if Radio State is not disabled for leader and follower
+    follower_radio_state = self.follower.rs("radio.state")
+    if follower_radio_state == Enums.radio_states["disabled"]:
+      raise TestCaseFailure(f"Follower Radio State is Disabled")
+    else:
+      self.logger.put("Follower Radio is Enabled")
     
-  def full_mission_checkout(self, followerState, leaderState):
+    leader_radio_state = self.leader.rs("radio.state")
+    if leader_radio_state == Enums.radio_states["disabled"]:
+       raise TestCaseFailure(f"Leader Radio State is Disabled")
+    else:
+      self.logger.put("Leader Radio is Enabled")
+
+    # Set satellite designations
+    self.logger.put("Setting follower sat designation to trigger Far Field / Follower State.")
+    self.flight_controller_follower.write_state("pan.sat_designation", Enums.sat_designations["follower"])
+    self.logger.put("Setting leader sat designation to trigger Far Field / Leader State.")
+    self.flight_controller_leader.write_state("pan.sat_designation", Enums.sat_designations["leader"])
+
+    # Wait for follower and Leader
+    while self.follower.rs("pan.state") != Enums.mission_states["follower"] or self.leader.rs("pan.state") != Enums.mission_states["leader"]:
+      self.cycle()
+
     
-    # 1. Startup
-    if followerState == 0 and leaderState == 0: 
-      if self.firstStartup:
-        self.logger.put("Follower and Leader Starting Up")
-        self.firstStartup = False
+    self.logger.put("Follower in Far Field / Follower State")
+    self.logger.put("Leader in Far Field / Leader State")
 
-    # 2. Detumble
-    elif followerState == 1 and leaderState == 1:
-      if self.firstDetumble:
-        self.logger.put("Follower and Leader Detumbling")
-        self.firstDetumble = False
+    # Wait for near field
+    while self.follower.rs("pan.state") != Enums.mission_states["follower_close_approach"] or self.leader.rs("pan.state") != Enums.mission_states["leader_close_approach"]:
+      self.leader.ws("pan.state", Enums.mission_states["leader_close_approach"])
+      self.follower.ws("pan.state", Enums.mission_states["follower_close_approach"])
+      self.cycle()
+    
+    self.logger.put("Follower in Near Field / Follower Close Approach State")
+    self.logger.put("Leader in Near Field / Leader Close Approach State")
 
-    # 3. Standby
-    elif followerState == 3 and leaderState == 3:
-      if self.firstStandby:
-        self.logger.put("Follower and Leader in Standby")
-        self.firstStandby = False
-      else:
-
-        # 3.1 After first cycle of standby, check we can disable the radios
-
-        self.logger.put("SETTING FOLLOWER RADIO TO DISABLED:")
-        self.follower.print_ws("radio.state", 5)
-        self.cycle()
-        follower_radio_state = self.follower.print_rs("radio.state")
-        self.soft_assert( follower_radio_state == 5, f"Follower Radio State is {follower_radio_state} when it should be 5")
-        
-        self.logger.put("SETTING LEADER RADIO TO DISABLED:")
-        self.leader.print_ws("radio.state", 5)
-        self.cycle()
-        leader_radio_state = self.leader.print_rs("radio.state")
-        self.soft_assert( leader_radio_state == 5, f"Leader Radio State is {leader_radio_state} when it should be 5")
-
-        # 3.2 Set the sattelite designations to trigger Far Field
-
-        self.logger.put("Setting follower sat designation to trigger Far Field / Follower State.")
-        self.flight_controller_follower.write_state("pan.sat_designation", Enums.sat_designations["follower"])
-        self.logger.put("Setting leader sat designation to trigger Far Field / Leader State.")
-        self.flight_controller_leader.write_state("pan.sat_designation", Enums.sat_designations["leader"])
+    # If it made it to nearfield with no assertion, then it passed
+    self.finish()
 
 
-    # 4. Far Field
-    elif followerState == 4 and leaderState == 5:
-      if self.firstFarField:
-        self.logger.put("Follower in Far Field / Follower State")
-        self.logger.put("Leader in Far Field / Leader State")
-        self.firstFarField = False
-
-    # 5. Near Field
-    elif followerState == 6 and leaderState == 7:
-      if self.firstNearField:
-        self.logger.put("Follower in Near Field / Follower Close Approach State")
-        self.logger.put("Leader in Near Field / Leader Close Approach State")
-        self.firstNearField = False
-
-        self.cycle()
-        
-        # 5.1 Test setting the radios back to enabled (or the wait state)
-
-        self.logger.put("SETTING FOLLOWER BACK TO ENABLED (wait):")
-        self.follower.print_ws("radio.state",0)
-        self.cycle()
-        follower_radio_state = self.follower.print_rs("radio.state")
-        self.soft_assert( follower_radio_state == 0, f"Follower Radio State is {follower_radio_state} when it should be 0")
-
-        self.logger.put("SETTING LEADER BACK TO ENABLED (wait):")
-        self.leader.print_ws("radio.state",0)
-        self.cycle()
-        leader_radio_state = self.leader.print_rs("radio.state")
-        self.soft_assert( leader_radio_state == 0, f"Leader Radio State is {leader_radio_state} when it should be 0")
-        
-        # If it reaches this point everything worked as expected
-        self.finish(error=True)
+  
