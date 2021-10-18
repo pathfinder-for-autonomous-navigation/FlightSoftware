5ed3ece09182b82725f0028a2045cc33dd82a8f3
Shihao Cao <shehowcow@gmail.com>
2021-10-18 02:38:36 -0400

    Revert Telem (#835)
    * Revert "Mission state telemetry (#824)"
    
    This reverts commit e729af055b44d15fe9f81212ab648b259768155d.
    
    * revert and merge conflicts

--- test/test_fsw_downlink_producer/test_downlink_producer.cpp
+++ test/test_fsw_downlink_producer/test_downlink_producer.cpp
@@ -11,21 +11,7 @@ struct TestFixture {
     std::unique_ptr<DownlinkProducer> downlink_producer;
 
     std::shared_ptr<ReadableStateField<unsigned int>> foo1_fp;
-    std::shared_ptr<Fault> adcs_wheel1_fault_fp;
-    std::shared_ptr<Fault> adcs_wheel2_fault_fp;
-    std::shared_ptr<Fault> adcs_wheel3_fault_fp;
-    std::shared_ptr<Fault> adcs_wheelpot_fault_fp;
-    std::shared_ptr<Fault> adcs_functional_fault_fp;
-    std::shared_ptr<Fault> prop_pressurize_fault_fp;
-    std::shared_ptr<Fault> attitude_estimator_fault_fp;
-    std::shared_ptr<Fault> gs_low_batt_fault_fp;
-    std::shared_ptr<Fault> prop_overpressured_fault_fp;
-    std::shared_ptr<Fault> tank2_temp_fault_fp;
-    std::shared_ptr<Fault> tank1_temp_fault_fp;
-    std::shared_ptr<Fault> piksi_dead_fault_fp;
-    std::shared_ptr<Fault> gs_hk_fault_fp;
     std::shared_ptr<ReadableStateField<unsigned int>> cycle_count_fp;
-    std::shared_ptr<WritableStateField<unsigned char>> pan_state_fp;
     InternalStateField<char*>* snapshot_ptr_fp;
     InternalStateField<size_t>* snapshot_size_bytes_fp;
     WritableStateField<unsigned char>* shift_flows_id1_fp;
@@ -37,31 +23,15 @@ struct TestFixture {
     void init(const std::vector<DownlinkProducer::FlowData>& flow_data) {
         // Create required field(s)
         cycle_count_fp = registry.create_readable_field<unsigned int>("pan.cycle_no");
-        pan_state_fp = registry.create_writable_field<unsigned char>("pan.state");
-        pan_state_fp->set(static_cast<unsigned char>(mission_state_t::startup));
 
         // Create field(s) for serialization and initialize them to
         // default values
         foo1_fp = registry.create_readable_field<unsigned int>("foo1");
-        adcs_wheel1_fault_fp = registry.create_fault("adcs_monitor.wheel1_fault", 1);
-        adcs_wheel2_fault_fp = registry.create_fault("adcs_monitor.wheel2_fault", 1);
-        adcs_wheel3_fault_fp = registry.create_fault("adcs_monitor.wheel3_fault", 1);
-        adcs_wheelpot_fault_fp = registry.create_fault("adcs_monitor.wheel_pot_fault", 1);
-        adcs_functional_fault_fp = registry.create_fault("adcs_monitor.functional_fault", 1);
-        prop_pressurize_fault_fp = registry.create_fault("prop.pressurize_fail", 1);
-        attitude_estimator_fault_fp = registry.create_fault("attitude_estimator.fault", 1);
-        gs_low_batt_fault_fp = registry.create_fault("gomspace.low_batt", 1);
-        prop_overpressured_fault_fp = registry.create_fault("prop.overpressured", 1);
-        tank2_temp_fault_fp = registry.create_fault("prop.tank2_temp_high", 1);
-        tank1_temp_fault_fp = registry.create_fault("prop.tank1_temp_high", 1);
-        piksi_dead_fault_fp = registry.create_fault("piksi_fh.dead", 1);
-        gs_hk_fault_fp = registry.create_fault("gomspace.get_hk", 1);
         cycle_count_fp->set(20);
         foo1_fp->set(400);
 
         downlink_producer = std::make_unique<DownlinkProducer>(registry);
         downlink_producer->init_flows(flow_data);
-        downlink_producer->init();
         snapshot_ptr_fp = registry.find_internal_field_t<char*>("downlink.ptr");
         snapshot_size_bytes_fp = registry.find_internal_field_t<size_t>(
                                     "downlink.snap_size");
@@ -532,163 +502,6 @@ void test_toggle() {
     TEST_ASSERT_EQUAL(0, tf.toggle_flow_id_fp->get()); 
 }
 
-void test_fault_reordering() {
-    TestFixture tf;
-
-    std::vector<DownlinkProducer::FlowData> flow_data = {
-        {
-            1, true, {"foo1"} 
-        },
-        {
-            2, true, {"foo1"} 
-        },
-        {
-            3, true, {"foo1"} 
-        },
-        {
-            4, true, {"foo1"} 
-        },
-        {
-            5, true, {"foo1"} 
-        },
-        {
-            6, true, {"foo1"} 
-        },
-        {
-            7, true, {"adcs_monitor.wheel1_fault.base"} 
-        }
-    };
-    tf.init(flow_data);
-    std::vector<DownlinkProducer::Flow> flows=tf.downlink_producer->get_flows();
-
-    std::vector<int> desired_ids={1,2,3,4,5,6,7};
-    for (size_t i = 0; i<flows.size(); i++){
-        unsigned char flow_id;
-        flows[i].id_sr.deserialize(&flow_id);
-        TEST_ASSERT_EQUAL(desired_ids[i], flow_id);
-    }
-    
-    // Signal the fault and cycle
-    tf.adcs_wheel1_fault_fp->override();
-    tf.downlink_producer->execute();
-
-    // Get the new flow vector and check that the flows have been reordered as desired
-    flows=tf.downlink_producer->get_flows();
-    desired_ids={1,2,7,3,4,5,6};
-    for (size_t i = 0; i<flows.size(); i++){
-        unsigned char flow_id;
-        flows[i].id_sr.deserialize(&flow_id);
-        TEST_ASSERT_EQUAL(desired_ids[i], flow_id);
-    }
-
-    // unsignal the fault and cycle
-    tf.adcs_wheel1_fault_fp->un_override();
-    tf.adcs_wheel1_fault_fp->suppress();
-    tf.downlink_producer->execute();
-
-    // Get the new flow vector and check that the flows have been reordered as desired
-    flows=tf.downlink_producer->get_flows();
-    desired_ids={1,2,3,4,5,6,7};
-    for (size_t i = 0; i<flows.size(); i++){
-        unsigned char flow_id;
-        flows[i].id_sr.deserialize(&flow_id);
-        TEST_ASSERT_EQUAL(desired_ids[i], flow_id);
-    }
-}
-
-void test_state_flow_reordering() {
-    TestFixture tf;
-    std::vector<DownlinkProducer::FlowData> flow_data = {
-        {
-            1, true, {"foo1"} 
-        },
-        {
-            2, true, {"foo1"} 
-        },
-        {
-            3, true, {"foo1"} 
-        },
-        {
-            4, true, {"foo1"} 
-        },
-        {
-            5, true, {"foo1"} 
-        },
-        {
-            6, true, {"foo1"} 
-        },
-        {
-            7, true, {"foo1"} 
-        }, 
-        {
-            8, true, {"foo1"} 
-        },
-        {
-            9, true, {"foo1"} 
-        },
-        {
-            10, true, {"foo1"} 
-        },
-        {
-            11, true, {"foo1"} 
-        },
-        {
-            12, true, {"foo1"} 
-        },
-        {
-            13, true, {"foo1"} 
-        },
-        {
-            14, true, {"foo1"} 
-        }, 
-        {
-            15, true, {"foo1"} 
-        },
-        {
-            16, true, {"foo1"} 
-        },
-        {
-            17, true, {"foo1"} 
-        }
-    };
-    tf.init(flow_data);
-    std::vector<DownlinkProducer::Flow> flows=tf.downlink_producer->get_flows();
-
-    std::vector<int> desired_ids={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17};
-    for (size_t i = 0; i<flows.size(); i++){
-        unsigned char flow_id;
-        flows[i].id_sr.deserialize(&flow_id);
-        TEST_ASSERT_EQUAL(desired_ids[i], flow_id);
-    }
-
-    // change state to follower
-    tf.pan_state_fp->set(static_cast<unsigned char>(mission_state_t::follower));
-    tf.downlink_producer->execute();
-
-    // Get the new flow vector and check that the flows have been reordered as desired
-    flows=tf.downlink_producer->get_flows();
-    desired_ids={1,2,3,4,5,6,7,8,9,10,11,12,13,16,14,15,17};
-    for (size_t i = 0; i<flows.size(); i++){
-        unsigned char flow_id;
-        flows[i].id_sr.deserialize(&flow_id);
-        TEST_ASSERT_EQUAL(desired_ids[i], flow_id);
-    }
-
-    // change state to any state that is not follower or follower_close_approach
-    tf.pan_state_fp->set(static_cast<unsigned char>(mission_state_t::standby));
-
-    tf.downlink_producer->execute();
-
-    // Get the new flow vector and check that the flows have been reordered as desired
-    flows=tf.downlink_producer->get_flows();
-    desired_ids={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17};
-    for (size_t i = 0; i<flows.size(); i++){
-        unsigned char flow_id;
-        flows[i].id_sr.deserialize(&flow_id);
-        TEST_ASSERT_EQUAL(desired_ids[i], flow_id);
-    }
-}
-
 int test_downlink_producer_task() {
     UNITY_BEGIN();
     RUN_TEST(test_task_initialization);
@@ -701,8 +514,6 @@ int test_downlink_producer_task() {
     RUN_TEST(test_shift_priorities);
     RUN_TEST(test_shift_statefield_cmd);
     RUN_TEST(test_toggle);
-    RUN_TEST(test_fault_reordering);
-    RUN_TEST(test_state_flow_reordering);
     return UNITY_END();
 }
 
