2010903baa2bb92e36d6931b21cbcf6b63bfbb93
Duncan-McD <dam342@cornell.edu>
2022-01-08 23:09:31 -0500

    fixed issues with pro gsw side

--- pro/__main__.py
+++ pro/__main__.py
@@ -1,8 +1,15 @@
 import sys
+import json
 from .pro import RendevousOrbitPredictor
+from argparse import ArgumentParser
 
 def main(args):
-  pro = RendevousOrbitPredictor(3, "412111111")
+  parser = ArgumentParser(description="Predictor of Rendevous Orbit")
+  parser.add_argument('-c', '--conf', action='store', help='Config File location', required=True)
+  args = parser.parse_args(args)
+  f = open(args.conf)
+  config = json.load(f)
+  pro = RendevousOrbitPredictor(config['times'], config['imei'],)
   pro.start()
 
 if __name__ == "__main__":
--- pro/pro.py
+++ pro/pro.py
@@ -1,64 +1,119 @@
 import sched
 import time
+from datetime import datetime
 import requests
+import json
 from elasticsearch import Elasticsearch
 
 searchURL = 'http://localhost:5000/search-es'
 updateURL = 'http://localhost:5000/update-time'
-
+st_base = 'https://www.space-track.org'
+st_login = '/ajaxauth/login'
+st_upload = '/fileshare/query/class/upload/folder_id/'
+#TODO: DETERMINE FOLDER ID
+st_folder_id = 'ID'
+st_logout = '/ajaxauth/logout'
 class RendevousOrbitPredictor(object):
   # def __init__(self, amount_per_day, start, imei):
-  def __init__(self, amount_per_day, imei):
+  def __init__(self, times, imei):
     self.scheduler = sched.scheduler(time.time, time.sleep)
-    # self.start_time = start
-    self.seconds_apart = 60 * 60 * (24/int(amount_per_day))
     self.index = 'statefield_report_' + imei
     self.es = Elasticsearch()
-    self.critical_telem = {
-      "orbit.vel": -1,
-      "orbit.pos": -1
-    }
+    self.times = self.time_string_to_time(times)
+
+  def time_string_to_time(self, l):
+    time_objs = []
+    for str in l:
+      parsed_time = list(time.strptime(str, "%H:%M:%S"))
+      current_time = list(time.localtime())
+      full_time = time.struct_time(tuple(current_time[0:3] + parsed_time[3:6] + current_time[6:]))
+      time_objs.append(time.mktime(full_time))
+    return time_objs
+
 
   # Starts the first run (currently time of starting the script 
   #  - can be changed to a fixed point in the future)
   def start(self):
-    self.scheduler.enter(0, 1, self.periodic_send)
+    self.next = self.get_next_time()
+    if self.next>= len(self.times):
+      self.next_day_of_times()
+      self.next = 0
+    self.scheduler.enterabs(self.times[self.next], 1, self.periodic_send)
+    print("Waiting for " + self.epoch_time_to_string(self.times[self.next]) + "...")
+    
     self.scheduler.run()
+  
+
+  def get_next_time(self):
+    new_t =time.time()
+    i = 0
+    for t in self.times:
+      if new_t < t:
+        return i
+      i = i+1
+    return i
+
+  def epoch_time_to_string(self, t):
+    return time.strftime("%a, %d %b %Y %H:%M:%S %z", time.localtime(t))
 
   def periodic_send(self):
     #Scheduling next run
-    self.scheduler.enter(self.seconds_apart, 1, self.periodic_send)
-
-    #updating the current value of each of the telemetry frields from ES
-    for key in self.critical_telem:
-      self.critical_telem[key] = self.request_telem(key)
+    self.next = self.next + 1
+    if self.next>= len(self.times):
+      self.next_day_of_times()
+      self.next = 0
+    self.scheduler.enterabs(self.times[self.next], 1, self.periodic_send)
     
     # Call scripts for making calculations and generating files to upload
     self.call_scripts()
 
-    # Upload file to space-track
-    self.upload_file()
+    #Try Uploading the file to space-track 5 times
+    # uploaded = false
+    # tries = 0
+    # while not uploaded and tries < 5:
+    #   tries = tries + 1
+    #   uploaded = self.upload_file()
 
     # update the time field in elasticsearch
     self.update_time()
 
-  
-  def request_telem(self, name):
-    val = requests.get(searchURL, params={'index':self.index, 'field':name})
-    return val.text
+    print("Waiting for " + self.epoch_time_to_string(self.times[self.next]) + "...")
+
+  def next_day_of_times(self):
+    i = 0
+    for t in self.times:
+      self.times[i] = t + (24*60*60)
+      i = i + 1
 
 
-  # TODO 2: Call python file that makes calculations/generates file
+  # TODO 1: Call python file that makes calculations/generates file
   def call_scripts(self):
     print("TODO: call_scripts")
 
-  # TODO 3: Upload File to space-track using API or Email
+
   def upload_file(self):
-    print("TODO: upload_file")
-    
-  # TODO 4: Update the "Last Uploaded" field
+    with requests.Session() as session:
+      resp = session.post(st_base + st_login, data = {'identity': self.username, 'password': self.password})
+      if resp.status_code != 200:
+        return false
+      resp = session.post(st_base + st_upload + st_folder_id, data = {'file': FILENAME_HERE})
+      if resp.status_code != 200:
+        return false
+      return true
+
   def update_time(self):
-    print("TODO: update_time")
-  
+    
+    # Current Time
+    val = time.strftime("%a, %d %b %Y %H:%M:%S %z", time.localtime())
+    data=json.dumps({
+        "pro.last": val,
+        "time.downlink_received": str(datetime.utcnow().isoformat())[:-3]+'Z'
+    })
+
+    #index the time of file upload
+    res = self.es.index(index=self.index, body=data)
+    
+    # Confirm result status
+    print("pro.last " + res['result'] + ": " + val)
 
     
