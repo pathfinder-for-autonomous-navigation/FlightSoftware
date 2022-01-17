a71f02d0d7759986d3e0e1d48bc8d5fcc398b1ac
masterchief <pan.ssds.qlocate@gmail.com>
2022-01-17 09:11:16 +0000

    added logs to Mission Control landing page

--- groundserver/pan-mission-control/src/App.jsx
+++ groundserver/pan-mission-control/src/App.jsx
@@ -3,13 +3,39 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <img src="/PANMissionControl.png" alt="Pan Mission Control Log" />
-     <ul>
-      <h1><a href="https://mct.panmc.dev">OpenMCT Station</a></h1>
-      <h1><a href="https://follower.panmc.dev/swagger">Follower Commander</a></h1>
-      <h1><a href="https://leader.panmc.dev/swagger">Leader Commander</a></h1>
-      <h1><a href="https://github.com/pathfinder-for-autonomous-navigation/FlightSoftware/issues/852">Uplink Command Log</a></h1>
-     </ul>
+      <img src="/PANMissionControl.png" alt="Pan Mission Control Log" className='ResponsiveImage'/>
+      <div>
+        <ul className='Module'>
+          <h1 className='ModuleName'>Telemetry</h1>
+          <h2>
+            <a href="https://mct.panmc.dev" target="_blank">OpenMCT Station</a>
+          </h2>
+        </ul>
+
+        <ul className='Module'>
+          <h1 className='ModuleName'>Uplinks</h1>
+          <div>
+            <h2 className='Commander'>Leader:</h2>
+            <h2 className='Commander'>
+              <a href="https://leader.panmc.dev/swagger" target="_blank">Commander</a>
+            </h2>
+            <h2 className='Log'>
+              <a href="https://github.com/pathfinder-for-autonomous-navigation/FlightSoftware/issues/852" target="_blank">Log</a>
+            </h2>
+          </div>
+          
+          <div>
+            <h2 className='Commander'>Follower:</h2>
+            <h2 className='Commander'>
+              <a href="https://follower.panmc.dev/swagger" target="_blank">Commander</a>
+            </h2>
+            <h2 className='Log'>
+              <a href="https://github.com/pathfinder-for-autonomous-navigation/FlightSoftware/issues/853" target="_blank">Log</a>
+            </h2>
+          </div> 
+          <h3 className='Notice'>Reminder: Fill out a log for each uplink </h3>
+        </ul>
+      </div>
     </div>
   );
 }
